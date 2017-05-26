34b8107cc26d6a3ec23552a65a348306fc999bd2
Andre Natal <anatal@gmail.com>
2017-05-26 12:46:52 -0700

    Adding reachability to check for internet connection

--- ios/voicebank-ios-wrapper/ViewController.swift
+++ ios/voicebank-ios-wrapper/ViewController.swift
@@ -1,32 +1,33 @@
-//
-//  ViewController.swift
-//  voicebank-ios-wrapper
-//
-//  Created by Andre Natal on 5/10/17.
-//  Copyright © 2017 Andre Natal. All rights reserved.
-//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import WebKit
 
 class ViewController: UIViewController, WKScriptMessageHandler {
     var webView: WKWebView?
     var recorder: Recorder!
-    
+    @IBOutlet weak var labelStatus: UILabel!
+
     override func loadView() {
         super.loadView()
         
-        // create the webview and load the commonvoice website in it
-        webView = WKWebView(frame: self.view.frame)
-        webView?.configuration.userContentController.add(self, name: "scriptHandler")
-        webView?.scrollView.isScrollEnabled = false
-        self.view.addSubview(webView!)
-        let url = URL(string: "https://test.mozvoice.org")
-        let request = URLRequest(url: url!)
-        webView?.load(request)
-        
-        // start the recorder object and ask permission to capture
-        recorder = Recorder()
-        recorder.webView = webView!
+        if !Reachability.isConnectedToNetwork(){
+            labelStatus.text = "Internet Connection not available!"
+        } else {
+            // create the webview and load the commonvoice website in it
+            webView = WKWebView(frame: self.view.frame)
+            webView?.configuration.userContentController.add(self, name: "scriptHandler")
+            webView?.scrollView.isScrollEnabled = false
+            self.view.addSubview(webView!)
+            let url = URL(string: "http://192.168.0.44:8000/index-cv.html")
+            let request = URLRequest(url: url!)
+            webView?.load(request)
+            
+            // start the recorder object and ask permission to capture
+            recorder = Recorder()
+            recorder.webView = webView!
+        }
     }
     
     public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
