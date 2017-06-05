ec7c04ed7aabfdb686efbee3860bded77ec19aa8
Marcus Cavanaugh <m@mcav.com>
2017-06-06 01:55:58 +0200

    Add a confirm modal (library function), and use it to allow the user to retry mic access. Fixes #61.

---
+++ web/src/lib/confirm.ts
@@ -0,0 +1,49 @@
+/**
+ * Present a modal dialog (asynchronously). Usage:
+ *
+ *   confirm('are you sure?').then(...)
+ *
+ * @param label A string label.
+ * @return {Promise}
+ */
+export default function confirm(label: string, okLabel: string, cancelLabel: string) {
+  let element = document.createElement('div');
+  element.classList.add('confirm-modal');
+
+  let mainSection = document.createElement('div');
+  mainSection.classList.add('main-section');
+  element.appendChild(mainSection);
+
+  const labelElement = document.createElement('label');
+  labelElement.textContent = label;
+  mainSection.appendChild(labelElement);
+
+  const confirmButton = document.createElement('button');
+  confirmButton.textContent = okLabel;
+  mainSection.appendChild(confirmButton);
+
+  const cancelButton = document.createElement('button');
+  cancelButton.textContent = cancelLabel;
+  mainSection.appendChild(cancelButton);
+
+  function close() {
+    element.parentElement.removeChild(element);
+  }
+
+  return new Promise((resolve, reject) => {
+    document.body.appendChild(element);
+    confirmButton.addEventListener('click', () => {
+      close();
+      resolve();
+    });
+    cancelButton.addEventListener('click', () => {
+      close();
+      reject();
+    });
+
+    // In the next loop, add the visible class so that we can animate the transition.
+    setTimeout(() => {
+      element.classList.add('visible');
+    });
+  });
+}
