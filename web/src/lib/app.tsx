f9ae436cd2491569251d8bf8a192beeb04b785e6
Michael Henretty <michael.henretty@gmail.com>
2017-06-07 00:25:29 +0200

    simplify app boot process

--- web/src/lib/app.tsx
+++ web/src/lib/app.tsx
@@ -10,9 +10,7 @@ import DebugBox from './debug-box';
  */
 export default class App {
 
-  // Allows controls of the different pages.
   box: DebugBox;
-  pages: Pages;
   user: User;
 
   /**
@@ -20,31 +18,18 @@ export default class App {
    */
   constructor() {
     if (isNativeIOS()) {
-      // Put up the debug box in ios app for now.
-      this.box = new DebugBox();
-
       // For styling fixes on ios.
       document.body.classList.add('ios');
+
+      // Put up the debug box in ios app for now.
+      this.box = new DebugBox();
     }
 
     this.user = new User();
-    this.signalLoading();
-  }
-
-  /**
-   * Inform user that page is loading.
-   */
-  private signalLoading(): void {
-    document.body.classList.remove('loaded');
-    document.body.classList.add('loading');
-  }
-
-  /**
-   * Inform user that page has loaded.
-   */
-  private signalLoaded(): void {
-    document.body.classList.remove('loading');
     document.body.classList.add('loaded');
+
+    // Force binding of handleNavigation to this instance.
+    this.handleNavigation = this.handleNavigation.bind(this);
   }
 
   /**
@@ -64,31 +49,23 @@ export default class App {
    */
   private handleNavigation(href: string) {
     let page = this.getPageName(href);
-    // Update the url history and inform apprioriate controller.
     window.history.pushState(null, '', page);
-    this.route();
+    this.renderCurrentPage();
+  }
+
+  private renderCurrentPage() {
+    // Render the main controller, Pages.
+    render((
+      <Pages user={this.user}
+             navigate={this.handleNavigation}
+             currentPage={this.getPageName()} />
+    ), document.body, document.body.lastChild as Element);
   }
 
   /**
    * Entry point for the application.
    */
   run(): void {
-    // We'll need a bound navigation handler both now and later.
-    let handler = this.handleNavigation.bind(this);
-
-    // Listen and respond to any navigation requests.
-    //this.pages.on('nav', handler);
-
-    this.signalLoaded();
-    handler();
-  }
-
-  /**
-   * Give our page contoller the right page name.
-   */
-  route(): void {
-    let name: string = this.getPageName();
-    let root = document.getElementById('pages');
-    render(<Pages user={this.user} navigate={this.handleNavigation.bind(this)} currentPage={name} />, root, root.firstElementChild);
+    this.renderCurrentPage();
   }
 }
