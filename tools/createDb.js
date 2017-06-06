e953be802668257625644b0977b8ff89e00f64c3
Michael Henretty <michael.henretty@gmail.com>
2017-06-06 16:28:34 +0200

    test postgres database creation in travis

--- tools/createDb.js
+++ tools/createDb.js
@@ -13,16 +13,11 @@ const DBNAME = config.PGNAME || DEFAULT;
  */
 function run(callback) {
   // Make sure we have a superuser username and password
-  let user = config.DB_ROOT_USER;
-  let pass = config.DB_ROOT_PASS;
+  let user = config.DB_ROOT_USER || 'postgres';
+  let pass = config.DB_ROOT_PASS || '';
+
   if (!user) {
     console.error('need DB_ROOT_USER defined in config.json');
-    process.exitCode = 1;
-    return;
-  }
-
-  if (!pass) {
-    console.error('need DB_ROOT_PASS defined in config.json');
     process.exitCode = 1;
     return;
   }
@@ -74,6 +69,7 @@ function run(callback) {
     .onComplete((err, result) => {
       if (err) {
         console.error('database create error', err);
+        process.exitCode = 1;
       }
 
       // Clean up any open connections.
@@ -87,13 +83,9 @@ function run(callback) {
 // Allow running as a script and module.
 if (require.main === module) {
   run(err => {
-    if (err) {
-      console.error('Problem creating db', err);
-      process.exitCode = 1;
-      return;
+    if (!err) {
+      console.log('Db created.');
     }
-
-    console.log('Db created.');
   });
 } else {
   exports.run = run;
