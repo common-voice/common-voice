3e797a879bc8fbb400b31a78a7a3df096ca7952e
Michael Henretty <michael.henretty@gmail.com>
2017-06-09 05:24:07 -0700

    remove gray box around clicked links

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,6 +2,10 @@ declare var define;
 declare var require;
 define('preact', () => { return preact; })
 
+// Safari hack to allow :active styles.
+document.addEventListener("touchstart", function(){}, true);
+
+// Start the app when DOM is ready.
 document.addEventListener('DOMContentLoaded', () => {
   let App = require('./lib/app').default;
   let app = new App();
