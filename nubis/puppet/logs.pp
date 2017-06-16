6554e43ce633300f74d051c083cdeb20e2271855
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-06-16 14:46:03 -0400

    add node service logs to fluent

---
+++ nubis/puppet/logs.pp
@@ -0,0 +1,17 @@
+class { '::fluentd':
+  service_ensure => stopped
+}
+
+fluentd::configfile { $project_name: }
+
+fluentd::source { 'node-output':
+  configfile => $project_name,
+  type       => 'tail',
+  format     => 'none',
+
+  tag        => 'forward.voice.node.stdout',
+  config     => {
+    'path'     => "/var/log/upstart/${project_name}.log",
+    'pos_file' => "/var/log/upstart/${project_name}.pos",
+  },
+}
