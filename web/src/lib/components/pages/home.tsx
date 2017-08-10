9f1cd533683eecb6c29100ade63e0f33b7526c04
Michael Henretty <michael.henretty@gmail.com>
2017-08-10 14:08:50 +0100

    just a couple of language tweaks

--- web/src/lib/components/pages/home.tsx
+++ web/src/lib/components/pages/home.tsx
@@ -26,7 +26,7 @@ export default class Home extends Component<Props, void> {
       <h1 id="home-title">Project Common Voice</h1>
       <div id="home-layout">
         <div className="left-column">
-          <p>Voice is natural, voice is human. That’s why we’re fascinated with creating usable voice technology for our machines. To train voice systems, a large amount of voice samples is required. Most of the training data used by large companies isn’t available to the majority of developers. We think that stifles innovation so we’re launching Project Common Voice, a project to help make voice recognition open to everyone. Now you can donate your voice to help us build an open-source voice database that anyone can use to make innovative apps for devices and the web.</p>
+          <p>Voice is natural, voice is human. That’s why we’re fascinated with creating usable voice technology for our machines. But to create voice systems, an extremely large amount of voice data is required. Most of the data used by large companies isn’t available to the majority of people. We think that stifles innovation. So we’ve launched Project Common Voice, a project to help make voice recognition open to everyone. Now you can donate your voice to help us build an open-source voice database that anyone can use to make innovative apps for devices and the web.</p>
 
           <p>Read a sentence to help machines learn how real people speak. Check the work of other contributors to improve the quality. It’s that simple!</p>
         </div>
