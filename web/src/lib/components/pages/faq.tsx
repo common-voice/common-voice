3704ad8c953f3809ca1d92ed3d6417c08127b98a
Michael Henretty <michael.henretty@gmail.com>
2017-08-18 14:33:29 +0100

    correct language on faq

--- web/src/lib/components/pages/faq.tsx
+++ web/src/lib/components/pages/faq.tsx
@@ -15,7 +15,7 @@ export default class FAQ extends Component<Props, State> {
       <p>Voice recognition technology could revolutionize the way we interact with machines, but the currently available systems are expensive and proprietary. Common Voice is a project to make voice recognition technology easily accessible to everyone. People donate their voices to a massive database that will let anyone quickly and easily train voice-enabled apps. All voice data will be available to developers.</p>
 
       <h3>Why is it important?</h3>
-      <p>Voice is natural, voice is human. It’s the easiest and most natural way to communicate. With Common Voice, developers can build amazing things––from real-time translators to voice-enabled administrative assistants. But the data they need to build these apps isn’t publicly available. Common Voice will give them what they need to innovate.</p>
+      <p>Voice is natural, voice is human. It’s the easiest and most natural way to communicate. We want developers to be able to build amazing things from real-time translators to voice-enabled administrative assistants. But right now there isn't enough publicly available data to build these kinds of apps. Common Voice will give developers what they need to innovate.</p>
 
       <h3>When will the dataset be available?</h3>
       <p>Mozilla started capturing voices in June and aims to release the open source database later in 2017, under the <a href="https://creativecommons.org/publicdomain/zero/1.0/">CC-0</a> license.</p>
