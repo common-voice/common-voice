a3cdbd2644dc3db5c94d231ad03896a2cce938a4
Gregor Weber <gregorwbr@gmail.com>
2017-11-13 19:07:22 +0100

    turn stateless pages into stateless components

--- web/src/components/pages/not-found.tsx
+++ web/src/components/pages/not-found.tsx
@@ -1,16 +1,8 @@
 import * as React from 'react';
 
-import { RouteComponentProps } from 'react-router';
-
-interface Props extends RouteComponentProps<any> {}
-
-export default class NotFound extends React.Component<Props, {}> {
-  render() {
-    return (
-      <div id="not-found-container">
-        <h2>Not found</h2>
-        <p>I'm afraid I don't know what you're looking for.</p>
-      </div>
-    );
-  }
-}
+export default () => (
+  <div id="not-found-container">
+    <h2>Not found</h2>
+    <p>I'm afraid I don't know what you're looking for.</p>
+  </div>
+);
