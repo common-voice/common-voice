fc4de75661633926b332905d1cd4c8b4e123e8cd
Michael Henretty <michael.henretty@gmail.com>
2017-11-18 17:52:53 +0100

    print model metrics after load

--- server/src/lib/api.ts
+++ server/src/lib/api.ts
@@ -48,6 +48,7 @@ export default class API {
    */
   async loadCache(): Promise<void> {
     await Promise.all([this.clip.loadCache(), this.corpus.loadCache()]);
+    this.model.printMetrics();
   }
 
   /**
