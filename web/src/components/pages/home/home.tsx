6fcc6a6a17f47d7b3f20033f333ceb2856e8ed6f
Gregor Weber <gregorwbr@gmail.com>
2017-11-28 11:56:36 +0100

    fix #706 show more link semibold & fix #708 less explanatory text on mobile

--- web/src/components/pages/home/home.tsx
+++ web/src/components/pages/home/home.tsx
@@ -22,7 +22,9 @@ class Home extends React.Component<RouteComponentProps<any>, State> {
           The Common Voice project is Mozilla's initiative to help teach
           machines how real people speak.
         </h2>
-        <div id="wall-of-text">
+        <div
+          id="wall-of-text"
+          className={showWallOfText ? 'show-more-text' : ''}>
           <CardAction id="contribute-button" to="/record">
             <div>
               <RecordIcon />
@@ -34,29 +36,30 @@ class Home extends React.Component<RouteComponentProps<any>, State> {
             Voice is natural, voice is human. That’s why we’re fascinated with
             creating usable voice technology for our machines. But to create
             voice systems, an extremely large amount of voice data is required.
+          </p>
+
+          <p className="more-text-mobile">
             Most of the data used by large companies isn’t available to the
             majority of people. We think that stifles innovation. So we’ve
             launched Project Common Voice, a project to help make voice
-            recognition open to everyone.{' '}
-            {showWallOfText &&
-              'Now you can donate your voice to help us build an open-source voice database' +
-                ' that anyone can use to make innovative apps for devices and the web.'}
+            recognition open to everyone.
+          </p>
+
+          <p className="more-text-desktop">
+            Now you can donate your voice to help us build an open-source voice
+            database that anyone can use to make innovative apps for devices and
+            the web. <br />
+            Read a sentence to help machines learn how real people speak. Check
+            the work of other contributors to improve the quality. It’s that
+            simple!
           </p>
 
           {!showWallOfText && (
             <a
-              id="show-more-text"
+              id="show-more-button"
               onClick={() => this.setState({ showWallOfText: true })}>
               Read More
             </a>
-          )}
-
-          {showWallOfText && (
-            <p>
-              Read a sentence to help machines learn how real people speak.
-              Check the work of other contributors to improve the quality. It’s
-              that simple!
-            </p>
           )}
         </div>
 
