256eeb7016399eb6f022c4571be6d4c406413eaa
Gregor Weber <gweber@mozilla.com>
2017-12-23 16:35:54 +0100

    get sentences from DB (or S3) just in time
    building the sentence map might have eaten too much memory on prod

--- server/src/lib/model/db/migrate-data/migrate-votes.ts
+++ server/src/lib/model/db/migrate-data/migrate-votes.ts
@@ -4,18 +4,16 @@ import { VoteData } from './fetch-s3-data';
 export async function migrateVotes(
   connection: IConnection,
   votes: VoteData[],
-  sentences: any,
   print: any
 ) {
   const completeVotes: any[] = [];
   const votesWithUnknowns: VoteData[] = [];
   for (const vote of votes as VoteData[]) {
-    const sentence = sentences[vote.clip_sentence_id];
     const [
       [row],
     ] = await connection.execute(
       'SELECT id FROM clips WHERE client_id = ? AND original_sentence_id = ?',
-      [vote.clip_client_id, sentence ? sentence.id : vote.clip_sentence_id]
+      [vote.clip_client_id, vote.clip_sentence_id]
     );
     if (row && vote.clip_client_id && vote.voter_client_id) {
       (vote as any).clip_id = row.id;
@@ -28,7 +26,7 @@ export async function migrateVotes(
   if (votesWithUnknowns.length) {
     print(
       votesWithUnknowns.length,
-      'votes with unknown foreign keys found. Those will NOT be migrated:',
+      'votes with unknown clips found. Those will NOT be migrated:',
       JSON.stringify(votesWithUnknowns)
     );
   }
