c8821e4cf725c8c8130ce68b607be00e13e28c4a
Gregor Weber <gweber@mozilla.com>
2018-01-09 12:18:04 +0100

    fix rate limiting findIndex result not being handled

--- server/src/lib/model/db/migrate-data/aws-rate-limit.ts
+++ server/src/lib/model/db/migrate-data/aws-rate-limit.ts
@@ -6,6 +6,7 @@ let requestTimesInTimestep: number[] = [];
 export async function rateLimit() {
   const now = Date.now();
   const index = requestTimesInTimestep.findIndex(t => t > now - TIMESTEP);
+  if (index === -1) return;
   requestTimesInTimestep = requestTimesInTimestep.slice(index);
   if (requestTimesInTimestep.length > AWS_MAX_GET_CALLS_PER_TIMESTEP) {
     await new Promise(resolve =>
