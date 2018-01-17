9ee29f5f4bf1aa22ba553a717f9517b424428ae3
Gregor Weber <gweber@mozilla.com>
2018-01-17 13:19:38 +0100

    reduce complexity of recording saving fn, with async-await

--- server/src/lib/clip.ts
+++ server/src/lib/clip.ts
@@ -138,7 +138,7 @@ export default class Clip {
       } else if (this.isClipDemographic(request)) {
         this.saveClipDemographic(request, response);
       } else {
-        this.saveClip(request, response);
+        await this.saveClip(request, response);
       }
     } else if (this.isRandomClipJsonRequest(request)) {
       await this.serveRandomClip(request, response);
@@ -257,101 +257,82 @@ export default class Clip {
   /**
    * Save clip posted to server
    */
-  saveClip(request: http.IncomingMessage, response: http.ServerResponse) {
-    this.save(request)
-      .then(timestamp => {
-        respond(response, '' + timestamp);
-      })
-      .catch(e => {
-        console.error('saving clip error', e, e.stack);
-        respond(response, 'Error', 500);
-      });
+  async saveClip(request: http.IncomingMessage, response: http.ServerResponse) {
+    try {
+      const timestamp = await this.save(request);
+      respond(response, '' + timestamp);
+    } catch (e) {
+      console.error('saving clip error', e, e.stack);
+      respond(response, 'Error', 500);
+    }
   }
 
   /**
    * Save the request body as an audio file.
    */
-  save(request: http.IncomingMessage): Promise<string> {
-    let info = request.headers;
-    let uid = info.uid as string;
-    let sentence = decodeURI(info.sentence as string);
+  async save(request: http.IncomingMessage): Promise<string> {
+    const info = request.headers;
+    const uid = info.uid as string;
+    const sentence = decodeURI(info.sentence as string);
 
     if (!uid || !sentence) {
       return Promise.reject('Invalid headers');
     }
 
-    return new Promise((resolve: Function, reject: Function) => {
-      // Obtain contentType
-      let contentType = info['content-type'] as string;
+    // Where is our audio clip going to be located?
+    const folder = uid + '/';
+    const filePrefix = hash(sentence);
+    const file = folder + filePrefix + '.mp3';
+    const txtFile = folder + filePrefix + '.txt';
 
-      // Where is our audio clip going to be located?
-      let folder = uid + '/';
-      let filePrefix = hash(sentence);
-      let file = folder + filePrefix + '.mp3';
-      let txtFile = folder + filePrefix + '.txt';
+    // if the folder does not exist, we create it
+    let params = { Bucket: this.config.BUCKET_NAME, Key: folder };
+    await new Promise(resolve => this.s3.putObject(params, resolve));
 
-      let f = ff(
-        () => {
-          // if the folder does not exist, we create it
-          let params = { Bucket: this.config.BUCKET_NAME, Key: folder };
-          this.s3.putObject(params, f.wait());
+    // If upload was base64, make sure we decode it first.
+    let transcoder;
+    if ((info['content-type'] as string).includes('base64')) {
+      // If we were given base64, we'll need to concat it all first
+      // So we can decode it in the next step.
+      const chunks: Buffer[] = [];
+      await new Promise(resolve => {
+        request.on('data', (chunk: Buffer) => {
+          chunks.push(chunk);
+        });
+        request.on('end', resolve);
+      });
 
-          // If we were given base64, we'll need to concat it all first
-          // So we can decode it in the next step.
-          if (contentType.includes('base64')) {
-            let chunks: Buffer[] = [];
-            f.pass(chunks);
-            request.on('data', (chunk: Buffer) => {
-              chunks.push(chunk);
-            });
-            request.on('end', f.wait());
-          }
-        },
-        (chunks: Buffer[]) => {
-          // If upload was base64, make sure we decode it first.
-          if (contentType.includes('base64')) {
-            let passThrough = new PassThrough();
-            passThrough.end(
-              Buffer.from(Buffer.concat(chunks).toString(), 'base64')
-            );
-            let transcoder = new Transcoder(passThrough);
-            transcoder = transcoder.audioCodec('mp3').format('mp3');
-            let transcoderStream = transcoder.stream();
-            let params = {
-              Bucket: this.config.BUCKET_NAME,
-              Key: file,
-              Body: transcoderStream,
-            };
-            this.s3.upload(params, f());
-          } else {
-            // For now base64 uploads, we can just stream data.
-            let transcoder = new Transcoder(request);
-            transcoder = transcoder.audioCodec('mp3').format('mp3');
-            let transcoderStream = transcoder.stream();
-            let params = {
-              Bucket: this.config.BUCKET_NAME,
-              Key: file,
-              Body: transcoderStream,
-            };
-            this.s3.upload(params, f());
-          }
+      const passThrough = new PassThrough();
+      passThrough.end(Buffer.from(Buffer.concat(chunks).toString(), 'base64'));
+      transcoder = new Transcoder(passThrough);
+    } else {
+      // For non-base64 uploads, we can just stream data.
+      transcoder = new Transcoder(request);
+    }
 
-          // Don't forget about the sentence text!
-          let params = {
-            Bucket: this.config.BUCKET_NAME,
-            Key: txtFile,
-            Body: sentence,
-          };
-          this.s3.putObject(params, f());
-        },
-        () => {
-          // File saving is now complete.
-          console.log('file written to s3', file);
-          this.model.db.saveClip(uid, filePrefix, file, sentence);
-          resolve(filePrefix);
-        }
-      ).onError(reject);
-    });
+    await Promise.all([
+      this.s3
+        .upload({
+          Bucket: this.config.BUCKET_NAME,
+          Key: file,
+          Body: transcoder
+            .audioCodec('mp3')
+            .format('mp3')
+            .stream(),
+        })
+        .promise(),
+      this.s3
+        .putObject({
+          Bucket: this.config.BUCKET_NAME,
+          Key: txtFile,
+          Body: sentence,
+        })
+        .promise(),
+    ]);
+
+    console.log('file written to s3', file);
+    this.model.db.saveClip(uid, filePrefix, file, sentence);
+    return filePrefix;
   }
 
   async serveRandomClip(
