bd0908a7a8ba819c0abd94e312e382f35e09acf7
Gregor Weber <gweber@mozilla.com>
2018-01-18 16:21:41 +0100

    move clip retrieval into model

--- server/src/lib/model.ts
+++ server/src/lib/model.ts
@@ -1,7 +1,9 @@
 import DB from './model/db';
-import { default as Clips } from './model/clips';
 import { CommonVoiceConfig } from '../config-helper';
 import { DBClipWithVoters } from './model/db/tables/clip-table';
+import * as Random from 'random-js';
+
+const CLIP_CACHE_SIZE = 1000;
 
 /**
  * The Model loads all clip and user data into memory for quick access.
@@ -9,12 +11,45 @@ import { DBClipWithVoters } from './model/db/tables/clip-table';
 export default class Model {
   config: CommonVoiceConfig;
   db: DB;
-  clips: Clips;
+  randomEngine: Random.MT19937;
+
+  private clipsWithFewVotes: DBClipWithVoters[] = [];
+  private isRefillingClipCache = false;
 
   constructor(config: CommonVoiceConfig) {
     this.config = config;
     this.db = new DB(this.config);
-    this.clips = new Clips(this.db);
+    this.randomEngine = Random.engines.mt19937();
+    this.randomEngine.autoSeed();
+  }
+
+  private async refillClipsWithFewVotes() {
+    if (this.isRefillingClipCache) return;
+    this.isRefillingClipCache = true;
+    this.clipsWithFewVotes = this.clipsWithFewVotes.concat(
+      Random.shuffle(
+        this.randomEngine,
+        await this.db.findClipsWithFewVotes(CLIP_CACHE_SIZE)
+      )
+    );
+    this.isRefillingClipCache = false;
+  }
+
+  /**
+   * Fetch a random clip but make sure it's not the user's.
+   */
+  async getEllibleClip(client_id: string): Promise<DBClipWithVoters> {
+    if (this.clipsWithFewVotes.length == 0)
+      await this.refillClipsWithFewVotes();
+
+    const i = this.clipsWithFewVotes.findIndex(
+      clip => clip.client_id !== client_id && !clip.voters.includes(client_id)
+    );
+    if (i == -1) return null;
+
+    const clip = this.clipsWithFewVotes[i];
+    this.clipsWithFewVotes.splice(i, 1);
+    return clip;
   }
 
   private print(...args: any[]) {
@@ -33,17 +68,13 @@ export default class Model {
 
     this.print(totalUserClients, ' total user clients');
     this.print((listeners / totalUserClients).toFixed(2), '% users who listen');
-    this.print((submitters / totalUserClients).toFixed(2), '% users who submit');
+    this.print(
+      (submitters / totalUserClients).toFixed(2),
+      '% users who submit'
+    );
     this.print(totalClips, ' total clips');
     this.print(votes, ' total votes');
     this.print(unverified, ' unverified clips');
-  }
-
-  /**
-   * Fetch a random clip but make sure it's not the user's.
-   */
-  getEllibleClip(client_id: string): Promise<DBClipWithVoters> {
-    return this.clips.getEllibleClip(client_id);
   }
 
   /**
