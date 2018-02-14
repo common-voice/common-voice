cdfb9e1663bf17dd47a622eb1cda30893e3c29f7
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-02-14 12:01:12 -0500

    Parse systemd's journal for JSON formatted messages
    Fixes #868

---
+++ nubis/puppet/logs.pp
@@ -0,0 +1,20 @@
+# Fluentd module doesn't know how to handle parser filters yet
+file { "/etc/td-agent/config.d/0-${project_name}-json.conf":
+  ensure => present,
+  owner   => 'root',
+  group  => 'root',
+  mode   => '0644',
+  content => @(EOF)
+# Possibly parse the message field as JSON formatted
+<filter ec2.forward.systemd.journal>
+  @type parser
+  key_name message
+  reserve_data true
+  inject_key_prefix message.
+  emit_invalid_record_to_error false
+  <parse>
+    @type json
+  </parse>
+</filter>
+EOF 
+}
