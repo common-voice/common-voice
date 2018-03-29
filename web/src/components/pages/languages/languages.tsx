262e52e70299dcf598cf1649e4ed36aebc80d6d8
Gregor Weber <gweber@mozilla.com>
2018-03-29 12:53:30 +0200

    implement language overview design

--- web/src/components/pages/languages/languages.tsx
+++ web/src/components/pages/languages/languages.tsx
@@ -28,7 +28,7 @@ const LocalizationBox = ({
   onShowHelpTranslateModal?: (locale: Locale) => any;
   progress: number;
 }) => (
-  <div className="language">
+  <li className="language">
     <div className="info">
       <h2>{locale.name}</h2>
       <div className="numbers">
@@ -46,16 +46,17 @@ const LocalizationBox = ({
     </div>
     {onShowHelpTranslateModal && (
       <button onClick={() => onShowHelpTranslateModal(locale)}>
-        Help Translate!
+        Help Translate >
       </button>
     )}
-  </div>
+  </li>
 );
 
 interface Props extends PropsFromState {}
 
 interface State {
   localizations: any;
+  selectedLanguageSection: 'in-progress' | 'launched';
   showAll: boolean;
   showHelpTranslateModalFor: Locale;
   showLanguageRequestModal: boolean;
@@ -64,6 +65,7 @@ interface State {
 class LanguagesPage extends React.Component<Props, State> {
   state: State = {
     localizations: [],
+    selectedLanguageSection: 'in-progress',
     showAll: false,
     showHelpTranslateModalFor: null,
     showLanguageRequestModal: false,
@@ -88,13 +90,14 @@ class LanguagesPage extends React.Component<Props, State> {
   render() {
     const {
       localizations,
+      selectedLanguageSection,
       showAll,
       showHelpTranslateModalFor,
       showLanguageRequestModal,
     } = this.state;
 
     return (
-      <div>
+      <div className={'selected-' + selectedLanguageSection}>
         {showHelpTranslateModalFor && (
           <HelpTranslateModal
             locale={this.state.showHelpTranslateModalFor}
@@ -106,14 +109,18 @@ class LanguagesPage extends React.Component<Props, State> {
 
         <br />
 
-        <div>
-          Don't see your language on Common Voice yet?
-          <br />
-          <Button
-            rounded
-            onClick={() => this.setState({ showLanguageRequestModal: true })}>
-            Request a Language
-          </Button>
+        <div className="top">
+          <img src="/img/waves-mixed.svg" alt="Waves" />
+
+          <div className="text">
+            <h2>Don't see your language on Common Voice yet?</h2>
+            <Button
+              outline
+              rounded
+              onClick={() => this.setState({ showLanguageRequestModal: true })}>
+              Request a Language
+            </Button>
+          </div>
         </div>
 
         <br />
@@ -126,49 +133,90 @@ class LanguagesPage extends React.Component<Props, State> {
           />
         )}
 
-        <div>
-          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <h1>In Progress</h1>
-
-            {!showAll && (
-              <a
-                className="show-all-languages"
-                href="javascript:void(0)"
-                onClick={() => this.setState({ showAll: true })}>
-                See All
-              </a>
-            )}
-          </div>
-
+        <div className="mobile-headings">
           <Hr />
 
-          <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-            {(showAll ? localizations : localizations.slice(0, 3)).map(
-              (localization: any, i: number) => (
-                <LocalizationBox
-                  key={i}
-                  {...localization}
-                  onShowHelpTranslateModal={locale =>
-                    this.setState({ showHelpTranslateModalFor: locale })
-                  }
-                />
-              )
-            )}
+          <div className="labels">
+            <h2
+              className="in-progress"
+              onClick={() =>
+                this.setState({ selectedLanguageSection: 'in-progress' })
+              }>
+              In Progress
+            </h2>
+            <h2
+              className="launched"
+              onClick={() =>
+                this.setState({
+                  selectedLanguageSection: 'launched',
+                  showAll: false,
+                })
+              }>
+              Launched
+            </h2>
           </div>
         </div>
 
-        <br />
-        <br />
+        <div className="language-sections">
+          <section className="in-progress">
+            <div className="desktop-heading">
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <h1>In Progress</h1>
 
-        <div>
-          <h1>Launched</h1>
+                {!showAll && (
+                  <a
+                    className="show-all-languages-desktop"
+                    href="javascript:void(0)"
+                    onClick={() => this.setState({ showAll: true })}>
+                    See All
+                  </a>
+                )}
+              </div>
 
-          <Hr />
+              <Hr />
+            </div>
 
-          <LocalizationBox
-            locale={{ code: 'en', name: 'English', population: EN_POPULATION }}
-            progress={1}
-          />
+            <ul style={{ display: 'flex', flexWrap: 'wrap' }}>
+              {(showAll ? localizations : localizations.slice(0, 3)).map(
+                (localization: any, i: number) => (
+                  <LocalizationBox
+                    key={i}
+                    {...localization}
+                    onShowHelpTranslateModal={locale =>
+                      this.setState({ showHelpTranslateModalFor: locale })
+                    }
+                  />
+                )
+              )}
+            </ul>
+
+            {!showAll && (
+              <button
+                className="show-all-languages-mobile"
+                onClick={() => this.setState({ showAll: true })}>
+                See All
+              </button>
+            )}
+          </section>
+
+          <section className="launched">
+            <div className="desktop-heading">
+              <h1>Launched</h1>
+
+              <Hr />
+            </div>
+
+            <ul>
+              <LocalizationBox
+                locale={{
+                  code: 'en',
+                  name: 'English',
+                  population: EN_POPULATION,
+                }}
+                progress={1}
+              />
+            </ul>
+          </section>
         </div>
       </div>
     );
