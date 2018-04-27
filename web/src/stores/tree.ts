99523f4b5e8ebd7213cb1a5687414dfa6cecd070
Felix Baumann <felix.bau@gmx.de>
2018-04-27 04:56:09 +0200

    noticed a spelling error in a file

--- web/src/stores/tree.ts
+++ web/src/stores/tree.ts
@@ -2,7 +2,7 @@ import API from '../services/api';
 import { Recordings } from './recordings';
 import { User } from './user';
 import { Clips } from './clips';
-import { RequestedLanguages } from './requested-langauges';
+import { RequestedLanguages } from './requested-languages';
 
 export default interface StateTree {
   api: API;
