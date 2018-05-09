d6146124b558b9aed9d4b3c26f1feaaebe036cd7
Gregor Weber <gweber@mozilla.com>
2018-05-09 13:05:54 +0200

    staging banner clarification

--- web/src/components/layout/layout.tsx
+++ web/src/components/layout/layout.tsx
@@ -258,9 +258,13 @@ class Layout extends React.PureComponent<LayoutProps, LayoutState> {
         {window.location.hostname == 'voice.allizom.org' &&
           showStagingBanner && (
             <div className="staging-banner">
-              You're on the staging server.{' '}
-              <a href="https://voice.mozilla.org">Don't waste your breath.</a>{' '}
-              <a href="https://github.com/mozilla/voice-web/issues/new">
+              You're on the staging server. Voice data is not collected here.{' '}
+              <a href="https://voice.mozilla.org" target="_blank">
+                Don't waste your breath.
+              </a>{' '}
+              <a
+                href="https://github.com/mozilla/voice-web/issues/new"
+                target="_blank">
                 Feel free to report issues.
               </a>{' '}
               <button
