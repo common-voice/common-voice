211a1e4de7ba98bac449f57a74b9881a0700cdab
Ignacio Gomez <i.gomez10@uniandes.edu.co>
2018-05-24 21:36:41 +0200

    correct evaluation

--- server/src/lib/model/db.ts
+++ server/src/lib/model/db.ts
@@ -31,9 +31,9 @@ export default class DB {
    * Normalize email address as input.
    */
    private formatEmail(email?: string): string {
-	let re = new RegExp('/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/');
-    if (!email || re.test(email) ) {
-    	return '';
+     let re = new RegExp('/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/');
+     if (!email || !re.test(email) ) {
+       return '';
     }
     return email.toLowerCase();
   }
