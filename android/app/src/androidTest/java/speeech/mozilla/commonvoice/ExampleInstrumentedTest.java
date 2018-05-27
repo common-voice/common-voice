6a25971e4d3ba54a5f7eb0fd88a83ad2c64f0757
missuniverse <31463320+missuniverse@users.noreply.github.com>
2018-05-27 16:32:39 -0400

    Update HTTP links to HTTPS.
    Update HTTP links to HTTPS.

--- android/app/src/androidTest/java/speeech/mozilla/commonvoice/ExampleInstrumentedTest.java
+++ android/app/src/androidTest/java/speeech/mozilla/commonvoice/ExampleInstrumentedTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
 /**
  * Instrumentation test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see <a href="https://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
