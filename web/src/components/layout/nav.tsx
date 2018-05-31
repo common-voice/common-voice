3ccc310c709aab1076edd44443fad7069e20c071
Gregor Weber <gweber@mozilla.com>
2018-05-31 14:30:18 +0200

    nav links to new contribution experience on non-production

--- web/src/components/layout/nav.tsx
+++ web/src/components/layout/nav.tsx
@@ -1,6 +1,7 @@
 import { Localized } from 'fluent-react';
 import * as React from 'react';
 import URLS from '../../urls';
+import { isProduction } from '../../utility';
 import { ContributableLocaleLock, LocaleNavLink } from '../locale-helpers';
 
 import './nav.css';
@@ -8,9 +9,20 @@ import './nav.css';
 export default ({ children, ...props }: { [key: string]: any }) => (
   <nav {...props} className="nav-list">
     <ContributableLocaleLock>
-      <Localized id="speak">
-        <LocaleNavLink to={URLS.RECORD} exact />
-      </Localized>
+      {isProduction() ? (
+        <Localized id="speak">
+          <LocaleNavLink to={URLS.RECORD} exact />
+        </Localized>
+      ) : (
+        <React.Fragment>
+          <Localized id="speak">
+            <LocaleNavLink to={URLS.SPEAK} exact />
+          </Localized>
+          <Localized id="listen">
+            <LocaleNavLink to={URLS.LISTEN} exact />
+          </Localized>
+        </React.Fragment>
+      )}
     </ContributableLocaleLock>
     <Localized id="datasets">
       <LocaleNavLink to={URLS.DATA} exact />
