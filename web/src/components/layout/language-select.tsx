fb80f70b2a694d23df02333b7963b1f465a41eb9
Gregor Weber <gweber@mozilla.com>
2018-06-06 12:10:30 -0700

    update language-select and downshift

--- web/src/components/layout/language-select.tsx
+++ web/src/components/layout/language-select.tsx
@@ -8,38 +8,33 @@ interface Props {
   onChange: (locale: string) => any;
 }
 
-export default class LanguageSelect extends React.PureComponent<Props> {
-  render() {
-    const { locale, locales, onChange } = this.props;
-    return (
-      <Downshift
-        defaultHighlightedIndex={locales.findIndex(([code]) => code == locale)}
-        isOpen={true}
-        onChange={onChange}
-        render={({ getInputProps, getItemProps, highlightedIndex }) => (
-          <div className="language-select with-down-arrow">
-            <div className="selection" tabIndex={0} {...getInputProps()}>
-              {getNativeNameWithFallback(locale)}
-            </div>
-            <div className="list-wrapper">
-              <div className="triangle" />
-              <ul>
-                {locales.map(([code, name], index) => (
-                  <li
-                    key={code}
-                    className={[
-                      code === locale ? 'selected' : '',
-                      index == highlightedIndex ? 'highlighted' : '',
-                    ].join(' ')}
-                    {...getItemProps({ item: code })}>
-                    {name}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        )}
-      />
-    );
-  }
-}
+export default ({ locale, locales, onChange }: Props) => (
+  <Downshift
+    defaultHighlightedIndex={locales.findIndex(([code]) => code == locale)}
+    isOpen={true}
+    onChange={onChange}
+    render={({ getInputProps, getItemProps, highlightedIndex }) => (
+      <div className="language-select with-down-arrow">
+        <div className="selection" tabIndex={0} {...getInputProps()}>
+          {getNativeNameWithFallback(locale)}
+        </div>
+        <div className="list-wrapper">
+          <div className="triangle" />
+          <ul>
+            {locales.map(([code, name], index) => (
+              <li
+                key={code}
+                className={[
+                  code === locale ? 'selected' : '',
+                  index == highlightedIndex ? 'highlighted' : '',
+                ].join(' ')}
+                {...getItemProps({ item: code })}>
+                {name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    )}
+  />
+);
