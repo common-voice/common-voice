eb87ff629514e1fa0438455251e84a35f1128dc1
Gregor Weber <gweber@mozilla.com>
2018-08-22 16:55:49 +0200

    fix clips stats query positional argument

--- server/src/lib/model/db.ts
+++ server/src/lib/model/db.ts
@@ -464,7 +464,7 @@ export default class DB {
               HAVING COUNT(CASE WHEN votes.created_at BETWEEN ? AND ? THEN 1 END) > 0
             ) AS clips;
           `,
-          [d[1], ...d, ...d, localeId]
+          [d[1], ...d, localeId, ...d]
         )
       )
     );
