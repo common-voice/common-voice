11e7836df5dcaf08083f6f53897c2ef4b4028846
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-08-26 00:02:22 -0400

    Upgrade to Nubis v2.3.1

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 module "worker" {
-  source        = "github.com/nubisproject/nubis-terraform//worker?ref=v2.3.0"
+  source        = "github.com/nubisproject/nubis-terraform//worker?ref=v2.3.1"
   region        = "${var.region}"
   environment   = "${var.environment}"
   account       = "${var.account}"
@@ -29,7 +29,7 @@ module "worker" {
 }
 
 module "load_balancer" {
-  source       = "github.com/nubisproject/nubis-terraform//load_balancer?ref=v2.3.0"
+  source       = "github.com/nubisproject/nubis-terraform//load_balancer?ref=v2.3.1"
   region       = "${var.region}"
   environment  = "${var.environment}"
   account      = "${var.account}"
@@ -43,7 +43,7 @@ module "load_balancer" {
 }
 
 module "dns" {
-  source       = "github.com/nubisproject/nubis-terraform//dns?ref=v2.3.0"
+  source       = "github.com/nubisproject/nubis-terraform//dns?ref=v2.3.1"
   region       = "${var.region}"
   environment  = "${var.environment}"
   account      = "${var.account}"
@@ -63,7 +63,7 @@ resource "aws_db_parameter_group" "slow_query_enabled" {
 }
 
 module "database" {
-  source                 = "github.com/nubisproject/nubis-terraform//database?ref=v2.3.0"
+  source                 = "github.com/nubisproject/nubis-terraform//database?ref=v2.3.1"
   region                 = "${var.region}"
   environment            = "${var.environment}"
   account                = "${var.account}"
