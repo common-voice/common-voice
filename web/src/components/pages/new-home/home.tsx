7e5cd476a79a807317132a0c03bd72c3a6a1921b
Gregor Weber <gweber@mozilla.com>
2018-09-05 15:14:58 +0200

    fix #1435 alt homepage for non-contributable locales

--- web/src/components/pages/new-home/home.tsx
+++ web/src/components/pages/new-home/home.tsx
@@ -1,6 +1,8 @@
 import { Localized } from 'fluent-react';
 import * as React from 'react';
+import { ContributableLocaleLock } from '../../locale-helpers';
 import { RecordLink } from '../../primary-buttons/primary-buttons';
+import RequestLanguageModal from '../../request-language-modal/request-language-modal';
 import Hero from './hero';
 import { ClipsStats, VoiceStats } from './stats';
 
@@ -8,10 +10,18 @@ import './home.css';
 
 type HeroType = 'speak' | 'listen';
 
-type State = { activeHero: null | HeroType; showWallOfText: boolean };
+type State = {
+  activeHero: null | HeroType;
+  showRequestLanguageModal: boolean;
+  showWallOfText: boolean;
+};
 
 export default class HomePage extends React.Component<{}, State> {
-  state: State = { activeHero: null, showWallOfText: false };
+  state: State = {
+    activeHero: null,
+    showRequestLanguageModal: false,
+    showWallOfText: false,
+  };
 
   showHandlerFor = (hero: HeroType) => () =>
     this.setState({ activeHero: hero });
@@ -21,24 +31,39 @@ export default class HomePage extends React.Component<{}, State> {
     }));
 
   render() {
-    const { activeHero, showWallOfText } = this.state;
+    const { activeHero, showRequestLanguageModal, showWallOfText } = this.state;
     return (
       <div className="home">
-        <div className="heroes">
-          {['speak', 'listen'].map((type: HeroType) => (
-            <Hero
-              key={type}
-              type={type}
-              status={
-                activeHero === type
-                  ? 'active'
-                  : activeHero ? 'compressed' : null
-              }
-              onShow={this.showHandlerFor(type)}
-              onHide={this.hideHandlerFor(type)}
-            />
-          ))}
-        </div>
+        <ContributableLocaleLock
+          render={({ isContributable }: any) =>
+            isContributable ? (
+              <div className="heroes">
+                {['speak', 'listen'].map((type: HeroType) => (
+                  <Hero
+                    key={type}
+                    type={type}
+                    status={
+                      activeHero === type
+                        ? 'active'
+                        : activeHero ? 'compressed' : null
+                    }
+                    onShow={this.showHandlerFor(type)}
+                    onHide={this.hideHandlerFor(type)}
+                  />
+                ))}
+              </div>
+            ) : (
+              <div className="non-contributable-hero">
+                <img className="fading" src="/img/fading.svg" alt="Fading" />
+                <img
+                  className="waves"
+                  src="/img/home-waves/speak.svg"
+                  alt="Waves"
+                />
+              </div>
+            )
+          }
+        />
 
         <div className="text">
           <div className="inner">
@@ -94,10 +119,41 @@ export default class HomePage extends React.Component<{}, State> {
         <div className="mars">
           <img src="/img/mars.svg" alt="Mars" />
           <div className="cta">
-            <RecordLink />
-            <Localized id="ready-to-record">
-              <h1 />
-            </Localized>
+            <ContributableLocaleLock
+              render={({ isContributable }: any) =>
+                isContributable ? (
+                  <React.Fragment>
+                    <RecordLink />
+                    <Localized id="ready-to-record">
+                      <h1 />
+                    </Localized>
+                  </React.Fragment>
+                ) : (
+                  <React.Fragment>
+                    <Localized id="request-language-text">
+                      <h1 />
+                    </Localized>
+                    <div style={{ width: '100%' }} />
+                    <Localized id="request-language-button">
+                      <button
+                        type="button"
+                        className="request-language"
+                        onClick={() =>
+                          this.setState({ showRequestLanguageModal: true })
+                        }
+                      />
+                    </Localized>
+                    {showRequestLanguageModal && (
+                      <RequestLanguageModal
+                        onRequestClose={() =>
+                          this.setState({ showRequestLanguageModal: false })
+                        }
+                      />
+                    )}
+                  </React.Fragment>
+                )
+              }
+            />
           </div>
         </div>
       </div>
