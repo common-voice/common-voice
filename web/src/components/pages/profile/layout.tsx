ae0bc0045bb1cdf87e0652ee6f9b1780ab6ed591
Gregor Weber <gweber@mozilla.com>
2018-09-25 16:43:44 +0200

    redirect users without accounts accessing avatar/preference

--- web/src/components/pages/profile/layout.tsx
+++ web/src/components/pages/profile/layout.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
-import { Route, Switch } from 'react-router';
+import { connect } from 'react-redux';
+import { Redirect, Route, Switch } from 'react-router';
 import { NavLink } from 'react-router-dom';
+import { User } from '../../../stores/user';
+import StateTree from '../../../stores/tree';
 import URLS from '../../../urls';
 import { localeConnector, LocalePropsFromState } from '../../locale-helpers';
 import { CameraIcon, ToggleIcon, UserIcon } from '../../ui/icons';
@@ -8,7 +11,13 @@ import InfoPage from './info/info';
 
 import './layout.css';
 
-export default localeConnector(({ toLocaleRoute }: LocalePropsFromState) => {
+interface PropsFromState {
+  user: User.State;
+}
+
+interface Props extends LocalePropsFromState, PropsFromState {}
+
+const Layout = ({ toLocaleRoute, user }: Props) => {
   const infoRoute = toLocaleRoute(URLS.PROFILE_INFO);
   const avatarRoute = toLocaleRoute(URLS.PROFILE_AVATAR);
   const prefRoute = toLocaleRoute(URLS.PROFILE_PREFERENCES);
@@ -33,10 +42,25 @@ export default localeConnector(({ toLocaleRoute }: LocalePropsFromState) => {
       <div className="content">
         <Switch>
           <Route exact path={infoRoute} component={InfoPage} />
-          <Route exact path={avatarRoute} />
-          <Route exact path={prefRoute} />
+          <Route
+            exact
+            path={avatarRoute}
+            render={props =>
+              user.account ? null : <Redirect to={infoRoute} />
+            }
+          />
+          <Route
+            exact
+            path={prefRoute}
+            render={props =>
+              user.account ? null : <Redirect to={infoRoute} />
+            }
+          />
         </Switch>
       </div>
     </div>
   );
-});
+};
+export default connect<PropsFromState>(({ user }: StateTree) => ({ user }))(
+  localeConnector(Layout)
+);
