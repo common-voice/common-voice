5e40466d845ff41da38e00c00e70495c7768a2ea
Gregor Weber <gweber@mozilla.com>
2018-11-15 15:55:14 +0100

    show browser error when nothing's loaded after 5 seconds

--- server/src/server.ts
+++ server/src/server.ts
@@ -21,11 +21,11 @@ const FULL_CLIENT_PATH = path.join(__dirname, '..', '..', 'web');
 
 const CSP_HEADER = [
   `default-src 'none'`,
-  `style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://optimize.google.com https://fonts.googleapis.com 'unsafe-inline' https://optimize.google.com`,
+  `style-src 'self' https://fonts.googleapis.com https://optimize.google.com 'unsafe-inline'`,
   `img-src 'self' www.google-analytics.com www.gstatic.com https://optimize.google.com https://www.gstatic.com https://gravatar.com data:`,
   `media-src data: blob: https://*.amazonaws.com https://*.amazon.com`,
   // Note: we allow unsafe-eval locally for certain webpack functionality.
-  `script-src 'self' 'unsafe-eval' 'sha256-a3JWJigb4heryKXgeCs/ZhQEaNkHypiyApGw7hQMdTA=' 'sha256-CwRubg9crsF8jHlnzlIggcJhxGbh5OW22+liQqQNE18=' 'sha256-KkfRSrCB8bso9HIC5wm/5cCYUmNSRWNQqyPbvopRCz4=' https://www.google-analytics.com https://pontoon.mozilla.org https://optimize.google.com`,
+  `script-src 'self' 'unsafe-eval' 'sha256-a3JWJigb4heryKXgeCs/ZhQEaNkHypiyApGw7hQMdTA=' 'sha256-CwRubg9crsF8jHlnzlIggcJhxGbh5OW22+liQqQNE18=' 'sha256-KkfRSrCB8bso9HIC5wm/5cCYUmNSRWNQqyPbvopRCz4=' https://www.google-analytics.com https://pontoon.mozilla.org https://optimize.google.com 'sha256-fhx5fok4Jf0thg+q+QgWRyxhAwG/gzCRpxLP4qTdY1I='`,
   `font-src 'self' https://fonts.gstatic.com`,
   `connect-src 'self' https://pontoon.mozilla.org/graphql https://*.amazonaws.com https://*.amazon.com https://www.gstatic.com https://www.google-analytics.com`,
   `frame-src https://optimize.google.com`,
