37675ff26c51c34fa5d9a09e11ffcc7f8ce062ef
Gregor Weber <gweber@mozilla.com>
2018-11-29 18:28:06 +0100

    stop using consul as cache

--- server/src/lib/lazy-cache.ts
+++ server/src/lib/lazy-cache.ts
@@ -1,8 +1,3 @@
-const consul = require('consul')({ promisify: true });
-import { getConfig } from '../config-helper';
-
-const { ENVIRONMENT } = getConfig();
-
 type Fn<T, S> = (...args: S[]) => Promise<T>;
 
 function isExpired(at: number, timeMs: number) {
@@ -41,55 +36,10 @@ function memoryCache<T, S>(f: Fn<T, S>, timeMs: number): Fn<T, S> {
   };
 }
 
-function consulCache<T, S>(
-  cacheKey: string,
-  f: Fn<T, S>,
-  timeMs: number
-): Fn<T, S> {
-  return async (...args) => {
-    const key = cacheKey + JSON.stringify(args);
-    const result = await consul.kv.get(key);
-
-    let value: any;
-    let renewCache = true;
-    if (result && result.Value) {
-      const cached = JSON.parse(result.Value);
-      value = cached.value;
-      renewCache = isExpired(cached.at, timeMs);
-    }
-
-    if (!renewCache) return value;
-
-    return new Promise(resolve => {
-      const lock = consul.lock({ key: key + '-lock' });
-      lock.on('acquire', async () => {
-        const result = await consul.kv.get(key);
-        if (result && result.Value) {
-          const cached = JSON.parse(result.Value);
-          renewCache = isExpired(cached.at, timeMs);
-          resolve(cached.value);
-        }
-
-        if (renewCache) {
-          value = await f(...args);
-          await consul.kv.set(key, JSON.stringify({ at: Date.now(), value }));
-          resolve(value);
-        }
-
-        await lock.release();
-      });
-
-      lock.acquire();
-    });
-  };
-}
-
 export default function lazyCache<T, S>(
   cacheKey: string,
   f: Fn<T, S>,
   timeMs: number
 ): Fn<T, S> {
-  return !ENVIRONMENT || ENVIRONMENT === 'default'
-    ? memoryCache(f, timeMs)
-    : consulCache(cacheKey, f, timeMs);
+  return memoryCache(f, timeMs);
 }
