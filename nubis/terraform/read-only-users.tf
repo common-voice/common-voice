83e5e212de10bc8350b94dee1079e10277db9965
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-12-17 15:30:42 -0500

    create individual user

--- nubis/terraform/read-only-users.tf
+++ nubis/terraform/read-only-users.tf
@@ -1,16 +1,17 @@
 resource "aws_iam_user" "clips_readonly" {
-  name = "${var.service_name}-${var.environment}-clips_readonly"
-  path = "/applicaton/${var.service_name}/"
+  count = "${var.read_only_public_users}"
+  name  = "${var.service_name}-${var.environment}-clips-readonly-${format("%3d", count.index)}"
+  path  = "/applicaton/${var.service_name}/"
 }
 
 resource "aws_iam_access_key" "clips_readonly" {
   count = "${var.read_only_public_users}"
-  user  = "${aws_iam_user.clips_readonly.name}"
+  user  = "${element(aws_iam_user.clips_readonly.*.name, count.index)}"
 }
 
 resource "aws_iam_user_policy" "clips_readonly" {
   name = "clips-readonly-access"
-  user = "${aws_iam_user.clips_readonly.name}"
+  user = "${element(aws_iam_user.clips_readonly.*.name, count.index)}"
 
   policy = "${data.aws_iam_policy_document.clips_readonly.json}"
 }
