2051c9c4a64afa2b0062ed4835dcafc811282fb3
mritunjaygoutam12 <mritunjaygoutam2204@gmail.com>
2019-03-07 18:03:22 +0530

    logo url with import

--- web/src/components/layout/logo.tsx
+++ web/src/components/layout/logo.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { LocaleLink } from '../locale-helpers';
+import * as logo from '../../../img/cv-logo-one-color-white.svg';
+import * as logoBw from '../../../img/cv-logo-bw.svg';
 
 export default (props: { reverse?: boolean }) => {
-  const imgSrc = props.reverse
-    ? '~img/cv-logo-one-color-white.svg'
-    : '~img/cv-logo-bw.svg';
+  const imgSrc = props.reverse ? logo : logoBw;
 
   return (
     <LocaleLink className="main-logo" to="">
