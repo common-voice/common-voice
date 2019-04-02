f8b85034a669cec51a1ebea593d16fd0f39c4d75
mritunjaygoutam12 <mritunjaygoutam2204@gmail.com>
2019-04-03 03:40:30 +0530

    saveclip at backend error handling

--- web/src/components/pages/contribution/speak/speak.tsx
+++ web/src/components/pages/contribution/speak/speak.tsx
@@ -391,14 +391,17 @@ class SpeakPage extends React.Component<Props, State> {
               tallyRecording();
             }
             retries = 0;
-          } catch (e) {
-            console.error(e);
+          } catch (error) {
+            let msg;
+            if (error.status >= 400) {
+              msg =
+                'Upload of this clip keeps failing at server, reload the page or try after sometime';
+            } else {
+              msg = 'Upload of this clip keeps failing, keep retrying?m';
+            }
             retries--;
             await new Promise(resolve => setTimeout(resolve, 1000));
-            if (
-              retries == 0 &&
-              confirm('Upload of this clip keeps failing, keep retrying?')
-            ) {
+            if (retries == 0 && confirm(msg)) {
               retries = 3;
             }
           }
