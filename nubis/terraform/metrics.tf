f26bc92a66969385b43e58f640b4f470f346c2ac
The-smooth-operator <alberto.delbarrio.albelda@gmail.com>
2019-05-27 18:32:46 +0200

    Fix broken Terraform code

--- nubis/terraform/metrics.tf
+++ nubis/terraform/metrics.tf
@@ -3,7 +3,25 @@
 resource "aws_iam_role" "cloudwatch_fetch_metrics" {
   name               = "cloudwatch_fetch_metrics"
   path               = "/itsre/"
-  assume_role_policy = "${data.aws_iam_policy_document.allow_fetch_cloudwatch_metrics.json}"
+  assume_role_policy = "${data.aws_iam_policy_document.allow_assume_role.json}"
+}
+
+data "aws_iam_policy_document" "allow_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${var.metrics_user}"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "allow_fetch_cloudwatch_metrics" {
+  name   = "allow_fetch_cloudwatch_metrics"
+  role   = "${aws_iam_role.cloudwatch_fetch_metrics.id}"
+  policy = "${data.aws_iam_policy_document.allow_fetch_cloudwatch_metrics.json}"
 }
 
 data "aws_iam_policy_document" "allow_fetch_cloudwatch_metrics" {
@@ -17,16 +35,6 @@ data "aws_iam_policy_document" "allow_fetch_cloudwatch_metrics" {
 
     effect    = "Allow"
     resources = ["*"]
-  }
-
-  statement {
-    actions = ["sts:AssumeRole"]
-    effect  = "Allow"
-
-    principals {
-      type        = "AWS"
-      identifiers = ["${var.metrics_user}"]
-    }
   }
 }
 
