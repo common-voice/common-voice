ee0118439cc32b4311f4162c2e4b8d90971db73c
The-smooth-operator <alberto.delbarrio.albelda@gmail.com>
2019-05-28 10:29:35 +0200

    Create one role per environment

--- nubis/terraform/metrics.tf
+++ nubis/terraform/metrics.tf
@@ -1,7 +1,7 @@
 # Allows mozilla-itsre account to consume Cloudwatch metrics
 
 resource "aws_iam_role" "cloudwatch_fetch_metrics" {
-  name               = "cloudwatch_fetch_metrics"
+  name               = "cloudwatch_fetch_metrics-${var.environment}"
   path               = "/itsre/"
   assume_role_policy = "${data.aws_iam_policy_document.allow_assume_role.json}"
 }
@@ -19,7 +19,7 @@ data "aws_iam_policy_document" "allow_assume_role" {
 }
 
 resource "aws_iam_role_policy" "allow_fetch_cloudwatch_metrics" {
-  name   = "allow_fetch_cloudwatch_metrics"
+  name   = "allow_fetch_cloudwatch_metrics-${var.environment}"
   role   = "${aws_iam_role.cloudwatch_fetch_metrics.id}"
   policy = "${data.aws_iam_policy_document.allow_fetch_cloudwatch_metrics.json}"
 }
