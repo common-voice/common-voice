cd12039ce23ab3cba2251dab09cd4adb4757a173
mritunjaygoutam12 <mritunjaygoutam2204@gmail.com>
2019-07-29 16:36:19 +0530

    minor change

--- web/src/components/pages/profile/avatar-setup/avatar-setup.tsx
+++ web/src/components/pages/profile/avatar-setup/avatar-setup.tsx
@@ -28,7 +28,6 @@ const Lottie = lazy(() => import('react-lottie'));
 const animationData = require('../../../layout/data.json');
 
 import './avatar-setup.css';
-import { string, object } from 'prop-types';
 
 function resizeImage(file: File, maxSize: number): Promise<Blob> {
   const reader = new FileReader();
