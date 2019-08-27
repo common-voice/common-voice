769ea8cb9762410d33d8f6882da23fe548ec9f66
Philippe M. Chiasson <gozer@ectoplasm.org>
2019-08-27 13:57:15 -0400

    add ES cluster

---
+++ nubis/terraform/elasticsearch.tf
@@ -0,0 +1,26 @@
+module "info" {
+  source      = "github.com/nubisproject/nubis-terraform//info?ref=v2.3.1"
+  region      = "${var.region}"
+  environment = "${var.environment}"
+  account     = "${var.account}"
+}
+
+resource "aws_iam_service_linked_role" "es" {
+  aws_service_name = "es.amazonaws.com"
+}
+
+resource "aws_security_group" "es" {
+  name        = "${var.service_name}-${var.environment}-es"
+  description = "Managed by Terraform"
+  vpc_id      = "${module.info.vpc_id}"
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "${split(module.info.private_network_cidr, ",")}",
+    ]
+  }
+}
