f55c33e4c5262ef777d338fb9948f17cc030842e
Gregor Weber <gweber@mozilla.com>
2019-09-16 13:51:02 +0200

    Add migration for adding auth_token to user_clients

---
+++ server/src/lib/model/db/migrations/20190916114856-add-auth-token-to-user-clients.ts
@@ -0,0 +1,11 @@
+export const up = async function(db: any): Promise<any> {
+  return db.runSql(
+    `
+      ALTER TABLE user_clients ADD COLUMN auth_token TEXT;
+    `
+  );
+};
+
+export const down = function(): Promise<any> {
+  return null;
+};
