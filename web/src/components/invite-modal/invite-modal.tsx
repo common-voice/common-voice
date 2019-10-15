1a723151d5125a69d48ae2ce60da4bc4d88a222b
Riley Shaw <rileyjshaw@gmail.com>
2019-10-15 12:46:22 -0700

    Create invite modal for the Open Voice Challenge
    NOTE: This component still needs to be hooked up to the dashboard page.
    
    Reference: https://jira.mozilla.com/secure/RapidBoard.jspa?rapidView=235&view=detail&selectedIssue=OI-1176&quickFilter=762
    
    Test plan:
    
    - Add the following to `layout.tsx`:
    
    ```
    // Up top:
    import InviteModal from '../invite-modal/invite-modal';
    
    // ...
    // In render():
    <InviteModal
    	inviteId="replace-this"
    	onRequestClose={() => {
    	this.setState({});
    	}}
    	teamId="SAP"
    />
    ```
    - Load localhost:9000
    
    Invite modal should pop up.
    
    - Click "Copy link"
    
    Text should change to "Copied".
    A unique invite code should be copied to your clipboard.
    
    - Wait 2 seconds
    
    Button text should revert to "Copy link"

---
+++ web/src/components/invite-modal/invite-modal.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import { useEffect, useRef, useState } from 'react';
+import BalanceText from 'react-balance-text';
+import Modal, { ModalProps } from '../modal/modal';
+import { FontIcon } from '../ui/icons';
+import { Button } from '../ui/ui';
+
+import './invite-modal.css';
+
+export interface InviteModalProps extends ModalProps {
+  inviteId: string;
+  teamId: string;
+}
+
+export default ({ inviteId, teamId, ...props }: InviteModalProps) => {
+  const [copiedRecently, setCopiedRecently] = useState<boolean>(false);
+  const inputRef = useRef();
+  // Reset the button's copied state after a brief timeout.
+  useEffect(() => {
+    if (copiedRecently) {
+      const timer = setTimeout(() => {
+        setCopiedRecently(false);
+      }, 2000);
+      return () => clearTimeout(timer);
+    }
+  }, [copiedRecently]);
+
+  return (
+    <Modal {...props} innerClassName="invite-modal">
+      <img alt="" src={require('./images/mail.svg')} role="presentation" />
+
+      <h1>
+        <BalanceText>
+          Invite colleagues and friends to join your challenge team
+        </BalanceText>
+      </h1>
+
+      <input
+        readOnly
+        ref={inputRef}
+        type="text"
+        value={`https://voice.mozilla.org/?ovchall=1&challenge_team=${teamId}&invite_id=${inviteId}`}
+      />
+
+      <Button
+        rounded
+        onClick={() => {
+          if (inputRef.current) {
+            (inputRef.current as HTMLInputElement).select();
+            document.execCommand('copy');
+            setCopiedRecently(true);
+          }
+        }}>
+        {copiedRecently ? 'Copied' : 'Copy link'}
+        <FontIcon type="link" className="icon" />
+      </Button>
+    </Modal>
+  );
+};
