76e367afc5f986df38a3209f667601acbdb66d16
Riley Shaw <rileyjshaw@gmail.com>
2019-10-18 17:43:05 -0700

    Fix SQL syntax error
    ```
    web        | [BE]   cause:
    web        | [BE]    { Error: ER_PARSE_ERROR: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '(''),
    web        | [BE]       logo_url TEXT NOT NULL DEFAULT (''),
    web        | [BE]       created_at DATETIME DEFAU' at line 3
    ```

--- server/src/lib/model/db/migrations/20191017134955-add-teams.ts
+++ server/src/lib/model/db/migrations/20191017134955-add-teams.ts
@@ -3,8 +3,8 @@ export const up = async function(db: any): Promise<any> {
     `
     CREATE TABLE teams (
       team_name VARCHAR(255) PRIMARY KEY,
-      invite_token TEXT DEFAULT (''),
-      logo_url TEXT NOT NULL DEFAULT (''),
+      invite_token TEXT DEFAULT '',
+      logo_url TEXT NOT NULL DEFAULT '',
       created_at DATETIME DEFAULT now()
     );
     `
