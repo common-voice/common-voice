9249bcd20a1adb6fd2efd358c0aeff0469c2880e
Riley Shaw <rileyjshaw@gmail.com>
2019-11-12 16:30:45 -0500

    Simplify date spoofing
    re: https://github.com/mozilla/voice-web/pull/2295

--- web/src/components/pages/dashboard/challenge/constants.ts
+++ web/src/components/pages/dashboard/challenge/constants.ts
@@ -1,5 +1,4 @@
 import { ChallengeDuration, ChallengeTeamToken } from 'common/challenge';
-const queryString = require('query-string');
 
 export const challengeLogos: {
   [key in ChallengeTeamToken]: {
@@ -17,41 +16,28 @@ export const challengeLogos: {
   sap: { background: '#fff', border: true, url: require('./images/sap.svg') },
 };
 
-export const isChallengeLive = (challenge: ChallengeDuration) => {
-  const now = new Date();
-  return now >= challenge.start && now < challenge.end;
-};
-
-export const isBeforeChallenge = (challenge: ChallengeDuration) => {
-  const now = new Date();
-  return now < challenge.start;
-};
-
 const isValidDate = (dateObj: any) => {
   return dateObj instanceof Date && !isNaN(dateObj.getTime());
 };
 
-const getChallengeDates = (): ChallengeDuration => {
-  const params = queryString.parse(location.search);
-
-  let defaultDates: ChallengeDuration = {
-    start: new Date('2019-11-18'),
-    end: new Date('2019-12-18'),
-  };
-
-  let challengeDates: ChallengeDuration = defaultDates;
-
-  if (params.challenge_start && isValidDate(new Date(params.challenge_start))) {
-    challengeDates.start = new Date(params.challenge_start);
-  }
-
-  if (params.challenge_end && isValidDate(new Date(params.challenge_end))) {
-    challengeDates.end = new Date(params.challenge_end);
-  }
-
-  return challengeDates.end > challengeDates.start
-    ? challengeDates
-    : defaultDates;
+const getNow = () => {
+  const [, date] = location.search
+    ? location.search.match(/date=(\d+-\d+-\d+)/)
+    : [];
+  return isValidDate(date) ? new Date(date) : new Date();
 };
 
-export const pilotDates: ChallengeDuration = getChallengeDates();
+export const isChallengeLive = (challenge: ChallengeDuration) => {
+  const now = getNow();
+  return now >= challenge.start && now < challenge.end;
+};
+
+export const isBeforeChallenge = (challenge: ChallengeDuration) => {
+  const now = getNow();
+  return now < challenge.start;
+};
+
+export const pilotDates: ChallengeDuration = {
+  start: new Date('2019-11-18'),
+  end: new Date('2019-12-18'),
+};
