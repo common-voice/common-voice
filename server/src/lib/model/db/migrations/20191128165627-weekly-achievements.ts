c450bae71841930d6c8c47b8144bbab547fd99f2
Jenny Zhang <jennyzhang@mozilla.com>
2019-11-28 22:14:58 -0500

    Add column to achievements table to specify week, alter queries

---
+++ server/src/lib/model/db/migrations/20191128165627-weekly-achievements.ts
@@ -0,0 +1,14 @@
+export const up = async function(db: any): Promise<any> {
+  return db.runSql(`
+    ALTER TABLE achievements ADD COLUMN week SMALLINT DEFAULT NULL;
+
+    UPDATE achievements SET week = 1 WHERE name LIKE 'challenge_engagement_rank%' LIMIT 3;
+    UPDATE achievements SET week = 2 WHERE name like 'challenge_social_rank%' LIMIT 3;
+    UPDATE achievements SET week = 3 WHERE name LIKE 'challenge_validated_rank%'
+        OR name LIKE 'challenge_contributed_rank%' LIMIT 6;
+  `);
+};
+
+export const down = function(): Promise<any> {
+  return null;
+};
