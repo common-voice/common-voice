e76a5f3cd540c83b748946852414fc695ec71bc5
John Giannelos <jgiannelos@mozilla.com>
2020-02-13 18:44:55 +0100

    Update config setup to use dotenv

--- server/src/config-helper.ts
+++ server/src/config-helper.ts
@@ -1,5 +1,10 @@
 import * as fs from 'fs';
 import { S3 } from 'aws-sdk';
+import { config } from 'dotenv';
+
+if (process.env.SERVER_CONFIG_PATH) {
+  config({path: process.env.SERVER_CONFIG_PATH})
+}
 
 export type CommonVoiceConfig = {
   VERSION: string;
@@ -34,66 +39,57 @@ export type CommonVoiceConfig = {
   SENTRY_DSN: string;
 };
 
-const DEFAULTS: CommonVoiceConfig = {
-  VERSION: null, // Migration number (e.g. 20171205171637), null = most recent
-  RELEASE_VERSION: process.env.GIT_COMMIT_SHA || null, // release version set by nubis,
-  PROD: false, // Set to true for staging and production.
-  SERVER_PORT: 9000,
-  DB_ROOT_USER: 'root', // For running schema migrations.
-  DB_ROOT_PASS: '',
-  MYSQLUSER: 'voicecommons', // For normal DB interactions.
-  MYSQLPASS: 'voicecommons',
-  MYSQLDBNAME: 'voiceweb',
-  MYSQLHOST: 'localhost',
-  MYSQLPORT: 3306,
-  BUCKET_NAME: 'common-voice-corpus',
-  BUCKET_LOCATION: '',
-  ENVIRONMENT: process.env.ENVIRONMENT || 'default',
-  SECRET: 'TODO: Set a secure SECRET in config.json',
-  ADMIN_EMAILS: null,
-  S3_CONFIG: {
+const castDefault = (value: string): any => value;
+const castBoolean = (value: string): boolean => value === 'true';
+const castInt = (value: string): number => parseInt(value);
+const castJson = (value: string): object => JSON.parse(value);
+const configEntry = (key: string, defaultValue: any, cast = castDefault) => ((process.env[key]) ? cast(process.env[key]) : defaultValue);
+
+const BASE_CONFIG: CommonVoiceConfig = {
+  VERSION: configEntry('CV_VERSION', null), // Migration number (e.g. 20171205171637), null = most recent
+  RELEASE_VERSION: configEntry('GIT_COMMIT_SHA', null), // release version set by nubis,
+  PROD: configEntry('CV_PROD', false, castBoolean), // Set to true for staging and production.
+  SERVER_PORT: configEntry('CV_SERVER_PORT', 9000, castInt),
+  DB_ROOT_USER: configEntry('CV_DB_ROOT_USER', 'root'), // For running schema migrations.
+  DB_ROOT_PASS: configEntry('CV_DB_ROOT_PASS', ''),
+  MYSQLUSER: configEntry('CV_MYSQLUSER', 'voicecommons'), // For normal DB interactions.
+  MYSQLPASS: configEntry('CV_MYSQLPASS', 'voicecommons'),
+  MYSQLDBNAME: configEntry('CV_MYSQLDBNAME', 'voiceweb'),
+  MYSQLHOST: configEntry('CV_MYSQLHOST', 'localhost'),
+  MYSQLPORT: configEntry('CV_MYSQLPORT', 3306, castInt),
+  BUCKET_NAME: configEntry('CV_BUCKET_NAME', 'common-voice-corpus'),
+  BUCKET_LOCATION: configEntry('CV_BUCKET_LOCATION', ''),
+  ENVIRONMENT: configEntry('ENVIRONMENT', 'default'),
+  SECRET: configEntry('CV_SECRET', 'super-secure-secret'),
+  ADMIN_EMAILS: configEntry('CV_ADMIN_EMAILS', null),
+  S3_CONFIG: configEntry('CV_S3_CONFIG', {
     signatureVersion: 'v4',
     useDualstack: true,
-  },
+  }, castJson),
   AUTH0: {
-    DOMAIN: '',
-    CLIENT_ID: '',
-    CLIENT_SECRET: '',
+    DOMAIN: configEntry('CV_AUTH0_DOMAIN', ''),
+    CLIENT_ID: configEntry('CV_AUTH0_CLIENT_ID', ''),
+    CLIENT_SECRET: configEntry('CV_AUTH0_CLIENT_SECRET', ''),
   },
-  IMPORT_SENTENCES: true,
-  REDIS_URL: null,
-  KIBANA_URL: null,
-  KIBANA_PREFIX: '/_plugin/kibana',
-  KIBANA_ADMINS: null,
-  LAST_DATASET: '2019-06-12',
-  SENTRY_DSN: '',
+  IMPORT_SENTENCES: configEntry('CV_IMPORT_SENTENCES', true, castBoolean),
+  REDIS_URL: configEntry('CV_REDIS_URL', null),
+  KIBANA_URL: configEntry('CV_KIBANA_URL', null),
+  KIBANA_PREFIX: configEntry('CV_KIBANA_PREFIX', '/_plugin/kibana'),
+  KIBANA_ADMINS: configEntry('CV_KIBANA_ADMINS', null),
+  LAST_DATASET: configEntry('CV_LAST_DATASET', '2019-06-12'),
+  SENTRY_DSN: configEntry('CV_SENTRY_DSN', '')
 };
 
 let injectedConfig: CommonVoiceConfig;
 
 export function injectConfig(config: any) {
-  injectedConfig = { ...DEFAULTS, ...config };
+  injectedConfig = { ...BASE_CONFIG, ...config };
 }
-
-let loadedConfig: CommonVoiceConfig;
 
 export function getConfig(): CommonVoiceConfig {
   if (injectedConfig) {
     return injectedConfig;
   }
 
-  if (loadedConfig) {
-    return loadedConfig;
-  }
-
-  let config = null;
-  try {
-    let config_path = process.env.SERVER_CONFIG_PATH || './config.json';
-    config = JSON.parse(fs.readFileSync(config_path, 'utf-8'));
-  } catch (err) {
-    console.error(err, 'could not load config.json, using defaults');
-  }
-  loadedConfig = { ...DEFAULTS, ...config };
-
-  return loadedConfig;
+  return BASE_CONFIG
 }
