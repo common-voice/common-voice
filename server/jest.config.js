39ff4f4e3ee83c29e8052afbedbc3f59e4e4d8e6
Riley Shaw <rileyjshaw@gmail.com>
2020-02-28 17:20:34 -0800

    Fix tests
    - Add moduleNameWrapper for Jest tests
    - Use the full `ts-jest` preset
    - Remove unneeded `console.log` that was causing local tests to fail
    - Update testing deps
    - Ensure /common is built before running tests

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.tsx?$': '<rootDir>/../node_modules/ts-jest/preprocessor.js',
+  moduleNameMapper: {
+    '^common$': '<rootDir>/../common/index.ts',
   },
+  moduleDirectories: ['node_modules', '<rootDir>/../node_modules'],
+  testEnvironment: 'node',
+  preset: 'ts-jest',
   testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
 };
