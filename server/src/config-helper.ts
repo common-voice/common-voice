ad92dc8d686d4f672c7bdb0d4edd7f1817c8b85e
Jenny Zhang <jennyzhang@mozilla.com>
2020-03-26 17:51:17 -0400

    Trim down error message for K8s environment attempting to load config.json

--- server/src/config-helper.ts
+++ server/src/config-helper.ts
@@ -160,7 +160,7 @@ export function getConfig(): CommonVoiceConfig {
     let config_path = process.env.SERVER_CONFIG_PATH || './config.json';
     fileConfig = JSON.parse(fs.readFileSync(config_path, 'utf-8'));
   } catch (err) {
-    console.error(err, 'could not load config.json, using defaults');
+    console.error(`Could not load config.json, using defaults (error message: ${err.message})`);
   }
   loadedConfig = { ...BASE_CONFIG, ...loadedSecrets, ...fileConfig };
 
