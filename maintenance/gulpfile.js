75004b31e84ff2c935d16c0df410df8582a01cd8
Jenny Zhang <jennyzhang@mozilla.com>
2020-04-01 16:57:31 -0400

    Fix indentations

--- maintenance/gulpfile.js
+++ maintenance/gulpfile.js
@@ -4,43 +4,43 @@ const inject = require('gulp-inject')
 const fs = require('fs');
 
 function setup(cb) {
-	try {
-		fs.copyFileSync("../web/src/components/vars.css", "css/vars.css", (err) => {
-	    if (err) throw err;
-		});
+  try {
+    fs.copyFileSync("../web/src/components/vars.css", "css/vars.css", (err) => {
+      if (err) throw err;
+    });
 
-		fs.copyFileSync("../web/index_template.html", "index.html", (err) => {
-	    if (err) throw err;
-		});
+    fs.copyFileSync("../web/index_template.html", "index.html", (err) => {
+      if (err) throw err;
+    });
 
-		replace({
-			files: 'index.html',
-			from: '<div id="root"></div>',
-			to: '<!-- inject:html --><!-- endinject -->'
-		}).then(res => {
-			console.log("index_template.html and vars.css copied to maintenance folder");
-			cb();
-		}).catch(err => {
-			throw err;
-		});
-	} catch(e) {
-		console.log("Error encountered when attempting to copy templates from /web: ", e.message);
-		cb();
-	}
+    replace({
+      files: 'index.html',
+      from: '<div id="root"></div>',
+      to: '<!-- inject:html --><!-- endinject -->'
+    }).then(res => {
+      console.log("index_template.html and vars.css copied to maintenance folder");
+      cb();
+    }).catch(err => {
+      throw err;
+    });
+  } catch(e) {
+    console.log("Error encountered when attempting to copy templates from /web: ", e.message);
+    cb();
+  }
 }
 
 function compileHtml(cb) {
-	src('./index.html')
-		.pipe(inject(src('maintenance.html'), {
-			transform: (filepath, file) => file.contents.toString()
-		}))
-		.pipe(dest('.'));
-	cb();
+  src('./index.html')
+    .pipe(inject(src('maintenance.html'), {
+      transform: (filepath, file) => file.contents.toString()
+    }))
+    .pipe(dest('.'));
+  cb();
 }
 
 function dev(cb) {
-	watch('maintenance.html', compileHtml);
-	cb();
+  watch('maintenance.html', compileHtml);
+  cb();
 }
 
 exports.dev = series(setup, compileHtml, dev);
