047c1b604daf604674dfae51f82b0c0a70196c29
JZ <jennyzhang@mozilla.com>
2020-04-16 22:17:10 -0400

    Improve activity queries (#2683)
    * Improve activity queries
    
    * add proper label for data

--- server/src/lib/model/db.ts
+++ server/src/lib/model/db.ts
@@ -437,18 +437,22 @@ export default class DB {
   async getVoicesStats(
     locale?: string
   ): Promise<{ date: string; value: number }[]> {
+    // It's necesary to manually create an array of all of the hours, because otherwise
+    // if a time interval has no contributions, that hour will just get dropped entirely
     const hours = Array.from({ length: 10 }).map((_, i) => i);
 
     const [rows] = await this.mysql.query(
       `
-        SELECT date, COUNT(DISTINCT client_id) AS value
+        SELECT date, COUNT(DISTINCT activity.client_id) AS value
         FROM (
           SELECT (TIMESTAMP(DATE_FORMAT(NOW(), '%Y-%m-%d %H:00')) - INTERVAL hour HOUR) AS date
           FROM (${hours.map(i => `SELECT ${i} AS hour`).join(' UNION ')}) hours
         ) date_alias
-        LEFT JOIN user_client_activities ON created_at BETWEEN date AND (date + INTERVAL 1 HOUR) ${
-          locale ? 'AND locale_id = ?' : ''
-        }
+        LEFT JOIN (
+          SELECT created_at, client_id FROM user_client_activities
+          WHERE created_at > (TIMESTAMP(DATE_FORMAT(NOW(), '%Y-%m-%d %H:00')) - INTERVAL 9 hour)
+          ${locale ? 'AND locale_id = ?' : ''}
+        ) activity ON created_at BETWEEN date AND (date + INTERVAL 1 HOUR)
         GROUP BY date
     `,
       [locale ? await getLocaleId(locale) : '']
@@ -461,48 +465,34 @@ export default class DB {
     locale?: string,
     client_id?: string
   ): Promise<{ date: string; value: number }[]> {
+    // It's necesary to manually create an array of all of the hours, because otherwise
+    // if a time interval has no contributions, that hour will just get dropped entirely
+    const hours = Array.from({ length: 10 }).map((_, i) => i);
+
     const [rows] = await this.mysql.query(
-      `
-    SELECT
-        date,
-        sum(value) AS value
-    FROM
-        ( SELECT
-            count(*) as value,
-            date_format( votes.created_at,
-            '%Y-%m-%d %H:00' ) AS date
-        FROM
-            votes
-        LEFT JOIN
-            clips
-                on clips.id = votes.clip_id
-        WHERE
-            votes.created_at > (
-                NOW() - INTERVAL 10 hour
-            )
+      `SELECT date, count(activity.created_at) AS value
+          FROM (
+            SELECT (TIMESTAMP(DATE_FORMAT(NOW(), '%Y-%m-%d %H:00')) - INTERVAL hour HOUR) AS date
+            FROM (${hours
+              .map(i => `SELECT ${i} AS hour`)
+              .join(' UNION ')}) hours
+          ) date_alias
+          LEFT JOIN (
+            SELECT created_at
+            FROM clips
+            WHERE clips.created_at > (TIMESTAMP(DATE_FORMAT(NOW(), '%Y-%m-%d %H:00')) - INTERVAL 9 hour)
+            ${locale ? 'AND clips.locale_id = :locale_id' : ''}
+            ${client_id ? 'AND clips.client_id = :client_id' : ''}
+
+            UNION
+
+            SELECT created_at
+            FROM votes
+            WHERE votes.created_at > (TIMESTAMP(DATE_FORMAT(NOW(), '%Y-%m-%d %H:00')) - INTERVAL 9 hour)
             ${locale ? 'AND clips.locale_id = :locale_id' : ''}
             ${client_id ? 'AND votes.client_id = :client_id' : ''}
-        GROUP BY
-            HOUR(votes.created_at)
-        UNION
-        SELECT
-            count(*) as value,
-            date_format( clips.created_at,
-            '%Y-%m-%d %H:00' ) as date
-        FROM
-            clips
-        WHERE
-            clips.created_at > (
-                NOW() - INTERVAL 10 hour
-            )
-          ${locale ? 'AND clips.locale_id = :locale_id' : ''}
-          ${client_id ? 'AND clips.client_id = :client_id' : ''}
-        GROUP BY
-            HOUR(clips.created_at)
-    ) as summary
-      GROUP BY date
-      ORDER BY date desc
-      LIMIT 10
+          ) activity ON created_at BETWEEN date AND (date + INTERVAL 1 HOUR)
+          GROUP BY date
     `,
       {
         locale_id: locale ? await getLocaleId(locale) : null,
