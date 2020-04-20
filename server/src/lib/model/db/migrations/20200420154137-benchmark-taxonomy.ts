7e4306e0f885eb921c4809fcbeb24e9490b50fe8
Jenny Zhang <jennyzhang@mozilla.com>
2020-04-20 16:19:42 -0400

    Add initial benchmark taxonomy

---
+++ server/src/lib/model/db/migrations/20200420154137-benchmark-taxonomy.ts
@@ -0,0 +1,15 @@
+export const up = async function(db: any): Promise<any> {
+  return db.runSql(
+    `
+    INSERT INTO taxonomies (tax_name, type)
+      VALUES ('Content', 'both');
+
+    INSERT INTO taxonomy_terms (taxonomy_id, term_name, user_selectable)
+      VALUES ((SELECT id FROM taxonomies WHERE tax_name = 'Content'), 'Benchmark', TRUE);
+    `
+  );
+};
+
+export const down = function(): Promise<any> {
+  return null;
+};
