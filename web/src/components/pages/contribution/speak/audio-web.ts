VIVEK332IYER/voice-web

Delete the native iOS app
Now that voice recording is supported in mobile Safari, there is no need
to maintain a separate native app for Common Voice.

This commit also removes outdated documentation under the `docs/`
folder.

web/src/components/pages/contribution/speak/audio-web.ts
@@ -1,4 +1,4 @@
-import { isNativeIOS, isIOS } from '../../../../utility';
+import { isIOS } from '../../../../utility';
 
 const AUDIO_TYPE = `audio/${isIOS() ? 'wav' : 'ogg'}; codecs=opus`;
 
@@ -36,10 +36,6 @@ export default class AudioWeb {
   };
 
   constructor() {
-    // Make sure we are in the right context before we allow instantiation.
-    if (isNativeIOS()) {
-      throw new Error('cannot use web audio in iOS app');
-    }
     this.recorderListeners = {
       start: null,
       dataavailable: null,

common-voice/common-voice

web/src/components/pages/contribution/speak/audio-web.ts
@@ -1,4 +1,4 @@
-import { isNativeIOS, getAudioFormat } from '../../../../utility';
+import { getAudioFormat } from '../../../../utility';
 
 interface BlobEvent extends Event {
   data: Blob;
@@ -32,10 +32,6 @@ export default class AudioWeb {
   };
 
   constructor() {
-    // Make sure we are in the right context before we allow instantiation.
-    if (isNativeIOS()) {
-      throw new Error('cannot use web audio in iOS app');
-    }
     this.recorderListeners = {
       start: null,
       dataavailable: null,