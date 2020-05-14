ec90cf3402c0fff577f3fb02489ebfc3c0c31f0f
Jenny Zhang <jennyzhang@mozilla.com>
2020-05-14 19:36:46 -0400

    Remove locale_id from taxonomy_entries - artefact

--- server/src/lib/model/db/migrations/20200507190147-add-constraints-to-taxonomy-entries.ts
+++ server/src/lib/model/db/migrations/20200507190147-add-constraints-to-taxonomy-entries.ts
@@ -4,9 +4,6 @@ export const up = async function (db: any): Promise<any> {
  	  ALTER TABLE taxonomy_entries ADD CONSTRAINT term_sentence_unique UNIQUE KEY (term_id, sentence_id);
     ALTER TABLE taxonomy_entries ADD CONSTRAINT term_clip_unique UNIQUE KEY (term_id, clip_id);
 
-    ALTER TABLE taxonomy_entries ADD COLUMN locale_id INT NOT NULL DEFAULT 1;
-    ALTER TABLE taxonomy_entries ADD CONSTRAINT taxonomy_entries_ibfk_4 FOREIGN KEY (locale_id) REFERENCES locales(id);
-
     ALTER TABLE clips ADD INDEX (is_valid, locale_id);
     ALTER TABLE sentences ADD INDEX (source(50));
     `
