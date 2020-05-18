1d8d6a08398f9642185a7ee7f933c3cd8f988f60
NanaKay007 <jnranikuabe@gmail.com>
2020-05-18 17:08:06 -0400

    basic demo layout

---
+++ web/src/components/layout/demo_layout.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { Switch, Route, withRouter } from 'react-router';
+import { useLocale } from '../locale-helpers';
+import { Spinner } from '../ui/ui';
+
+function DemoLayout() {
+  const [_, toLocaleRoute] = useLocale();
+
+  return (
+    <div>
+      <React.Suspense fallback={<Spinner />}>
+        <Switch>
+          <Route
+            exact
+            path={toLocaleRoute('/demo')}
+            render={() => {
+              return <h1>in get started</h1>;
+            }}
+          />
+          <Route
+            exact
+            path={toLocaleRoute('/demo/language-select')}
+            render={() => {
+              return <h1>in language select</h1>;
+            }}
+          />
+          {/* more routes to be added */}
+          <Route path="*" render={() => <h1>not found</h1>} />
+        </Switch>
+      </React.Suspense>
+    </div>
+  );
+}
+
+export default withRouter(DemoLayout);
