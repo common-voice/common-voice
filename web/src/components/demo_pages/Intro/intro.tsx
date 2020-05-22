d2da59db7a46a4f55bf6938d4a4239f8279c07a0
NanaKay007 <jnranikuabe@gmail.com>
2020-05-22 17:01:29 -0400

    used existing ui components and reduced nesting

--- web/src/components/demo_pages/Intro/intro.tsx
+++ web/src/components/demo_pages/Intro/intro.tsx
@@ -1,45 +1,30 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
-import arrow from './assets/Button/arrow-right.svg';
 import robot from './assets/Red.svg';
-import wave1 from './assets/_1.svg';
-import wave2 from './assets/_2.svg';
-import wave3 from './assets/_3.svg';
 import './intro.css';
-import { useLocale } from '../../locale-helpers';
 import { Localized, withLocalization } from 'fluent-react/compat';
+import { LinkButton } from '../../ui/ui';
+import { ArrowRight } from '../../ui/icons';
+import URLS from '../../../urls';
 
 export default withLocalization(function Intro() {
-  const [_, toLocaleRoute] = useLocale();
   return (
     <div className="container">
-      <div className="img-container">
-        <div className="waves">
-          <div className="layer"></div>
-          <img src={wave1} className="wave-1" alt="wave" />
-          <img src={wave2} className="wave-1" alt="" />
-          <img src={wave3} className="wave-1" alt="wave" />
-        </div>
-        <img src={robot} className="robot" alt="red robot" />
+      <div className="layer"></div>
+      <img src={robot} id="robot" alt="red robot" />
+      <div className="text">
+        <Localized id="welcome">
+          <h1 />
+        </Localized>
+        <Localized id="welcome-subheader">
+          <p />
+        </Localized>
       </div>
-      <div className="text-box">
-        <div className="text">
-          <h1>
-            <Localized id="welcome" />
-          </h1>
-          <p>
-            <Localized id="welcome-subheader" />
-          </p>
-          <Link
-            to={toLocaleRoute('/demo/language-select')}
-            className="btn-get-started">
-            <span>
-              <Localized id="get-started" />
-              <img src={arrow} alt="arrow right" />
-            </span>
-          </Link>
-        </div>
-      </div>
+      <LinkButton rounded to={URLS.DEMO_DATASETS} className="btn-get-started">
+        <Localized id="get-started">
+          <span />
+        </Localized>
+        <ArrowRight id="arrow-right" />
+      </LinkButton>
     </div>
   );
 });
