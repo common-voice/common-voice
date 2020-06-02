031b166c4ba4df6458561b3ee8722145301b2cd7
NanaKay007 <jnranikuabe@gmail.com>
2020-06-02 09:26:18 -0400

    renamed classes to use BEM format

--- web/src/components/demo-pages/Intro/intro.tsx
+++ web/src/components/demo-pages/Intro/intro.tsx
@@ -8,18 +8,21 @@ import URLS from '../../../urls';
 
 export default withLocalization(function Intro() {
   return (
-    <div className="container">
-      <div className="layer"></div>
+    <div id="intro-container">
+      <div id="intro-container--gradient-layer"></div>
       <img src={robot} id="robot" alt="red robot" />
-      <div className="text">
+      <div id="intro-container--text-box">
         <Localized id="demo-welcome">
-          <h1 className="text-header" />
+          <h1 id="intro-container--text-box__text-header" />
         </Localized>
         <Localized id="demo-welcome-subheader">
-          <p className="text-body" />
+          <p id="intro-container--text-box__text-body" />
         </Localized>
       </div>
-      <LinkButton rounded to={URLS.DEMO_DATASETS} className="btn-get-started">
+      <LinkButton
+        rounded
+        to={URLS.DEMO_DATASETS}
+        id="intro-container--btn-get-started">
         <Localized id="demo-get-started">
           <span />
         </Localized>
