88e7b8340ef68e53dc74e92dd3de72e72ffa9a23
NanaKay007 <jnranikuabe@gmail.com>
2020-06-04 18:18:55 -0400

    layout definition for some kiosk pages

---
+++ web/src/components/demo-pages/kiosk/kiosk.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import './kiosk.css';
+import { MicIcon, PlayOutlineGreenIcon } from '../../ui/icons';
+import { useLocation } from 'react-router-dom';
+import { useLocale } from '../../locale-helpers';
+import urls from '../../../urls';
+import stats from '../../pages/datasets/stats';
+// TODO: follow up with Jenny, whether or not to move to icons.tsx
+import grid from './assets/grid.svg';
+import { PageContentType } from './types';
+import { datasets } from './datasets';
+import dashboard from './dashboard';
+
+const BottomRightPane = () => {
+  return (
+    <ul id="kiosk-bottom-right-pane">
+      <li id="pane__micIcon">
+        <MicIcon />
+      </li>
+      <li>
+        <PlayOutlineGreenIcon />
+      </li>
+      <hr id="pane-divider" />
+      <li>
+        {/* TODO:replace with icon equivalent when implemented */}
+        <img src={grid} />
+      </li>
+    </ul>
+  );
+};
+
+function Kiosk() {
+  const PageContentFactory = (): PageContentType => {
+    //returns appropriate page content based on current route url
+    let page: PageContentType;
+    switch (pathname) {
+      case toLocaleRoute(urls.DEMO_DATASETS):
+        page = datasets();
+        break;
+      case toLocaleRoute(urls.DEMO_DASHBOARD):
+        page = dashboard();
+        break;
+      // TODO: add more kiosk routes here
+      default:
+        break;
+    }
+    return {
+      Card: page.Card,
+      Content: page.Content,
+    };
+  };
+
+  const [_, toLocaleRoute] = useLocale();
+  const { pathname } = useLocation();
+  const [PageContent, setPageContent] = React.useState<PageContentType>(
+    PageContentFactory()
+  );
+
+  React.useEffect(() => {
+    setPageContent(PageContentFactory());
+  }, [pathname]);
+
+  return (
+    <div id="kiosk-container">
+      <div id="pattern-bg">
+        <div id="gradient"></div>
+        <div id="mesh"></div>
+      </div>
+      <div id="kiosk-card">
+        <PageContent.Card />
+      </div>
+      <BottomRightPane />
+      <div id="kiosk--content">
+        <PageContent.Content />
+      </div>
+    </div>
+  );
+}
+
+export default Kiosk;
