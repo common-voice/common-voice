0c2634b5fa0bc1ac0e4e90578fdc3fb309e7b9fc
NanaKay007 <jnranikuabe@gmail.com>
2020-07-29 09:28:26 -0400

    scroll function

--- web/src/components/register-section/register-section.tsx
+++ web/src/components/register-section/register-section.tsx
@@ -1,7 +1,7 @@
 import * as cx from 'classnames';
 import { Localized } from '@fluent/react';
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { trackHome } from '../../services/tracker';
 import { BENEFITS, WHATS_PUBLIC } from '../../constants';
 import { useLocale } from '../locale-helpers';
@@ -21,6 +21,38 @@ export default function RegisterSection({
   const [index, setIndex] = useState(0);
   const [tab, setTab] = useState('benefits');
   const isBenefits = tab == 'benefits';
+  const demoMode = marsSrc == null;
+  const ulElement = useRef<HTMLUListElement>(null);
+  var lastScrollTop = 0;
+
+  const handleSroll = () => {
+    let root = ulElement.current;
+    let activeChild = root.children[index];
+    let { top } = activeChild.getBoundingClientRect();
+    var st =
+      root.getBoundingClientRect().top -
+      root.children[0].getBoundingClientRect().top; //used to monitor scroll direction
+    let nextIndex;
+    if (st > lastScrollTop) {
+      // downscroll code
+      nextIndex =
+        index + 1 < (isBenefits ? BENEFITS.length : WHATS_PUBLIC.length)
+          ? index + 1
+          : index;
+      if (top <= 30) {
+        setIndex(nextIndex);
+      }
+    } else {
+      // upscroll code
+      nextIndex = index - 1 >= 0 ? index - 1 : index;
+      let nextActiveChild = root.children[nextIndex];
+      if (nextActiveChild.getBoundingClientRect().top >= 30) {
+        setIndex(nextIndex);
+      }
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
+  };
+
   const info = (
     <div className="signup-info">
       <div className="tabs">
@@ -51,7 +83,7 @@ export default function RegisterSection({
       </div>
       <div className="list-and-bg">
         <div className="bg" />
-        <ul key={tab} onScroll={demoMode ? handleSroll: null} ref={ulElement}>
+        <ul key={tab} onScroll={demoMode ? handleSroll : null} ref={ulElement}>
           {(isBenefits ? BENEFITS : WHATS_PUBLIC).map((l, i) => (
             <li
               key={l}
