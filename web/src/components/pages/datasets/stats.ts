cd384d09dbf8f599f77e71c3091b817cb2b38888
Jenny Zhang <jennyzhang@mozilla.com>
2020-11-16 21:34:45 -0500

    Add old dataset versions for download
    * Add dataset stats files for old datasets
    * Exclude datasets with errors
    * Add version selects
    * Add release date to stats

--- web/src/components/pages/datasets/stats.ts
+++ web/src/components/pages/datasets/stats.ts
@@ -1,4 +1,7 @@
 const releases = [
+  'cv-corpus-1',
+  'cv-corpus-2',
+  'cv-corpus-3',
   'cv-corpus-4-2019-12-10',
   'cv-corpus-5-singleword',
   'cv-corpus-5-2020-06-22',
@@ -6,11 +9,12 @@ const releases = [
   'cv-corpus-5.1-singleword',
 ];
 
-export default releases.reduce(
-  (statsObj: { [key: string]: any }, releaseName: string) => {
+export default releases
+  .reverse()
+  .reduce((statsObj: { [key: string]: any }, releaseName: string) => {
     const { stats } = require(`./releases/${releaseName}`);
-    statsObj[releaseName] = stats;
+    if (!stats.exclude) {
+      statsObj[releaseName] = stats;
+    }
     return statsObj;
-  },
-  {}
-);
+  }, {});
