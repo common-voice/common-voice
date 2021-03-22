8ec3878183e47816b3809982b3e94069f63f32e2
Jenny Zhang <jyzhang@nvidia.com>
2021-03-22 19:13:55 -0400

    Fix S3Proxy race condition and network linkage

--- docker/prepare_s3.sh
+++ docker/prepare_s3.sh
@@ -2,14 +2,14 @@
 
 # Check if voice-web bucket exists if not create it
 echo "Checking if s3 bucket exists..."
-status=$(curl -XGET s3proxy/common-voice-corpus --write-out "%{http_code}\n" --output /dev/null --silent)
+status=$(curl -XGET http://s3proxy/common-voice-corpus --write-out "%{http_code}\n" --output /dev/null --silent)
 
 echo "GET bucket code: $status"
 
 if [ $status != "200" ]
 then
     echo "Bucket does not exist. Creating one..."
-    status=$(curl -XPUT s3proxy/common-voice-corpus --write-out "%{http_code}\n" --output /dev/null --silent)
+    status=$(curl -XPUT http://s3proxy/common-voice-corpus --write-out "%{http_code}\n" --output /dev/null --silent)
     echo "PUT bucket code: $status"
 fi
 
