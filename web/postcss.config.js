daf030c9900f0b7fee2a11c7b2f22656ffd0d06c
JZ <jennyzhang@mozilla.com>
2021-04-09 20:54:52 -0400

    Remove redundant imports (#3084)
    * Remove unneeded vars imports
    
    * Fix postcss settings, move postcss config to separate file

---
+++ web/postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: [
+    'postcss-import',
+    [
+      'postcss-color-mod-function',
+      {
+        importFrom: './src/components/vars.css',
+      },
+    ],
+    'postcss-nested',
+    'postcss-custom-media',
+    'postcss-preset-env',
+    'cssnano',
+  ],
+};
