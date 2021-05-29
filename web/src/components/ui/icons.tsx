9e2c9aa80ee06027ea714f5766bdc45e81a08b61
Alexandre Macabies <web+oss@zopieux.com>
2021-05-29 20:24:30 +0200

    Document the Return key shortcut for submitting clips.
    To accomodate the need for an icon as keyboard key, I've added an
    optional ReactNode that replaces the key if specified.

--- web/src/components/ui/icons.tsx
+++ web/src/components/ui/icons.tsx
@@ -46,6 +46,21 @@ export const ArrowRight = uniqueIcon((id, props) => (
   </svg>
 ));
 
+export const ReturnKeyIcon = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    <polyline points="9 10 4 15 9 20" />
+    <path d="M20 4v7a4 4 0 0 1-4 4H4" />
+  </svg>
+);
+
 export const BarChartIcon = () => (
   <svg width="24" height="24" viewBox="0 0 24 24">
     <defs>
