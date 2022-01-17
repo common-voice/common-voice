6ba913ccd1e0486262c49567c1a26f5262c11681
Zac Colley <zac@clubpenguin.email>
2022-01-17 14:44:03 +0000

    fix(http-status-codes): incorrect import
    doesnt have a default import, we should fix
    this when moving to v2 as that exports a
    StatusCodes object that is recommended at
    that point

--- server/src/server.ts
+++ server/src/server.ts
@@ -22,7 +22,7 @@ import authRouter, { authMiddleware } from './auth-router';
 import fetchLegalDocument from './fetch-legal-document';
 import * as proxy from 'http-proxy-middleware';
 import { createTaskQueues, TaskQueues } from './lib/takeout';
-import HttpStatus from 'http-status-codes';
+const HttpStatus = require('http-status-codes');
 
 require('source-map-support').install();
 const contributableLocales = require('locales/contributable.json');
