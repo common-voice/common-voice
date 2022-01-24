6a2a3ac43d6ed299b3a3f3b30fa473901cb87ec1
Zac Colley <zac@mozillafoundation.org>
2022-01-24 16:51:06 +0000

    fix(accessibility): select box reading out [object Object]
    we shouldn't be using objects in the select box

--- web/src/components/localization-select/localization-select-complex.tsx
+++ web/src/components/localization-select/localization-select-complex.tsx
@@ -8,38 +8,34 @@ import VisuallyHidden from '../visually-hidden/visually-hidden';
 
 import './localization-select.css';
 
-interface LocaleWithName {
-  code: string;
-  name: string;
-}
-
 interface Props {
   locale?: string;
   onLocaleChange: (props: any) => any;
 }
 
+function getLocaleWithName(locale: string) {
+  return LOCALES_WITH_NAMES.find(({ code }) => code === locale);
+}
+
 const LocalizationSelectComplex = ({ locale, onLocaleChange }: Props) => {
-  const initialSelectedItem =
-    LOCALES_WITH_NAMES.find(({ code }) => code === locale) || LOCALES[0];
-  const items = LOCALES_WITH_NAMES;
+  const localWithName = getLocaleWithName(locale);
+  const initialSelectedItem = localWithName ? localWithName.code : LOCALES[0];
+  const items = LOCALES_WITH_NAMES.map(locale => locale.code);
 
   function onSelectedItemChange({ selectedItem }: any) {
-    if (selectedItem.code) {
-      onLocaleChange(selectedItem.code);
+    if (selectedItem) {
+      onLocaleChange(selectedItem);
     }
   }
 
   const {
     isOpen,
-    selectedItem,
     getToggleButtonProps,
     getLabelProps,
     getMenuProps,
     highlightedIndex,
     getItemProps,
   } = useSelect({ items, initialSelectedItem, onSelectedItemChange });
-
-  const selectedLocale = selectedItem as LocaleWithName;
 
   // don't show select if we dont have multiple locales
   if (items.length <= 1) {
@@ -56,11 +52,7 @@ const LocalizationSelectComplex = ({ locale, onLocaleChange }: Props) => {
           <VisuallyHidden>
             <label {...getLabelProps()}>{label}</label>
           </VisuallyHidden>
-          <button
-            aria-label={selectedLocale.name}
-            className="button"
-            type="button"
-            {...getToggleButtonProps()}>
+          <button className="button" type="button" {...getToggleButtonProps()}>
             {locale}
           </button>
           <div className="list-wrapper">
@@ -68,13 +60,13 @@ const LocalizationSelectComplex = ({ locale, onLocaleChange }: Props) => {
             <ul {...getMenuProps()}>
               {items.map((item, index) => (
                 <li
-                  key={item.code}
+                  key={item}
                   className={classNames({
-                    selected: item.code === locale,
+                    selected: item === locale,
                     highlighted: index == highlightedIndex,
                   })}
                   {...getItemProps({ item })}>
-                  {item.name}
+                  {getLocaleWithName(item).name}
                 </li>
               ))}
             </ul>
