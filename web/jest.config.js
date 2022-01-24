70e97c7de0307d91654e45de5a0164caef49f4c1
Zac Colley <zac@mozillafoundation.org>
2022-01-24 16:24:07 +0000

    feat(testing): add basic testing setup for web
    including a localization render helper

---
+++ web/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  moduleNameMapper: {
+    '^common$': '<rootDir>/../common/index.ts',
+  },
+  moduleDirectories: ['node_modules', '<rootDir>/../node_modules'],
+  testEnvironment: 'jsdom',
+  preset: 'ts-jest',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  moduleNameMapper: {
+    '^.+\\.svg$': 'jest-svg-transformer',
+    '.+\\.(css|png|jpg)$': 'jest-transform-stub',
+  },
+  setupFiles: ['<rootDir>/src/test/jest.setup.js'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+    },
+  },
+};
