dbfcf843ee83ae0848db836b05d1da8eed6eeca7
g <gabriel@mozillafoundation.org>
2022-01-25 15:22:08 -0500

    Add migration

--- server/src/lib/model/db/migrations/20220125174611-dataset-8-db-entries.ts
+++ server/src/lib/model/db/migrations/20220125174611-dataset-8-db-entries.ts
@@ -1,9 +1,8 @@
 export const up = async function (db: any): Promise<any> {
   return db.runSql(`
-    INSERT INTO datasets(name, release_dir, multilingual, bundle_date, release_date)
-    	VALUES
-        ('Common Voice Corpus 8.0', 'cv-corpus-8.0-2022-01-19', TRUE, '2022-01-19', '2022-01-26'),
-      ON DUPLICATE KEY UPDATE name=name
+  INSERT IGNORE INTO datasets(name, release_dir, multilingual, bundle_date, release_date)
+  VALUES
+    ('Common Voice Corpus 8.0', 'cv-corpus-8.0-2022-01-19', TRUE, '2022-01-19', '2022-01-26')
   `);
 };
 
