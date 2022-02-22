81c9bb501c788ab80639d26ca29a339300f1cabe
Gabriel <gabriel@mozillafoundation.org>
2022-02-22 12:09:37 -0500

    Add variant data and import it

--- server/src/lib/model/db/migrations/20220222145730-import-variant-data.ts
+++ server/src/lib/model/db/migrations/20220222145730-import-variant-data.ts
@@ -1,72 +1,105 @@
-const VARIANTS = [
-  { locale_name: 'cy', name: 'North-Western Welsh', token: 'cy-northwes' },
-  { locale_name: 'cy', name: 'North-Eastern Welsh', token: 'cy-northeas' },
-  { locale_name: 'cy', name: 'Mid Wales', token: 'cy-midwales' },
-  { locale_name: 'cy', name: 'South-Western Welsh', token: 'cy-southwes' },
-  { locale_name: 'cy', name: 'South-Eastern Welsh', token: 'cy-southeas' },
-  { locale_name: 'cy', name: 'Patagonian Welsh', token: 'cy-wladfa' },
-  { locale_name: 'sw', name: 'Kiswahili Sanifu (EA)', token: 'sw-sanifu' },
+type Variant = {
+  id?: number;
+  variant_name?: string;
+  variant_token: string;
+  locale_name?: string;
+};
+
+const VARIANTS: Variant[] = [
   {
-    locale_name: 'sw',
-    name: 'Kiswahili cha Bara ya Kenya',
-    token: 'sw-barake',
+    locale_name: 'cy',
+    variant_name: 'North-Western Welsh',
+    variant_token: 'cy-northwes',
+  },
+  {
+    locale_name: 'cy',
+    variant_name: 'North-Eastern Welsh',
+    variant_token: 'cy-northeas',
+  },
+  {
+    locale_name: 'cy',
+    variant_name: 'Mid Wales',
+    variant_token: 'cy-midwales',
+  },
+  {
+    locale_name: 'cy',
+    variant_name: 'South-Western Welsh',
+    variant_token: 'cy-southwes',
+  },
+  {
+    locale_name: 'cy',
+    variant_name: 'South-Eastern Welsh',
+    variant_token: 'cy-southeas',
+  },
+  {
+    locale_name: 'cy',
+    variant_name: 'Patagonian Welsh',
+    variant_token: 'cy-wladfa',
   },
   {
     locale_name: 'sw',
-    name: 'Kiswahili cha Bara ya Tanzania',
-    token: 'sw-baratz',
-  },
-  { locale_name: 'sw', name: 'Kingwana (DRC)', token: 'sw-kingwana' },
-  {
-    locale_name: 'sw',
-    name: 'Kimvita (KE) - Central dialect',
-    token: 'sw-kimvita',
+    variant_name: 'Kiswahili Sanifu (EA)',
+    variant_token: 'sw-sanifu',
   },
   {
     locale_name: 'sw',
-    name: 'Kibajuni (KE) - Northern dialect',
-    token: 'sw-kibajuni',
+    variant_name: 'Kiswahili cha Bara ya Kenya',
+    variant_token: 'sw-barake',
   },
   {
     locale_name: 'sw',
-    name: 'Kimrima (TZ) - Northern dialect',
-    token: 'sw-kimrima',
+    variant_name: 'Kiswahili cha Bara ya Tanzania',
+    variant_token: 'sw-baratz',
   },
   {
     locale_name: 'sw',
-    name: 'Kiunguja (TZ) - Southern dialect',
-    token: 'sw-kiunguja',
+    variant_name: 'Kingwana (DRC)',
+    variant_token: 'sw-kingwana',
   },
   {
     locale_name: 'sw',
-    name: 'Kipemba (TZ) - Southern dialect',
-    token: 'sw-kipemba',
+    variant_name: 'Kimvita (KE) - Central dialect',
+    variant_token: 'sw-kimvita',
   },
   {
     locale_name: 'sw',
-    name: 'Kimakunduchi/Kikae (TZ) - Southern dialect',
-    token: 'sw-kikae',
+    variant_name: 'Kibajuni (KE) - Northern dialect',
+    variant_token: 'sw-kibajuni',
   },
-  { locale_name: 'pt', name: 'Portuguese (Brasil)', token: 'pt-BR' },
-  { locale_name: 'pt', name: 'Portuguese (Portugal)', token: 'pt-PT' },
+  {
+    locale_name: 'sw',
+    variant_name: 'Kimrima (TZ) - Northern dialect',
+    variant_token: 'sw-kimrima',
+  },
+  {
+    locale_name: 'sw',
+    variant_name: 'Kiunguja (TZ) - Southern dialect',
+    variant_token: 'sw-kiunguja',
+  },
+  {
+    locale_name: 'sw',
+    variant_name: 'Kipemba (TZ) - Southern dialect',
+    variant_token: 'sw-kipemba',
+  },
+  {
+    locale_name: 'sw',
+    variant_name: 'Kimakunduchi/Kikae (TZ) - Southern dialect',
+    variant_token: 'sw-kikae',
+  },
+  {
+    locale_name: 'pt',
+    variant_name: 'Portuguese (Brasil)',
+    variant_token: 'pt-BR',
+  },
+  {
+    locale_name: 'pt',
+    variant_name: 'Portuguese (Portugal)',
+    variant_token: 'pt-PT',
+  },
 ];
 
 export const up = async function (db: any): Promise<any> {
-  await db.runSql(
-    `
-      CREATE TABLE variants (
-          id int(10) unsigned AUTO_INCREMENT NOT NULL,
-          locale_id int(11) NOT NULL,
-          variant_name varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-          variant_token varchar(255) CHARACTER SET utf8mb4 NULL,
-          type varchar(255) CHARACTER SET utf8mb4 NULL,
-          created_at datetime DEFAULT CURRENT_TIMESTAMP,
-          PRIMARY KEY (id),
-          UNIQUE KEY variant_key (locale_id,variant_name),
-          CONSTRAINT variants_ibfk_1 FOREIGN KEY (locale_id) REFERENCES locales (id)
-      )
-      `
-  );
+  //remove existing test variant data
 
   const languageQuery = await db.runSql(
     `SELECT id, name FROM locales where name is not null`
@@ -81,9 +114,9 @@ export const up = async function (db: any): Promise<any> {
       `INSERT INTO variants (locale_id, variant_token, variant_name) VALUES ( ${
         mappedLanguages[row['locale_name']] +
         ',"' +
-        row['token'] +
+        row['variant_token'] +
         '","' +
-        row['name'] +
+        row['variant_name'] +
         '"'
       })`
     );
@@ -92,7 +125,5 @@ export const up = async function (db: any): Promise<any> {
 
 export const down = async function (db: any): Promise<any> {
   await db.runSql(`
-      DROP TABLE user_client_variants;
-      DROP TABLE variants;
     `);
 };
