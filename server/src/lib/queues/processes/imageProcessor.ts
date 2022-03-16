53482d6cafc0998db8748b0f3b4f8df1976cb8be
Gabriel <gabriel@mozillafoundation.org>
2022-03-16 10:28:51 -0400

    Update return values for endpoints

--- server/src/lib/queues/processes/imageProcessor.ts
+++ server/src/lib/queues/processes/imageProcessor.ts
@@ -35,6 +35,12 @@ const updateAvatarURL = async (
   if (oldAvatar) await deleteAvatar(client_id, oldAvatar, s3);
 };
 
+function PromiseTimeout(delayms: number): Promise<any> {
+  return new Promise(function (resolve, reject) {
+    setTimeout(resolve, delayms);
+  });
+}
+
 const imageProcessor = async (job: Job) => {
   const {
     s3,
@@ -54,6 +60,7 @@ const imageProcessor = async (job: Job) => {
     bucket: any;
   };
   //upload to S3 here
+  await PromiseTimeout(10000);
   try {
     await uploader
       .upload({
