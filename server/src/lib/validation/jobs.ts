d9a6b6200584e4a7096dd9fbcc75e88bffdc6e93
Gabriel <gabriel@mozillafoundation.org>
2022-03-24 10:56:01 -0400

    Update tests

--- server/src/lib/validation/jobs.ts
+++ server/src/lib/validation/jobs.ts
@@ -1,15 +1,13 @@
 import { AllowedSchema } from 'express-json-validator-middleware';
-const STRING_DIGIT = '^[0-9]+$';
 
 export const jobSchema: AllowedSchema = {
   type: 'object',
   required: ['jobId'],
   properties: {
     jobId: {
-      type: 'string',
-      maxLength: 10,
-      minLength: 1,
-      pattern: STRING_DIGIT,
+      format: 'positiveIntegerFormat',
+      type: 'number',
+      minimum: 1,
     },
   },
 };
