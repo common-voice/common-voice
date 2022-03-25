eab283b70793bdeb5c5e4966e247f0f149059fee
Gabriel <gabriel@mozillafoundation.org>
2022-03-25 14:11:55 -0400

    Update pagination logic

--- web/src/components/pages/dashboard/contributions/contributions.tsx
+++ web/src/components/pages/dashboard/contributions/contributions.tsx
@@ -33,41 +33,14 @@ const NoContributionsPage = ({
   </div>
 );
 
-const INTERVAL_LABELS: { [key: number]: string } = {
-  1: 'Daily',
-  7: 'Weekly',
-};
-
-const Wave = () => (
-  <svg className="wave" width="172" height="70" viewBox="0 0 172 70">
-    <defs>
-      <linearGradient id="wave-b" x1="50%" x2="50%" y1="100%" y2="0%">
-        <stop offset="0%" stopColor="#040101" stopOpacity="0" />
-        <stop offset="100%" stopColor="#000" stopOpacity="0.3" />
-      </linearGradient>
-      <path
-        id="wave-a"
-        d="M0 49.237c22.655-18.809 30.755-2.594 58.986-2.594C89.25 46.643 100.896 10 128 10c21 0 26 9 44 16.117V80H0V49.237z"
-      />
-    </defs>
-    <use
-      fill="url(#wave-b)"
-      fillRule="evenodd"
-      opacity=".5"
-      transform="matrix(-1 0 0 1 172 -10)"
-      xlinkHref="#wave-a"
-    />
-  </svg>
-);
-
 export default function ContributionsPage({ dashboardLocale }: Props) {
   const account = useAccount();
   const api = useAPI();
   const [clips, setClips] = useState(null);
   const [metadata, setMetadata] = useState(null);
   const [query, setQuery] = useState({
-    page: 1,
-    count: 5,
+    page: 0,
+    count: 10,
   });
 
   useEffect(() => {
@@ -118,14 +91,15 @@ export default function ContributionsPage({ dashboardLocale }: Props) {
         <div className="table-navigation">
           <p>
             {' '}
-            Displaying {metadata.offset}/{metadata.totalCount} clips{' '}
+            Displaying {query.page + 1}/
+            {Math.ceil(metadata.totalCount / query.count)} pages{' '}
           </p>
-          {metadata.offset > 5 && (
+          {metadata.offset >= query.count && (
             <Button onClick={() => setQuery({ ...query, page: --query.page })}>
               Previous
             </Button>
           )}
-          {metadata.offset < metadata.totalCount && (
+          {metadata.offset < metadata.totalCount - query.count && (
             <Button onClick={() => setQuery({ ...query, page: ++query.page })}>
               Next
             </Button>
