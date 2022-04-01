ea5a796a1a69af9c2d31bf202df80f67442ff472
Gabriel <gabriel@mozillafoundation.org>
2022-04-01 09:23:47 -0400

    Add missing CSP value for dev

--- server/src/csp-header-value.ts
+++ server/src/csp-header-value.ts
@@ -65,6 +65,7 @@ function getCSPHeaderValue() {
     // add s3proxy to allowed sources in development
     SOURCES['connect-src'].push(S3_LOCAL_DEVELOPMENT_ENDPOINT);
     SOURCES['media-src'].push(S3_LOCAL_DEVELOPMENT_ENDPOINT);
+    SOURCES['img-src'].push(S3_LOCAL_DEVELOPMENT_ENDPOINT);
   }
 
   return Object.entries(SOURCES)
