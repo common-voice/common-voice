ee8a6319ec2a0c600d9e560896a839c2bd357bd1
Zac Colley <zac@mozillafoundation.org>
2022-04-04 12:12:53 +0100

    Move to CSS modules for progress bar component

--- web/src/components/progress-bar/progress-bar.tsx
+++ web/src/components/progress-bar/progress-bar.tsx
@@ -1,23 +1,16 @@
 import * as React from 'react';
 
-export default ({
-  children,
-  progress,
-  secondary,
-}: {
-  children?: any;
-  progress: number;
-  secondary?: boolean;
-}) => (
-  <div className="progress-bar">
-    <div
-      className={'progress ' + (secondary ? 'blue' : '')}
-      style={
-        progress > 0
-          ? { width: 100 * progress + '%' }
-          : { width: 0, padding: 0 }
-      }>
-      {children}
-    </div>
+import VisuallyHidden from '../visually-hidden/visually-hidden';
+
+import styles from './progress-bar.module.css';
+
+interface Props {
+  value?: number;
+}
+
+const ProgressBar = ({ value = 0 }: Props) => (
+  <div className={styles.progress}>
+    <VisuallyHidden>{value}%</VisuallyHidden>
+    <div className={styles.progressBar} style={{ width: `${value}%` }} />
   </div>
 );
