832a6fcfe21195f82f36d15c4e323645eb7a84ad
Gabriel <gabriel@mozillafoundation.org>
2022-05-13 12:01:21 -0400

    Update startup scripts

--- server/src/lib/model/db/import-locales.ts
+++ server/src/lib/model/db/import-locales.ts
@@ -2,7 +2,7 @@ import { Language } from 'common';
 import fetch from 'node-fetch';
 import { getMySQLInstance } from './mysql';
 const TRANSLATED_MIN_PROGRESS = 0.75;
-
+const DEFAULT_TARGET_SENTENCE_COUNT = 5000;
 type Locale = {
   code: string;
   direction: string;
@@ -43,7 +43,7 @@ export async function importLocales() {
     console.log('Inserting languages into database');
 
     const [existingLangauges] = await db.query(`
-      SELECT l.name, l.target_sentence_count as target_sentence_count, count(1) as total_sentence_count
+      SELECT l.id, l.name, l.target_sentence_count as target_sentence_count, count(1) as total_sentence_count
       FROM locales l
       JOIN sentences s ON s.locale_id = l.id
       GROUP BY l.id
@@ -67,27 +67,57 @@ export async function importLocales() {
       obj[language.code] = {
         ...language,
         target_sentence_count:
-          allLanguages[language.code]?.target_sentence_count || 5000,
+          allLanguages[language.code]?.target_sentence_count ||
+          DEFAULT_TARGET_SENTENCE_COUNT,
         is_translated: isTranslated ? 1 : 0,
         is_contributable: isTranslated && hasEnoughSentences ? 1 : 0,
       };
       return obj;
     }, {});
+    Promise.all([
+      locales.map(lang => {
+        if (allLanguages[lang.code]) {
+          // this language exists in db, just update
 
-    await db.query(
-      'INSERT IGNORE INTO locales(name, target_sentence_count, native_name, is_contributable, is_translated, text_direction) VALUES ?',
-      [
-        locales.map(l => [
-          l.code,
-          newLanguageData[l.code].target_sentence_count,
-          l.name,
-          newLanguageData[l.code].is_contributable,
-          newLanguageData[l.code].is_translated,
-          l.direction,
-        ]),
-      ]
-    );
+          return db.query(
+            `
+            UPDATE locales
+            SET native_name = ?,
+            target_sentence_count = ?,
+            is_contributable = ?, 
+            is_translated = ?, 
+            text_direction = ?
+            WHERE id = ?
+            `,
+            [
+              lang.name,
+              newLanguageData[lang.code].target_sentence_count,
+              newLanguageData[lang.code].is_contributable,
+              newLanguageData[lang.code].is_translated,
+              lang.direction,
+              allLanguages[lang.code].id,
+            ]
+          );
+        } else {
+          // this is a new language, insert
+          console.log('lang', lang);
 
+          return db.query(
+            `INSERT IGNORE INTO locales(name, target_sentence_count, native_name, is_contributable, is_translated, text_direction) VALUES (?)`,
+            [
+              [
+                lang.code,
+                newLanguageData[lang.code].target_sentence_count,
+                lang.name,
+                newLanguageData[lang.code].is_contributable,
+                newLanguageData[lang.code].is_translated,
+                lang.direction,
+              ],
+            ]
+          );
+        }
+      }),
+    ]);
     // Make sure each language has at minimum an "unspecified" accent
     await db.query(`
     INSERT IGNORE INTO accents (locale_id, accent_name, accent_token, user_submitted)
