5cad702b58b41d3e451ce762b8b6463c486ff707
Gabriel <gabriel@mozillafoundation.org>
2022-05-17 10:56:29 -0400

    Formatting

--- server/src/lib/model/db/import-sentences.ts
+++ server/src/lib/model/db/import-sentences.ts
@@ -82,9 +82,7 @@ async function importLocaleSentences(
   version: number
 ) {
   await pool.query('INSERT IGNORE INTO locales (name) VALUES (?)', [locale]);
-  const [
-    [{ localeId }],
-  ] = await pool.query(
+  const [[{ localeId }]] = await pool.query(
     'SELECT id AS localeId FROM locales WHERE name = ? LIMIT 1',
     [locale]
   );
@@ -152,9 +150,11 @@ export async function importSentences(pool: any) {
     (await useRedis) ? await redis.get('sentences-version') : 0
   );
   const version = ((oldVersion || 0) + 1) % 256; //== max size of version column
-  const locales = ((await new Promise(resolve =>
-    fs.readdir(SENTENCES_FOLDER, (_, names) => resolve(names))
-  )) as string[]).filter(name => name !== 'LICENSE');
+  const locales = (
+    (await new Promise(resolve =>
+      fs.readdir(SENTENCES_FOLDER, (_, names) => resolve(names))
+    )) as string[]
+  ).filter(name => name !== 'LICENSE');
 
   print('locales', locales.join(','));
 
