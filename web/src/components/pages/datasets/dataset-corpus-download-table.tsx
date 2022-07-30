f465e62c1e7419b9ceb1e69cc7cf4beff5110d90
Gabriel <gabriel@mozillafoundation.org>
2022-07-30 15:56:58 -0400

    Fix table styling

--- web/src/components/pages/datasets/dataset-corpus-download-table.tsx
+++ web/src/components/pages/datasets/dataset-corpus-download-table.tsx
@@ -1,34 +1,85 @@
 import { Localized } from '@fluent/react';
 import * as React from 'react';
+import { useLocale } from '../../locale-helpers';
 import './dataset-corpus-download-table.css';
 
 interface Props {
   releaseData: any[];
   onRowSelect: any;
+  selectedId: number | null;
 }
 
-const COLUMN_VALUES = {
-  name: 'dataset-version',
-  release_date: 'dataset-date',
-  size: 'size',
-  total_clips_duration: 'recorded-hours',
-  valid_clips_duration: 'validated-hours',
-  license: 'cv-license',
-  total_users: 'number-of-voices',
-  audio_format: 'audio-format',
+const COLUMNS = {
+  name: {
+    display: (value: string) => {
+      return value;
+    },
+    label: 'dataset-version',
+  },
+  release_date: {
+    display: (value: string, locale: string) => {
+      return Intl.DateTimeFormat([locale, 'en']).format(new Date(value));
+    },
+    label: 'dataset-date',
+  },
+  size: {
+    display: (value: number) => {
+      return value;
+    },
+    label: 'size',
+  },
+  total_clips_duration: {
+    display: (value: number) => {
+      return value;
+    },
+    label: 'recorded-hours',
+  },
+  valid_clips_duration: {
+    display: (value: number) => {
+      const date = new Date(0);
+      date.setMilliseconds(value); // specify value for SECONDS here;
+      return date.getHours();
+    },
+    label: 'validated-hours',
+  },
+  license: {
+    display: () => {
+      return 'CC-0';
+    },
+    label: 'cv-license',
+  },
+  total_users: {
+    display: (value: string) => {
+      return value;
+    },
+    label: 'number-of-voices',
+  },
+  audio_format: {
+    display: (value: string) => {
+      return 'MP3';
+    },
+    label: 'audio-format',
+  },
 };
 
-const DatasetCorpusDownloadTable = ({ releaseData, onRowSelect }: Props) => {
-  const columnWidth = 100 / Object.keys(COLUMN_VALUES).length + '%';
+const DatasetCorpusDownloadTable = ({
+  releaseData,
+  onRowSelect,
+  selectedId,
+}: Props) => {
+  const labels = Object.keys(COLUMNS).map((col: string) => col);
+  console.log(labels);
+
+  const [locale] = useLocale();
 
   return (
     <table className="table dataset-table">
       <thead>
         <tr>
-          {Object.values(COLUMN_VALUES).map(name => {
+          {Object.values(COLUMNS).map(column => {
             return (
-              <th style={{ width: columnWidth }} key={name}>
-                <Localized id={name} />
+              <th key={column.label}>
+                <Localized id={column.label} />
               </th>
             );
           })}
@@ -37,11 +88,14 @@ const DatasetCorpusDownloadTable = ({ releaseData, onRowSelect }: Props) => {
       <tbody>
         {releaseData.map(row => {
           return (
-            <tr onClick={onRowSelect} key={row.id + row.release_dir}>
-              {Object.keys(COLUMN_VALUES).map((col, index) => {
+            <tr
+              onClick={() => onRowSelect(row.id)}
+              className={row.id === selectedId ? 'selected' : ''}
+              key={row.id + row.release_dir}>
+              {Object.keys(COLUMNS).map((col: string, index) => {
                 return (
-                  <td style={{ width: columnWidth }} key={index + col}>
-                    {row[col] || 0}
+                  <td key={index + COLUMNS[col].label}>
+                    {COLUMNS[col].display(row[col], locale)}
                   </td>
                 );
               })}
