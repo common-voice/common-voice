b30d9450bf78599fa507bb13f8cbae94ad06c2db
Gabriel <gabriel@mozillafoundation.org>
2022-08-24 10:10:06 -0400

    Make queries more generic

--- server/src/lib/model/statistics.ts
+++ server/src/lib/model/statistics.ts
@@ -1,60 +1,83 @@
 import lazyCache from '../lazy-cache';
 import { getMySQLInstance } from './db/mysql';
-import Model from '../model';
 
 const db = getMySQLInstance();
 
-export const getPastDateRange = (): string[] => {
-  const months = new Array(12)
-    .fill(0)
-    .map((s, i) => `NOW() - INTERVAL ${i + 1} MONTH`);
+type StatisticsCount = {
+  total_count: number;
+  date: string;
+};
+
+type StatisticTables = {
+  tableName: 'downloaders' | 'clips';
+};
+
+/**
+ * Attach metadata about the stats (like when it was fetched)
+ *
+ */
+const getFetchMetadata = () => {
   const today = new Date();
-  today.setDate(1); //ensure first of month
-  return months;
+  return {
+    last_fetched: today,
+  };
+};
+
+const buildResponse = (data: any) => {
+  return { ...data, metadata: getFetchMetadata() };
 };
 
 export const getDownloaderCount = lazyCache(
-  'downloader-monthly-stats',
+  'download-stats',
   async () => {
-    return await getDownloadersPastTwelveMonths();
+    const { total_count } = (await getTotal('downloaders'))[0];
+    const monthly_count = await getMonthlyContributions('downloaders');
+
+    const monthlyContributions = monthly_count.map(row => {
+      return { [row.date]: row.total_count };
+    });
+
+    let currentSum = 0;
+    const monthly_running_totals = monthly_count.map(row => {
+      const diff = total_count - currentSum;
+      currentSum += row.total_count;
+      const date = row.date;
+      return { [date]: diff };
+    });
+
+    return buildResponse({
+      total_count,
+      monthly_contributions: monthlyContributions,
+      montly_running_totals: monthly_running_totals,
+    });
   },
   1
 );
 
-const getDownloadersPastTwelveMonths = async () => {
+const getMonthlyContributions = async (
+  tableName: 'downloaders' | 'clips'
+): Promise<StatisticsCount[]> => {
   const [rows] = await db.query(`
-    SELECT
-    DATE_FORMAT(created_at, "%Y-%c-%d") as date,
-    id as count
-    FROM
-      downloaders d
-    WHERE
-      created_at > now() - INTERVAL 12 MONTH
-    GROUP BY
-      date_format(created_at, '%c')
-    ORDER BY created_at ASC
+  SELECT
+    MAX(DATE_FORMAT(created_at, "%Y-%c-%d")) as date,
+    COUNT(id) as total_count
+  FROM
+    ${tableName} d
+  WHERE
+    created_at > now() - INTERVAL 12 MONTH
+  GROUP BY
+    DATE_FORMAT(created_at, "%Y-%c")
+  ORDER BY created_at DESC;
   `);
   return rows;
 };
 
-async function getVoteLeaderboard(locale?: string): Promise<any[]> {
-  const [rows] = await db.query(
-    `
-      SELECT user_clients.client_id,
-             avatar_url,
-             avatar_clip_url,
-             username,
-             count(votes.id) as total
-      FROM user_clients
-      LEFT JOIN votes ON user_clients.client_id = votes.client_id
-      LEFT JOIN clips ON votes.clip_id = clips.id
-          WHERE visible = 1
-          ${locale ? 'AND clips.locale_id = :locale_id' : ''}
-      GROUP BY client_id
-        HAVING total > 0
-      ORDER BY total DESC
-    `
-  );
-
+const getTotal = async (tableName: 'downloaders' | 'clips') => {
+  const [rows] = await db.query(`
+    SELECT
+    count(1) as total_count
+    FROM
+      ${tableName} d
+  `);
   return rows;
-}
+};
