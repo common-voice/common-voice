66f6d1b92225868998406a3423a41408f101a52f
moz-rotimib <rotimi@mozillafoundation.org>
2022-10-11 14:56:25 +0200

    chore: continue styling partners page

--- web/src/components/pages/partner/partner.tsx
+++ web/src/components/pages/partner/partner.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Localized } from '@fluent/react';
 
-import { Line } from '../../ui/line/line';
 import Page from '../../ui/page';
 
 import './partner.css';
+import PageHeading from '../../ui/page-heading';
 
 const Partner = () => (
   <Page className="partner-main-container">
     <section className="partnerships-section">
       <div className="partner-container">
-        <Line />
-        <Localized id="partnerships-header">
-          <h1 />
-        </Localized>
+        <div className="partner-header-text">
+          <PageHeading>
+            <Localized id="partnerships-header" />
+          </PageHeading>
+          <p>
+            It takes a lot to make Common Voice happen! We don&apos;t do it
+            alone. Want to partner with us? We&apos;d love to hear from you.
+          </p>
+        </div>
+        <div className="partner-header-image">
+          <img
+            src={require('./images/mozilla-common-voice_foundation-mars.png')}
+            alt="robot"
+          />
+        </div>
       </div>
     </section>
   </Page>
