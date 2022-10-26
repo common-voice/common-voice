7e6ba6f66082de6f31486245f7da57f0232bedf7
Gabriel <gabriel@mozillafoundation.org>
2022-10-26 08:54:39 -0400

    Add queries for distinct values

--- server/src/lib/model/statistics.ts
+++ server/src/lib/model/statistics.ts
@@ -1,19 +1,25 @@
 import lazyCache from '../lazy-cache';
 import { getMySQLInstance } from './db/mysql';
 import { TableNames, TimeUnits } from 'common';
-
 const db = getMySQLInstance();
+
+// TODO: Replace with query that calculates average
+const AVG_CLIP_SECONDS = 4.694;
 
 type StatisticsCount = {
   total_count: number;
   date: string;
 };
 
+type QueryOptions = {
+  groupByColumn: string;
+  isDistinict: boolean;
+};
 /**
  * Attach metadata about the stats (like when it was fetched)
  *
  */
-const getFetchMetadata = () => {
+const getResponseMetadata = () => {
   const today = new Date();
   return {
     last_fetched: today,
@@ -21,15 +27,35 @@ const getFetchMetadata = () => {
 };
 
 const buildResponse = (data: any) => {
-  return { ...data, metadata: getFetchMetadata() };
+  return { ...data, metadata: getResponseMetadata() };
+};
+
+export const queryStatistics = async (
+  tableName: TableNames,
+  options?: QueryOptions
+) => {
+  const isDistinict = options?.isDistinict ?? false;
+  let total_count, monthly_count;
+
+  // two basic query paths (queries with distinct have to use group by)
+  if (isDistinict) {
+    // ({ total_count } = (await getTotal(tableName))[0]);
+    monthly_count = await getUniqueMonthlyContributions(tableName, options);
+    total_count = monthly_count.reduce(
+      (total: number, row) => (total += row.total_count),
+      0
+    );
+  } else {
+    ({ total_count } = (await getTotal(tableName))[0]);
+    monthly_count = await getMonthlyContributions(tableName);
+  }
+
+  return getTableStatistics(total_count, monthly_count);
 };
 
 export const getTableStatistics = lazyCache(
-  'get-table-statistic2sxx1x1',
-  async (tableName: TableNames) => {
-    const { total_count } = (await getTotal(tableName))[0];
-    const monthly_count = await getMonthlyContributions(tableName);
-
+  'get-table-stats2ax',
+  async (total_count: number, monthly_count: StatisticsCount[]) => {
     const monthlyContributions = monthly_count.reduce((obj: any, row) => {
       obj[row.date] = row.total_count;
       return obj;
@@ -61,6 +87,29 @@ const getMonthlyContributions = async (
       COUNT(created_at) as total_count
     FROM
       ${tableName} d
+    WHERE
+      created_at > now() - INTERVAL 12 MONTH
+    GROUP BY
+      DATE_FORMAT(created_at, "%Y-%c")
+    ORDER BY created_at DESC;
+  `);
+  return rows;
+};
+
+const getUniqueMonthlyContributions = async (
+  tableName: TableNames,
+  options: QueryOptions
+): Promise<StatisticsCount[]> => {
+  const { groupByColumn } = options;
+  const [rows] = await db.query(`
+  SELECT
+      MAX(DATE_FORMAT(created_at, "%Y-%c-%d")) as date,
+      COUNT(created_at) as total_count
+   FROM
+    (
+      SELECT DISTINCT * 
+      FROM ${tableName} d GROUP BY ${groupByColumn}
+    ) d
     WHERE
       created_at > now() - INTERVAL 12 MONTH
     GROUP BY
