2f443cee1cb0e16a236880e2274926c17ce6c691
moz-rotimib <rotimi@mozillafoundation.org>
2022-11-28 10:26:09 +0100

    feature: add second CTA screen and confetti

--- web/src/components/pages/contribution/speak/secondSubmissionCTA/secondSubmissionCTA.tsx
+++ web/src/components/pages/contribution/speak/secondSubmissionCTA/secondSubmissionCTA.tsx
@@ -1,3 +1,52 @@
 import * as React from 'react';
+import { Localized } from '@fluent/react';
+import Confetti from 'react-confetti';
 
-export const SecondPostSubmissionCTA = () => <div>Hello</div>;
+import useWindowSize from '../../../../../hooks/use-window-size';
+
+import './secondSubmissionCTA.css';
+
+export const SecondPostSubmissionCTA = () => {
+  const { height, width } = useWindowSize();
+
+  return (
+    <>
+      <Confetti
+        width={width}
+        height={height}
+        numberOfPieces={200}
+        gravity={0.03}
+      />
+      <div className="second-cta-container">
+        <div className="header-container">
+          <div className="happy-mars-container">
+            <img
+              src={require('./images/happy-mars@2x.png')}
+              alt="Happy Mars"
+              width={175}
+              height={175}
+            />
+          </div>
+          <div className="header-text-container">
+            <Localized id="second-cta-header-text">
+              <h1 className="header-text">
+                Thank you for contributing your voice!
+              </h1>
+            </Localized>
+          </div>
+        </div>
+
+        <div>
+          <div className="subtitle-text-container">
+            <Localized id="second-cta-subtitle-text">
+              <h2 className="subtitle-text">
+                With a profile, you can keep track of your activity and connect
+                with a community of voice data contributors.
+              </h2>
+            </Localized>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
