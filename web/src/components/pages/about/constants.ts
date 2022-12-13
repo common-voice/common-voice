d137db5af0eff7c41123e69153c849df09c1c89c
aosaimy <a.osaimy@gmail.com>
2022-12-13 10:57:17 +0000

    font

--- web/src/components/pages/about/constants.ts
+++ web/src/components/pages/about/constants.ts
@@ -1,9 +1,9 @@
 export const SECTIONS = {
   WHY_COMMON_VOICE: 'why-common-voice',
-  // HOW_IT_WORKS: 'how-it-works-2',
-  // PLAYBOOK: 'playbook',
-  // SUBSCRIBE: 'subscribe',
-  // GET_INVOLVED: 'get-involved',
+  HOW_IT_WORKS: 'how-it-works-2',
+  PLAYBOOK: 'playbook',
+  SUBSCRIBE: 'subscribe',
+  GET_INVOLVED: 'get-involved',
 };
 
 export const PLAYBOOK = {
