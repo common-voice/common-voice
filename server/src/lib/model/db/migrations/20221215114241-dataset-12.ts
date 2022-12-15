1b38c089677abd6402e778783ddc6e965e9d6e0c
Gabriel <gabriel@mozillafoundation.org>
2022-12-15 12:56:54 -0500

    Add cv12 stats

--- server/src/lib/model/db/migrations/20221215114241-dataset-12.ts
+++ server/src/lib/model/db/migrations/20221215114241-dataset-12.ts
@@ -1,5 +1,5 @@
 export const up = async function (db: any): Promise<any> {
-  return db.runSql(`
+  await db.runSql(`
   INSERT IGNORE INTO datasets(name, release_dir, multilingual, bundle_date, release_date, total_clips_duration,	valid_clips_duration,	release_type, download_path )
   VALUES
     ('Common Voice Corpus 12.0', 'cv-corpus-12.0-2022-12-07', TRUE, '2022-12-07', '2022-12-15', 94028400000, 61656878000, "complete","cv-corpus-12.0-2022-12-07/cv-corpus-12.0-2022-12-07-{locale}.tar.gz")
