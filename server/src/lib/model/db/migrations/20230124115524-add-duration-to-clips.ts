a40c7cb02654fd487f8e03ec05fbea2cf6bd8ccf
Dmitrij Feller <dmitrij@mozillafoundation.org>
2023-01-25 11:19:55 +0100

    perf: change alter table statement to use an inplace algorithm

--- server/src/lib/model/db/migrations/20230124115524-add-duration-to-clips.ts
+++ server/src/lib/model/db/migrations/20230124115524-add-duration-to-clips.ts
@@ -1,5 +1,5 @@
 export const up = async function (db: any): Promise<any> {
-  return db.runSql(`ALTER TABLE clips ADD COLUMN duration INT NOT NULL DEFAULT 0`);
+  return db.runSql(`ALTER TABLE clips ADD COLUMN duration INT NOT NULL DEFAULT 0, algorithm=inplace, lock=none`);
 };
 
 export const down = async function (db: any): Promise<any> {
