2145a959811aead55bcff09ba46d6f0197fade46
moz-rotimib <rotimi@mozillafoundation.org>
2023-02-06 15:40:05 +0100

    feat: add Tabs to guidelines page - add content components for Voice collection tab

---
+++ web/src/components/pages/guidelines/sidebar-content/voice-collection/varying-pronunciations.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const VaryingPronounciation = () => <h1>Mock content</h1>;
---
+++ web/src/components/pages/guidelines/sidebar-content/voice-collection/volume.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const Volume = () => <h1>Volume</h1>;
