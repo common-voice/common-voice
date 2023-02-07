94aa401a2ef2cbe39bed25247d4f28f48559430d
moz-rotimib <rotimi@mozillafoundation.org>
2023-02-07 18:35:57 +0100

    feat: add content section for voice collection tab

--- web/src/components/pages/guidelines/sidebar-content/voice-collection/misreadings.tsx
+++ web/src/components/pages/guidelines/sidebar-content/voice-collection/misreadings.tsx
@@ -1,3 +1,65 @@
+import { Localized } from '@fluent/react';
 import React from 'react';
 
-export const Misreadings = () => <h1>Misreading</h1>;
+import { NAV_IDS } from '../../constants';
+import { ExampleContent, ExampleTip } from './example-content';
+
+const exampleTips: ExampleTip[] = [
+  { text: 'misreadings-example-1', icon: 'check' },
+  {
+    text: 'misreadings-example-2',
+    icon: 'x',
+    explanation: 'misreadings-tip-1',
+  },
+  {
+    text: 'misreadings-example-3',
+    icon: 'x',
+    explanation: 'misreadings-tip-2',
+  },
+  {
+    text: 'misreadings-example-4',
+    icon: 'x',
+    explanation: 'misreadings-tip-3',
+  },
+];
+
+export const Misreadings = () => (
+  <div className="sidebar-content" id={NAV_IDS.MISREADINGS}>
+    <Localized id="misreadings">
+      <h3 className="guidelines-content-heading" />
+    </Localized>
+    <Localized id="misreadings-explanation-1">
+      <p className="guidelines-content-explanation" />
+    </Localized>
+    <Localized id="misreadings-explanation-2">
+      <p className="guidelines-content-explanation" />
+    </Localized>
+    <ul>
+      <Localized id="misreadings-explanation-3">
+        <li />
+      </Localized>
+      <Localized id="misreadings-explanation-4">
+        <li />
+      </Localized>
+      <Localized id="misreadings-explanation-5">
+        <li />
+      </Localized>
+      <Localized id="misreadings-explanation-6">
+        <li />
+      </Localized>
+      <Localized id="misreadings-explanation-7">
+        <li />
+      </Localized>
+    </ul>
+    <div>
+      <Localized id="example">
+        <p className="example" />
+      </Localized>
+      <ExampleContent
+        exampleText="misreadings-example-1"
+        exampleTips={exampleTips}
+      />
+    </div>
+    <span className="border" />
+  </div>
+);
--- web/src/components/pages/guidelines/sidebar-content/voice-collection/offensive-content.tsx
+++ web/src/components/pages/guidelines/sidebar-content/voice-collection/offensive-content.tsx
@@ -1,3 +1,33 @@
+import { Localized } from '@fluent/react';
 import React from 'react';
 
-export const OffensiveContent = () => <h1>Offensive Content</h1>;
+import { NAV_IDS } from '../../constants';
+
+export const OffensiveContent = () => (
+  <div className="sidebar-content" id={NAV_IDS.OFFENSIVE_CONTENT}>
+    <Localized id="offensive-content">
+      <h3 className="guidelines-content-heading" />
+    </Localized>
+    <Localized
+      id="offensive-content-explanation"
+      elems={{
+        participationGuidelines: (
+          <a
+            href="https://www.mozilla.org/en-US/about/governance/policies/participation/"
+            target="_blank"
+            rel="noreferrer"
+          />
+        ),
+        emailFragment: (
+          <a
+            href="mailto:commonvoice@mozilla.com"
+            target="_blank"
+            rel="noreferrer"
+          />
+        ),
+      }}>
+      <p className="guidelines-content-explanation" />
+    </Localized>
+    <span className="border" />
+  </div>
+);
