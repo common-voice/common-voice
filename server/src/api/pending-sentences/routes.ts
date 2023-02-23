9b0d37f045d879806db1a0deb4a1ef1eb86b827d
Dmitrij Feller <dmitrij@mozillafoundation.org>
2023-02-23 10:56:25 +0100

    feat: add pending sentence vote handler and route

--- server/src/api/pending-sentences/routes.ts
+++ server/src/api/pending-sentences/routes.ts
@@ -2,16 +2,23 @@ import addPendingSentenceHandler from './handler/add-pending-sentence-handler'
 import PromiseRouter from 'express-promise-router'
 import {
   AddPendingSentenceRequest,
+  AddPendingSentenceVoteRequest,
   GetPendingSentencesForReviewRequest,
 } from './validation/add-pending-sentence-request'
 import validate, { validateStrict } from '../../lib/validation'
 import getPendingSentenceHandler from './handler/get-pending-sentence-handler'
+import addPendingSentenceVoteHandler from './handler/add-pending-sentence-vote-handler'
 
 export default PromiseRouter({ mergeParams: true })
   .post(
     '/',
     validateStrict({ body: AddPendingSentenceRequest }),
     addPendingSentenceHandler
+  )
+  .post(
+    '/vote',
+    validateStrict({ body: AddPendingSentenceVoteRequest }),
+    addPendingSentenceVoteHandler
   )
   .get(
     '/review',
