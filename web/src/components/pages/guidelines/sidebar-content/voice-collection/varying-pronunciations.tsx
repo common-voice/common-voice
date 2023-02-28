8483052200c8c832e5d69e7b57d6072df161c978
moz-rotimib <rotimi@mozillafoundation.org>
2023-02-28 13:55:27 +0100

    feat: add sections for voice and sentence collection to guidelines page

--- web/src/components/pages/guidelines/sidebar-content/voice-collection/varying-pronunciations.tsx
+++ web/src/components/pages/guidelines/sidebar-content/voice-collection/varying-pronunciations.tsx
@@ -2,9 +2,9 @@ import { Localized } from '@fluent/react';
 import classNames from 'classnames';
 import React from 'react';
 import { ChevronDown } from '../../../../ui/icons';
-import { SidebarContentProps } from '../sidebar-content';
+import { SidebarContentProps } from '../voice-sidebar-content';
 
-import { ExampleContent, ExampleTip } from './example-content';
+import { ExampleContent, ExampleTip } from './example-content-voice';
 
 const exampleTips: ExampleTip[] = [
   {
