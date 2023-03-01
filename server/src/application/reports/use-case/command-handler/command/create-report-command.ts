cec00231dc3a55b596b5be3cba25a222a39a341c
Dmitrij Feller <dmitrij@mozillafoundation.org>
2023-03-01 12:08:48 +0100

    refactor: use constants for report kinds

--- server/src/application/reports/use-case/command-handler/command/create-report-command.ts
+++ server/src/application/reports/use-case/command-handler/command/create-report-command.ts
@@ -1,5 +1,14 @@
-export const reportKinds = ['clip', 'sentence', 'pending_sentence'] as const
-export type ReportKind = typeof reportKinds[number]
+export const REPORTED_CLIP = 'clip'
+export const REPORTED_SENTENCE = 'sentence'
+export const REPORTED_PENDING_SENTENCE = 'pending_sentence'
+
+export const REPORT_KINDS = [
+  REPORTED_CLIP,
+  REPORTED_SENTENCE,
+  REPORTED_PENDING_SENTENCE,
+] as const
+
+export type ReportKind = typeof REPORT_KINDS[number]
 
 export type CreateReportCommand = {
   clientId: string
