ecde7e7fe4b451ab536380a40bdaa9f1320e9dab
moz-rotimib <rotimi@mozillafoundation.org>
2023-03-01 16:35:51 +0100

    feat: add collapsible sections for mobile - make tabs header sticky on tablet view

--- web/src/components/pages/guidelines/sidebar-content/sentence-collection/public-domain.tsx
+++ web/src/components/pages/guidelines/sidebar-content/sentence-collection/public-domain.tsx
@@ -1,38 +1,55 @@
 import { Localized } from '@fluent/react';
+import classNames from 'classnames';
 import React from 'react';
+import { ChevronDown } from '../../../../ui/icons';
+import { TextButton } from '../../../../ui/ui';
+import { SidebarContentProps } from '../voice-sidebar-content';
 
-export const PublicDomain = () => {
+export const PublicDomain: React.FC<SidebarContentProps> = ({
+  id,
+  contentVisible,
+  toggleVisibleSection,
+}) => {
   return (
-    <div className="sidebar-content">
+    <div className="sidebar-content" id={id}>
       <span className="line" />
       <div className="sidebar-content-header">
         <Localized id="public-domain">
-          <h3 className="guidelines-content-heading" />
+          <TextButton
+            onClick={toggleVisibleSection}
+            className="guidelines-content-heading"
+          />
         </Localized>
+        <ChevronDown
+          onClick={toggleVisibleSection}
+          className={classNames('chevron', { 'rotate-180': contentVisible })}
+        />
       </div>
-      <div className="content-wrapper">
-        <Localized id="public-domain-explanation-1">
-          <p className="guidelines-content-explanation" />
-        </Localized>
-        <Localized id="public-domain-explanation-2">
-          <p className="guidelines-content-explanation" />
-        </Localized>
-        <ul>
-          <Localized id="public-domain-explanation-3">
-            <li />
+      {contentVisible && (
+        <div className="content-wrapper">
+          <Localized id="public-domain-explanation-1">
+            <p className="guidelines-content-explanation" />
           </Localized>
-          <Localized id="public-domain-explanation-4">
-            <li />
+          <Localized id="public-domain-explanation-2">
+            <p className="guidelines-content-explanation" />
           </Localized>
-          <Localized id="public-domain-explanation-5">
-            <li />
-          </Localized>
-          <Localized id="public-domain-explanation-6">
-            <li />
-          </Localized>
-        </ul>
-        <span className="border" />
-      </div>
+          <ul>
+            <Localized id="public-domain-explanation-3">
+              <li />
+            </Localized>
+            <Localized id="public-domain-explanation-4">
+              <li />
+            </Localized>
+            <Localized id="public-domain-explanation-5">
+              <li />
+            </Localized>
+            <Localized id="public-domain-explanation-6">
+              <li />
+            </Localized>
+          </ul>
+          <span className="border" />
+        </div>
+      )}
     </div>
   );
 };
