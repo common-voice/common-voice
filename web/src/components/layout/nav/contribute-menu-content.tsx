96527c96505325d478e4e491ea40e6195bbb9e0c
moz-rotimib <rotimi@mozillafoundation.org>
2023-03-06 16:29:13 +0100

    chore: show only write option in contribute menu

--- web/src/components/layout/nav/contribute-menu-content.tsx
+++ web/src/components/layout/nav/contribute-menu-content.tsx
@@ -3,19 +3,17 @@ import { Localized } from '@fluent/react';
 import classNames from 'classnames';
 
 import { LocaleLink } from '../../locale-helpers';
-import { EditIcon, ListenIcon, MicIcon, ReviewIcon } from '../../ui/icons';
+import { EditIcon, ListenIcon, MicIcon } from '../../ui/icons';
 import URLS from '../../../urls';
 
 type ContributeMenuContentProps = {
   className?: string;
   pathname?: string;
-  isUserLoggedIn: boolean;
 };
 
 export const ContributeMenuContent: React.FC<ContributeMenuContentProps> = ({
   className,
   pathname = '',
-  isUserLoggedIn,
 }) => {
   const speakActive = pathname.includes(URLS.SPEAK);
   const listenActive = pathname.includes(URLS.LISTEN);
@@ -62,15 +60,6 @@ export const ContributeMenuContent: React.FC<ContributeMenuContentProps> = ({
             </LocaleLink>
             {speakActive && <span className="border" />}
           </li>
-          {isUserLoggedIn && (
-            <li className="review">
-              <ReviewIcon />
-              <LocaleLink to={URLS.REVIEW} className="contribute-link">
-                <Localized id="review" />
-              </LocaleLink>
-              {listenActive && <span className="border" />}
-            </li>
-          )}
         </ul>
       </div>
     </div>
