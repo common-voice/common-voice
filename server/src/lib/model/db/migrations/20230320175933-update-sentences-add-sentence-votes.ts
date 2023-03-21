dd74cf03291a093bc59aeeeda91270326799783f
Dmitrij Feller <dmitrij@mozillafoundation.org>
2023-03-21 18:32:02 +0100

    feat: add sentence_metadata table

--- server/src/lib/model/db/migrations/20230320175933-update-sentences-add-sentence-votes.ts
+++ server/src/lib/model/db/migrations/20230320175933-update-sentences-add-sentence-votes.ts
@@ -1,7 +1,19 @@
 export const up = async function (db: any): Promise<any> {
   return db.runSql(`
-    ALTER TABLE sentences
-    ADD COLUMN is_validated TINYINT DEFAULT FALSE NOT NULL;
+    CREATE TABLE IF NOT EXISTS sentence_metadata
+    (
+      id INT NOT NULL AUTO_INCREMENT,
+      sentence_id VARCHAR(255) NOT NULL,
+      client_id CHAR(36) DEFAULT NULL,
+      created_at DATETIME NOT NULL DEFAULT NOW(),
+      updated_at DATETIME NOT NULL DEFAULT NOW(),
+      PRIMARY KEY (id),
+      FOREIGN KEY (sentence_id) REFERENCES sentences(id) ON DELETE CASCADE,
+      FOREIGN KEY (client_id) REFERENCES user_clients(client_id) ON DELETE SET NULL
+    );
+
+    ALTER TABLE sentences ADD COLUMN is_validated TINYINT DEFAULT FALSE NOT NULL;
+    
     -- Update all existing sentences to be valid
     UPDATE sentences SET is_validated = TRUE;
 
@@ -22,7 +34,10 @@ export const up = async function (db: any): Promise<any> {
 
 export const down = async function (db: any): Promise<any> {
   return db.runSql(`
-    ALTER TABLE sentences DROP COLUMN  is_validated;
+    ALTER TABLE sentences
+    DROP COLUMN is_validated;
+
+    DROP TABLE sentence_metadata;
 
     DROP TABLE sentence_votes;
   `);
