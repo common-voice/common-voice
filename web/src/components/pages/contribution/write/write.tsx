87f734b9dbe8360bd8686939e47362e220ec1d3e
moz-rotimib <rotimi@mozillafoundation.org>
2023-03-22 14:57:10 +0100

    feat: add more elements to write page

--- web/src/components/pages/contribution/write/write.tsx
+++ web/src/components/pages/contribution/write/write.tsx
@@ -3,70 +3,105 @@ import {
   withLocalization,
   WithLocalizationProps,
 } from '@fluent/react';
-import React from 'react';
+import React, { useState } from 'react';
 
-import { EditIcon } from '../../../ui/icons';
-import { LabeledInput } from '../../../ui/ui';
+import { QuestionIcon, SendIcon } from '../../../ui/icons';
+import { Button, LabeledCheckbox, LinkButton } from '../../../ui/ui';
+import URLS from '../../../../urls';
+import ExpandableInformation from '../../../expandable-information/expandable-information';
+import { PrimaryButton } from '../../../primary-buttons/primary-buttons';
 
 import './write.css';
+import { SentenceInputAndRules } from './sentence-input-and-rules/sentence-input-and-rules';
 
-const NUMBER_OF_SENTENCE_RULES = 7;
+export type WriteProps = WithLocalizationProps;
 
-const Write: React.FC<WithLocalizationProps> = ({ getString }) => (
-  <div className="write-page">
-    <div className="write-wrapper">
-      <div className="write-page-header">
-        <Localized id="sc-header-add">
-          <span />
-        </Localized>
-        <EditIcon />
-        <Localized id="write-page-header-public-domain">
-          <span />
-        </Localized>
-      </div>
-      <Localized id="write-page-subtitle">
-        <p className="subtitle" />
-      </Localized>
-      <div className="inputs-and-rules-wrapper">
-        <div className="inputs-wrapper">
-          <Localized id="sentence" attrs={{ label: true }}>
-            <LabeledInput
-              placeholder={getString('sentence-input-value')}
-              className="sentence-input"
-              component="textarea"
-            />
-          </Localized>
-          <Localized id="citation" attrs={{ label: true }}>
-            <LabeledInput
-              placeholder={getString('citation-input-value')}
-              className="citation-input"
-            />
-          </Localized>
-        </div>
-        <div className="rules">
-          <Localized id="what-can-i-add">
-            <p />
-          </Localized>
-          <ul>
-            {Array.from(
-              Array(NUMBER_OF_SENTENCE_RULES),
-              (_, index) => index + 1
-            ).map((_, index) => (
-              <Localized key={index} id={`new-sentence-rule-${index + 1}`}>
-                <li />
-              </Localized>
-            ))}
-            <Localized id="new-sentence-rule-1">
-              <li />
-            </Localized>
-            <Localized id="new-sentence-rule-2">
-              <li />
-            </Localized>
-          </ul>
+const Write: React.FC<WriteProps> = ({ getString }) => {
+  const [confirmPublicDomain, setConfirmPublicDomain] = useState(false);
+
+  const handlePublicDomainChange = () => {
+    setConfirmPublicDomain(!confirmPublicDomain);
+  };
+
+  return (
+    <div className="write-page">
+      <div className="write-wrapper">
+        <div className="write">
+          <div className="inputs-and-rules-container">
+            <SentenceInputAndRules getString={getString} />
+
+            <div className="expandable-container">
+              <ExpandableInformation summaryLocalizedId="how-to-cite">
+                <Localized id="how-to-cite-explanation-bold">
+                  <span className="bold" />
+                </Localized>
+                <Localized id="how-to-cite-explanation">
+                  <span />
+                </Localized>
+              </ExpandableInformation>
+            </div>
+          </div>
+
+          <div className="buttons">
+            <div>
+              <LinkButton
+                rounded
+                outline
+                className="hidden-sm-down guidelines-button"
+                blank
+                to={URLS.SPEAK}>
+                <QuestionIcon />
+                <Localized id="guidelines">
+                  <span />
+                </Localized>
+              </LinkButton>
+              <Button
+                rounded
+                outline
+                className="hidden-sm-down"
+                onClick={() => console.log('sksksk')}>
+                <SendIcon />
+                <Localized id="contact-us">
+                  <span />
+                </Localized>
+              </Button>
+            </div>
+            <div>
+              <form className="guidelines-form" data-testid="guidelines-form">
+                <LabeledCheckbox
+                  label={
+                    <Localized
+                      id="sc-submit-confirm"
+                      elems={{
+                        wikipediaLink: (
+                          <a
+                            href="https://en.wikipedia.org/wiki/Public_domain"
+                            target="_blank"
+                            rel="noreferrer"
+                          />
+                        ),
+                      }}>
+                      <span />
+                    </Localized>
+                  }
+                  required
+                  onChange={handlePublicDomainChange}
+                  data-testid="checkbox"
+                />
+                <Localized id="submit-form-action">
+                  <PrimaryButton
+                    className="submit"
+                    type="submit"
+                    disabled={!confirmPublicDomain}
+                  />
+                </Localized>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default withLocalization(Write);
