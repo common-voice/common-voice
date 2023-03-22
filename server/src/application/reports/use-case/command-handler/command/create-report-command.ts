e463715834feea351c714082b3cdb2fbdadb0b2f
Dmitrij Feller <dmitrij@mozillafoundation.org>
2023-03-22 14:32:42 +0100

    chore: remove unused report type

--- server/src/application/reports/use-case/command-handler/command/create-report-command.ts
+++ server/src/application/reports/use-case/command-handler/command/create-report-command.ts
@@ -1,11 +1,9 @@
 export const REPORTED_CLIP = 'clip'
 export const REPORTED_SENTENCE = 'sentence'
-export const REPORTED_PENDING_SENTENCE = 'pending_sentence'
 
 export const REPORT_KINDS = [
   REPORTED_CLIP,
   REPORTED_SENTENCE,
-  REPORTED_PENDING_SENTENCE,
 ] as const
 
 export type ReportKind = typeof REPORT_KINDS[number]
