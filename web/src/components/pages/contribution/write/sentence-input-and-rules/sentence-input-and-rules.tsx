119769b16d4759ef88d102c4e61fe3110bf040d9
moz-rotimib <rotimi@mozillafoundation.org>
2023-03-28 16:25:00 +0200

    chore: initial validation state for sentence submission

--- web/src/components/pages/contribution/write/sentence-input-and-rules/sentence-input-and-rules.tsx
+++ web/src/components/pages/contribution/write/sentence-input-and-rules/sentence-input-and-rules.tsx
@@ -6,6 +6,7 @@ import { LabeledInput } from '../../../../ui/ui';
 import { WriteProps } from '../write';
 import { Rules } from './rules';
 import ExpandableInformation from '../../../../expandable-information/expandable-information';
+import { SentenceSubmissionError } from 'common';
 
 type Props = {
   getString: WriteProps['getString'];
@@ -15,6 +16,7 @@ type Props = {
   handleCitationChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   sentence: string;
   citation: string;
+  error: SentenceSubmissionError;
 };
 
 export const SentenceInputAndRules: React.FC<Props> = ({
@@ -23,51 +25,54 @@ export const SentenceInputAndRules: React.FC<Props> = ({
   handleSentenceInputChange,
   sentence,
   citation,
-}) => (
-  <div className="inputs-and-instruction">
-    <div className="write-page-instruction">
-      <Localized id="sc-header-add">
-        <span />
-      </Localized>
-      <EditIcon />
-      <Localized id="write-page-instruction-public-domain">
-        <span />
-      </Localized>
-    </div>
-    <Localized id="write-page-subtitle">
-      <p className="subtitle" />
-    </Localized>
-    <div className="inputs-and-rules-wrapper">
-      <div className="inputs">
-        <Localized id="sentence" attrs={{ label: true }}>
-          <LabeledInput
-            placeholder={getString('sentence-input-value')}
-            className="sentence-input"
-            component="textarea"
-            onChange={handleSentenceInputChange}
-            value={sentence}
-          />
+  error,
+}) => {
+  return (
+    <div className="inputs-and-instruction">
+      <div className="write-page-instruction">
+        <Localized id="sc-header-add">
+          <span />
         </Localized>
-        <Localized id="citation" attrs={{ label: true }}>
-          <LabeledInput
-            placeholder={getString('citation-input-value')}
-            className="citation-input"
-            onChange={handleCitationChange}
-            value={citation}
-          />
+        <EditIcon />
+        <Localized id="write-page-instruction-public-domain">
+          <span />
         </Localized>
-        <div className="expandable-container">
-          <ExpandableInformation summaryLocalizedId="how-to-cite">
-            <Localized id="how-to-cite-explanation-bold">
-              <span className="bold" />
-            </Localized>
-            <Localized id="how-to-cite-explanation">
-              <span />
-            </Localized>
-          </ExpandableInformation>
-        </div>
       </div>
-      <Rules />
+      <Localized id="write-page-subtitle">
+        <p className="subtitle" />
+      </Localized>
+      <div className="inputs-and-rules-wrapper">
+        <div className="inputs">
+          <Localized id="sentence" attrs={{ label: true }}>
+            <LabeledInput
+              placeholder={getString('sentence-input-value')}
+              className="sentence-input"
+              component="textarea"
+              onChange={handleSentenceInputChange}
+              value={sentence}
+            />
+          </Localized>
+          <Localized id="citation" attrs={{ label: true }}>
+            <LabeledInput
+              placeholder={getString('citation-input-value')}
+              className="citation-input"
+              onChange={handleCitationChange}
+              value={citation}
+            />
+          </Localized>
+          <div className="expandable-container">
+            <ExpandableInformation summaryLocalizedId="how-to-cite">
+              <Localized id="how-to-cite-explanation-bold">
+                <span className="bold" />
+              </Localized>
+              <Localized id="how-to-cite-explanation">
+                <span />
+              </Localized>
+            </ExpandableInformation>
+          </div>
+        </div>
+        <Rules />
+      </div>
     </div>
-  </div>
-);
+  );
+};
