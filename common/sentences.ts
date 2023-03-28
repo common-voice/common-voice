80dc180d503c5da76ef0e6195fbe2cec1edb1b9b
moz-rotimib <rotimi@mozillafoundation.org>
2023-03-28 16:25:00 +0200

    chore: initial validation state for sentence submission

--- common/sentences.ts
+++ common/sentences.ts
@@ -4,3 +4,12 @@ export type SentenceSubmission = {
   localeId: number
   localeName: string
 }
+
+export enum SentenceSubmissionError {
+  TOO_LONG = 'TOO_LONG',
+  NO_NUMBERS = 'NO_NUMBERS',
+  NO_SYMBOLS = 'NO_SYMBOLS',
+  NO_ABBREVIATIONS = 'NO_ABBREVIATIONS',
+  NO_FOREIGN_SCRIPT = 'NO_FOREIGN_SCRIPT',
+  OTHER = 'NO_FOREIGN_SCRIPT',
+}
