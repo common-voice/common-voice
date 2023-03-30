199ebab832b11e7034740f22c9e90aeef4f798a2
moz-rotimib <rotimi@mozillafoundation.org>
2023-03-30 14:10:22 +0200

    chore: add citation error message - make rules container collapsible on mobile views

--- web/src/components/pages/contribution/write/sentence-input-and-rules/rules.tsx
+++ web/src/components/pages/contribution/write/sentence-input-and-rules/rules.tsx
@@ -1,80 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Localized } from '@fluent/react';
 import classNames from 'classnames';
 
 import { SentenceSubmissionError } from 'common';
+import { ChevronDown } from '../../../../ui/icons';
+import { TextButton } from '../../../../ui/ui';
 
 type Props = {
   error: SentenceSubmissionError;
 };
 
-export const Rules: React.FC<Props> = ({ error }) => (
-  <div className="rules">
-    <div className="inner">
-      <Localized id="what-can-i-add">
-        <p />
-      </Localized>
-      <ul>
-        <Localized
-          id="new-sentence-rule-1"
-          elems={{
-            noCopyright: (
-              <a
-                href="https://en.wikipedia.org/wiki/Public_domain"
-                target="_blank"
-                rel="noreferrer"
+export const Rules: React.FC<Props> = ({ error }) => {
+  const [rulesVisible, setShowRulesVisible] = useState(true);
+
+  const handleClick = () => {
+    setShowRulesVisible(!rulesVisible);
+  };
+
+  return (
+    <div className="rules">
+      <div className={classNames('inner', { 'rules-hidden': !rulesVisible })}>
+        <div className="rules-title-container">
+          <Localized id="what-can-i-add">
+            <TextButton onClick={handleClick} />
+          </Localized>
+          <ChevronDown
+            className={classNames({ 'rotate-180': rulesVisible })}
+            onClick={handleClick}
+          />
+        </div>
+        {rulesVisible && (
+          <ul>
+            <Localized
+              id="new-sentence-rule-1"
+              elems={{
+                noCopyright: (
+                  <a
+                    href="https://en.wikipedia.org/wiki/Public_domain"
+                    target="_blank"
+                    rel="noreferrer"
+                  />
+                ),
+                cc0: (
+                  <a
+                    href="https://creativecommons.org/share-your-work/public-domain/cc0/"
+                    target="_blank"
+                    rel="noreferrer"
+                  />
+                ),
+              }}>
+              <li />
+            </Localized>
+            <Localized id="new-sentence-rule-2">
+              <li
+                className={classNames({
+                  error: error === SentenceSubmissionError.TOO_LONG,
+                })}
               />
-            ),
-            cc0: (
-              <a
-                href="https://creativecommons.org/share-your-work/public-domain/cc0/"
-                target="_blank"
-                rel="noreferrer"
+            </Localized>
+            <Localized id="new-sentence-rule-3">
+              <li />
+            </Localized>
+            <Localized id="new-sentence-rule-4">
+              <li />
+            </Localized>
+            <Localized id="new-sentence-rule-5">
+              <li
+                className={classNames({
+                  error:
+                    error === SentenceSubmissionError.NO_NUMBERS ||
+                    error === SentenceSubmissionError.NO_SYMBOLS,
+                })}
               />
-            ),
-          }}>
-          <li />
-        </Localized>
-        <Localized id="new-sentence-rule-2">
-          <li
-            className={classNames({
-              error: error === SentenceSubmissionError.TOO_LONG,
-            })}
-          />
-        </Localized>
-        <Localized id="new-sentence-rule-3">
-          <li />
-        </Localized>
-        <Localized id="new-sentence-rule-4">
-          <li />
-        </Localized>
-        <Localized id="new-sentence-rule-5">
-          <li
-            className={classNames({
-              error:
-                error === SentenceSubmissionError.NO_NUMBERS ||
-                error === SentenceSubmissionError.NO_SYMBOLS,
-            })}
-          />
-        </Localized>
-        <Localized id="new-sentence-rule-6">
-          <li
-            className={classNames({
-              error: error === SentenceSubmissionError.NO_FOREIGN_SCRIPT,
-            })}
-          />
-        </Localized>
-        <Localized id="new-sentence-rule-7">
-          <li
-            className={classNames({
-              error: error === SentenceSubmissionError.NO_CITATION,
-            })}
-          />
-        </Localized>
-        <Localized id="new-sentence-rule-8">
-          <li className="last-rule" />
-        </Localized>
-      </ul>
+            </Localized>
+            <Localized id="new-sentence-rule-6">
+              <li
+                className={classNames({
+                  error: error === SentenceSubmissionError.NO_FOREIGN_SCRIPT,
+                })}
+              />
+            </Localized>
+            <Localized id="new-sentence-rule-7">
+              <li
+                className={classNames({
+                  error: error === SentenceSubmissionError.NO_CITATION,
+                })}
+              />
+            </Localized>
+            <Localized id="new-sentence-rule-8">
+              <li className="last-rule" />
+            </Localized>
+          </ul>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
