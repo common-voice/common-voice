4aea30cd266d39b7bc240eba6925e5024f040b36
Dmitrij Feller <dmitrij@mozillafoundation.org>
2023-05-11 12:46:36 +0200

    refactor: split up work inside the command handler

--- server/src/application/bulk-submissions/services/bulk-submission-storage-service.ts
+++
@@ -1,9 +0,0 @@
-import { Storage } from '@google-cloud/storage'
-import { getConfig } from '../../../config-helper'
-import { fileExists, upload } from '../../../infrastructure/storage/storage'
-
-const BUCKET_NAME = getConfig().BULK_SUBMISSION_BUCKET_NAME
-const storage = new Storage({ credentials: getConfig().GCP_CREDENTIALS })
-
-export const uploadBulkSubmission = upload(storage)(BUCKET_NAME)
-export const doesBulkSubmissionExist = fileExists(storage)(BUCKET_NAME)
--- server/src/infrastructure/storage/storage.ts
+++ server/src/infrastructure/storage/storage.ts
@@ -1,10 +1,5 @@
 import { Storage } from '@google-cloud/storage'
-import { getConfig } from '../../config-helper'
 import { taskEither as TE } from 'fp-ts'
-import { bool } from 'aws-sdk/clients/signer'
-
-const BUCKET_NAME = getConfig().BULK_SUBMISSION_BUCKET_NAME
-const storage = new Storage({ credentials: getConfig().GCP_CREDENTIALS })
 
 export const upload =
   (storage: Storage) =>
@@ -17,15 +12,15 @@ export const upload =
     )
   }
 
-  export const fileExists =
-  (storage: Storage) =>
-  (bucket: string) =>
-  (path: string): TE.TaskEither<Error, boolean> => {
-    return TE.tryCatch(
-      async () => {
-        const [exists] = await storage.bucket(bucket).file(path).exists()
-        return exists
-    },
-      (err: Error) => err
-    )
-  }
+export const fileExists =
+(storage: Storage) =>
+(bucket: string) =>
+(path: string): TE.TaskEither<Error, boolean> => {
+  return TE.tryCatch(
+    async () => {
+      const [exists] = await storage.bucket(bucket).file(path).exists()
+      return exists
+  },
+    (err: Error) => err
+  )
+}
