06020ab5ac8f71ec5d814a0f8f3d8887aab155c2
Dmitrij Feller <dmitrij@mozillafoundation.org>
2023-05-16 10:04:54 +0200

    feat: add local dev environment for gcp storage

--- server/src/infrastructure/storage/storage.ts
+++ server/src/infrastructure/storage/storage.ts
@@ -1,5 +1,21 @@
 import { Storage } from '@google-cloud/storage'
 import { taskEither as TE } from 'fp-ts'
+import { getConfig } from '../../config-helper'
+
+const BUCKET_NAME = getConfig().BULK_SUBMISSION_BUCKET_NAME
+
+const storageConfig =
+  getConfig().ENVIRONMENT === 'local'
+    ? {
+        apiEndpoint: getConfig().STORAGE_LOCAL_DEVELOPMENT_ENDPOINT,
+        projectId: 'local',
+        useAuthWithCustomEndpoint: false,
+      }
+    : {
+        credentials: getConfig().GCP_CREDENTIALS,
+      }
+
+const storage = new Storage(storageConfig)
 
 export const upload =
   (storage: Storage) =>
@@ -13,14 +29,17 @@ export const upload =
   }
 
 export const fileExists =
-(storage: Storage) =>
-(bucket: string) =>
-(path: string): TE.TaskEither<Error, boolean> => {
-  return TE.tryCatch(
-    async () => {
-      const [exists] = await storage.bucket(bucket).file(path).exists()
-      return exists
-  },
-    (err: Error) => err
-  )
-}
+  (storage: Storage) =>
+  (bucket: string) =>
+  (path: string): TE.TaskEither<Error, boolean> => {
+    return TE.tryCatch(
+      async () => {
+        const [exists] = await storage.bucket(bucket).file(path).exists()
+        return exists
+      },
+      (err: Error) => err
+    )
+  }
+
+export const uploadBulkSubmission = upload(storage)(BUCKET_NAME)
+export const doesBulkSubmissionExist = fileExists(storage)(BUCKET_NAME)
