af307b7263fedea2b8fcf446993d96492a3e7f69
Dmitrij Feller <dmitrij@mozillafoundation.org>
2023-05-16 10:34:09 +0200

    style: add newlines, adjust indentation

--- server/src/test/core/bulk-submissions/bulk-submissions.test.ts
+++ server/src/test/core/bulk-submissions/bulk-submissions.test.ts
@@ -1,13 +1,13 @@
-import { createBulkSubmissionFilepath } from "../../../core/bulk-submissions/bulk-submissions"
+import { createBulkSubmissionFilepath } from '../../../core/bulk-submissions/bulk-submissions'
 
 describe('Bulk submission module', () => {
-    it('should return correct filepath', () => {
-        const locale = 'en'
-        const data = 'Sentence\tSource\nHi, how are you?\tself\n'
-        
-        const result = createBulkSubmissionFilepath(locale, data)
-        const expected = 'en/bulk_submission_1664b29821df5b0925f9645c028bbf44.tsv'
+  it('should return correct filepath', () => {
+    const locale = 'en'
+    const data = 'Sentence\tSource\nHi, how are you?\tself\n'
 
-        expect(result).toBe(expected)
-    })
+    const result = createBulkSubmissionFilepath(locale, data)
+    const expected = 'en/bulk_submission_1664b29821df5b0925f9645c028bbf44.tsv'
+
+    expect(result).toBe(expected)
+  })
 })
