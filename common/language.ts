aacf767d138272cc4a3ed91a77f1214b302b1a6a
Rebreda <gh@qxg.ca>
2023-06-22 14:16:28 -0400

    Reformat types without semis

--- common/language.ts
+++ common/language.ts
@@ -1,40 +1,40 @@
 // a single accent object
 export type Accent = {
-  id: number;
-  name: string;
-  token?: string;
-  clientId?: string;
-};
+  id: number
+  name: string
+  token?: string
+  clientId?: string
+}
 
 type SentenceCount = {
-  currentCount: number;
-  targetSentenceCount: number;
-};
+  currentCount: number
+  targetSentenceCount: number
+}
 
 // a single accent object
 export type Language = {
-  id: number;
-  name: string;
-  sentenceCount: SentenceCount;
-  is_contributable?: boolean;
-  is_translated?: boolean;
-  native_name: string;
-  text_direction: string;
-};
+  id: number
+  name: string
+  sentenceCount: SentenceCount
+  is_contributable?: boolean
+  is_translated?: boolean
+  native_name: string
+  text_direction: string
+}
 
 // single variant object
 export type Variant = {
-  id: number;
-  name: string;
-  token: string;
-};
+  id: number
+  name: string
+  token: string
+}
 
 /*
   an object storing all
   accent/locale/variant data for a user
 */
 export type UserLanguage = {
-  locale: string;
-  variant?: Variant;
-  accents?: Accent[];
-};
+  locale: string
+  variant?: Variant
+  accents?: Accent[]
+}
