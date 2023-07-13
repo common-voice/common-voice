292585dd183a38b90a8a0fa6dff4856e29bea09e
Dmitrij Feller <119537889+moz-dfeller@users.noreply.github.com>
2023-07-13 15:31:34 +0200

    fix: update source encoding migration (#4113)

--- server/src/lib/model/db/migrations/20230711121706-fix-citation-encoding.ts
+++ server/src/lib/model/db/migrations/20230711121706-fix-citation-encoding.ts
@@ -1,6 +1,20 @@
+import {} from 'mysql2'
+
 export const up = async function (db: any): Promise<any> {
-  return db.runSql(`
-    ALTER TABLE sentences MODIFY COLUMN source text CHARACTER SET utf8mb4 DEFAULT NULL;
+  await db.runSql(`
+    ALTER TABLE sentences ADD COLUMN source_new text CHARACTER SET utf8mb4 DEFAULT NULL
+  `)
+
+  await db.runSql(`
+    UPDATE sentences SET source_new = source
+  `)
+
+  await db.runSql(`
+    ALTER TABLE sentences CHANGE COLUMN source source_old text
+  `)
+
+  await db.runSql(`
+    ALTER TABLE sentences CHANGE COLUMN source_new source text CHARACTER SET utf8mb4 DEFAULT NULL
   `)
 }
 
