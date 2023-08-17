4e0788a96544d8233fa06338ab01de50aefa071d
Raivis Dejus <raivisd@scandiweb.com>
2023-08-17 18:26:15 +0300

    Adding extra letters to the check

--- server/src/lib/model/db/migrations/20230815103600-hide-problematic-sentences.ts
+++ server/src/lib/model/db/migrations/20230815103600-hide-problematic-sentences.ts
@@ -1,13 +1,13 @@
 export const up = async function (db: any): Promise<any> {
   await db.runSql(`
     UPDATE sentences SET is_used = FALSE
-    WHERE (created_at > '2023-05-01 00:00:00' AND text REGEXP '.*\\\\?[a-z].*') OR text = ''
+    WHERE (created_at > '2023-05-01 00:00:00' AND text REGEXP '.*\\\\?[a-zāčēģīķļņšūžâîûüöç].*') OR text = ''
   `);
 
   await db.runSql(`
     UPDATE sentences SET is_used = FALSE
     WHERE locale_id=(SELECT id from locales WHERE name='lv')
-      AND source REGEXP '.*\\\\?[a-z].*' 
+      AND source REGEXP '.*\\\\?[a-zāčēģīķļņšūžâîûüöç].*' 
   `);
 };
 
