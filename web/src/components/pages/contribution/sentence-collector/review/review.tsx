21f4993136bde503d9082fed6295a21a9c43ab10
adarrssh <adarsh00502@gmail.com>
2023-08-19 20:18:29 +0530

    fix: skip button text overflow

--- web/src/components/pages/contribution/sentence-collector/review/review.tsx
+++ web/src/components/pages/contribution/sentence-collector/review/review.tsx
@@ -303,7 +303,7 @@ const Review: React.FC<Props> = ({ getString }) => {
             data-testid="skip-button">
             <SkipIcon />
             <Localized id="skip">
-              <span />
+              <span className="skip-text" />
             </Localized>{' '}
           </Button>
           <VoteButton
