a84f532c60331c9872bcb4efb99e133c24c76747
Kalepu Satya Sai Teja <mail@kssaiteja.me>
2023-09-19 14:26:47 +0530

    styling updated for new XIcon

--- web/src/components/share-buttons/share-buttons.tsx
+++ web/src/components/share-buttons/share-buttons.tsx
@@ -7,7 +7,7 @@ import * as React from 'react';
 import { useRef } from 'react';
 import { trackSharing } from '../../services/tracker';
 import { Notifications } from '../../stores/notifications';
-import { FontIcon } from '../ui/icons';
+import { FontIcon, XICon } from '../ui/icons';
 import { useLocale } from '../locale-helpers';
 import URLS from '../../urls';
 
@@ -69,9 +69,7 @@ function ShareButtons({ getString, shareTextId }: Props) {
         target="_blank"
         rel="noopener noreferrer"
         onClick={() => trackSharing('twitter', locale)}>
-        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
-          <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/>
-        </svg>
+        <XICon className=""/>
       </a>
     </>
   );
