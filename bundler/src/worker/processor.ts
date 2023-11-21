31c51a1f27fec34dafe9d8bdc1a10060bdc0fef1
Dmitrij Feller <119537889+moz-dfeller@users.noreply.github.com>
2023-11-21 16:45:39 +0100

    Oi 2796 update dataset release process (#4243)
    * feat: add new bundler process
    
    * feat: add storage, update gitignore, add .env, ...
    
    * feat: add config, update query, add step do download clips, add DEL function to redis, etc.
    
    * feat: add corpora creater as dependency to Dockerfile
    
    * feat: update corpora creator dependency
    
    * feat: add corpora creator step
    
    * feat: don't copy the bundler to the cv docker image
    
    * feat: add tar package
    
    * feat: add compress step
    
    * feat: add mp3-duration-reporter dependency
    
    * feat: add mp3 duration and line count functionality
    
    * feat: update stats process
    
    * feat: add get reported sentences step
    
    * feat: add upload step
    
    * feat: add checksum calculation to stats
    
    * refactor: get queries dir from config
    
    * fix: add reported.tsv back
    
    * refactor: use pass environment to calculation
    
    * feat: remove obsolete s3 script
    
    * feat: add common-voice-bundler to prepare_storage script
    
    * feat: add cli command to start dataset release
    
    * feat: add parameter to add job queue name
    
    * feat: pass cli parameters as env to pipeline
    
    * feat: add files to archive based on release type
    
    * fix: remove debug log
    
    * feat: add example .env file

---
+++ bundler/src/worker/processor.ts
@@ -0,0 +1,41 @@
+import { pipe } from 'fp-ts/lib/function'
+import { runFetchAllClipsForLocale } from '../core/clips'
+import { isMinorityLanguage } from '../core/ruleOfFive'
+import { AppEnv, ProcessLocaleJob } from '../types'
+import { Job } from 'bullmq'
+import { runCorporaCreator } from '../infrastructure/corporaCreator'
+import { runCompress } from '../core/compress'
+import { runMp3DurationReporter } from '../infrastructure/mp3DurationReporter'
+import { runStats } from '../core/stats'
+import { runReportedSentences } from '../core/reportedSentences'
+import { runUpload } from '../core/upload'
+import * as RTE from 'fp-ts/readerTaskEither'
+import path from 'node:path'
+
+const processPipeline = pipe(
+  RTE.Do,
+  RTE.bind('isMinorityLanguage', isMinorityLanguage),
+  RTE.chainFirst(({ isMinorityLanguage }) =>
+    runFetchAllClipsForLocale(isMinorityLanguage),
+  ),
+  RTE.bind('totalDurationInMs', runMp3DurationReporter),
+  RTE.chainFirst(runCorporaCreator),
+  RTE.chainFirst(runReportedSentences),
+  RTE.bind('tarFilepath', runCompress),
+  RTE.bind('uploadPath', ({ tarFilepath }) => runUpload(tarFilepath)),
+  RTE.bind('stats', ({ totalDurationInMs, tarFilepath }) =>
+    runStats(totalDurationInMs, tarFilepath),
+  ),
+)
+
+export const processLocale = async (job: Job<ProcessLocaleJob>) => {
+  const { locale, releaseName } = job.data
+  const releaseDirPath = path.join(__dirname, '..', '..', releaseName)
+  const env: AppEnv = {
+    ...job.data,
+    releaseDirPath,
+    releaseTarballsDirPath: path.join(releaseDirPath, 'tarballs'),
+    clipsDirPath: path.join(releaseDirPath, locale, 'clips'),
+  }
+  await processPipeline(env)()
+}
