6f399c907d6c1b56e43818be19f68f3c97ea01ee
moz-rotimib <113194143+moz-rotimib@users.noreply.github.com>
2023-11-27 14:20:47 +0100

    OI 2797: Add E2E tests for Listen page (#4248)
    * test: add initial test for listen page
    
    * test: add tests for more user flows on listen page
    
    * test: add command to run tests

--- web/cypress/e2e/home-page.cy.ts
+++ web/cypress/e2e/home-page.cy.ts
@@ -1,6 +1,6 @@
 describe('The Home Page', () => {
   it('successfully loads', () => {
-    cy.visit('http://localhost:9000')
+    cy.visit('/')
 
     cy.contains('Common Voice')
     cy.contains('Speak')
