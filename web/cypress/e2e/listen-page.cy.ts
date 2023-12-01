36e338e8ab0595915e2d5ff86cf74c28d94b72ad
moz-rotimib <rotimi@mozillafoundation.org>
2023-12-01 17:09:02 +0100

    test: add listenToFiveClips action

--- web/cypress/e2e/listen-page.cy.ts
+++ web/cypress/e2e/listen-page.cy.ts
@@ -1,3 +1,5 @@
+import { listenToFiveClips } from '../actions/listen.action'
+
 describe('The Listen Page', () => {
   it('successfully loads', () => {
     cy.visit('/listen')
@@ -84,31 +86,7 @@ describe('The Listen Page', () => {
   it('listens to 5 clips', () => {
     cy.visit('/listen')
 
-    const playButton = cy.get('[data-testid=play-button]')
-
-    const yesButton = cy.get('[data-testid=vote-yes-button]')
-    const noButton = cy.get('[data-testid=vote-no-button]')
-
-    playButton.click()
-    noButton.click()
-
-    playButton.click()
-    // wait for recording to finish before voting yes
-    cy.wait(3000)
-    yesButton.click()
-
-    playButton.click()
-    noButton.click()
-
-    playButton.click()
-    // wait for recording to finish before voting yes
-    cy.wait(3000)
-    yesButton.click()
-
-    playButton.click()
-    // wait for recording to finish before voting yes
-    cy.wait(3000)
-    yesButton.click()
+    listenToFiveClips()
 
     const successPage = cy.get('[data-testid=contribution-success]')
     successPage.should('exist')
