142ea4bc5731cbe495dacdeb94336778f37a2882
moz-rotimib <rotimi@mozillafoundation.org>
2024-02-02 11:34:05 +0100

    chore: update clashing pontoon key

--- web/src/components/pages/datasets/dataset-download-email-prompt.tsx
+++ web/src/components/pages/datasets/dataset-download-email-prompt.tsx
@@ -223,7 +223,9 @@ const DatasetDownloadEmailPrompt = ({
             </div>
             <div className="donate-btn-container">
               <DonateButton />
-              <Localized id="why-donate" elems={{ b: <strong /> }}>
+              <Localized
+                id="donate-button-explanation"
+                elems={{ b: <strong /> }}>
                 <p className="why-donate" />
               </Localized>
             </div>
