73af512744460f387589b028b8589364df006224
moz-rotimib <rotimi@mozillafoundation.org>
2024-03-11 14:10:46 +0100

    fix: change location of donate banner in datasets page

--- web/src/components/pages/datasets/datasets.tsx
+++ web/src/components/pages/datasets/datasets.tsx
@@ -3,17 +3,11 @@ import * as React from 'react';
 import DatasetInfo from './dataset-info';
 import Resources from './resources';
 import Page from '../../ui/page';
-import { DonateBanner } from '../../donate-banner';
-
-import './datasets.css';
 
 const Datasets = () => (
   <Page className="datasets-content">
     <DatasetInfo />
     <Resources />
-    <section className="donate-banner-section">
-      <DonateBanner />
-    </section>
   </Page>
 );
 
