30885acf9eeeb2b0dea091ba12c0b38d5a75e947
moz-rotimib <rotimi@mozillafoundation.org>
2024-03-22 17:48:08 +0100

    chore: update metadata viewer to show all sex/gender options

--- web/src/components/pages/datasets/metadata-viewer/gender-splits.tsx
+++ web/src/components/pages/datasets/metadata-viewer/gender-splits.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useLocalization } from '@fluent/react'
+import { Localized, useLocalization } from '@fluent/react'
 
 import { Gender } from './types'
 import { formatNumberToPercentage } from '../../../../utility'
@@ -17,6 +17,20 @@ export const GenderSplits = ({ genderSplits }: Props) => {
         el =>
           el.length > 0 &&
           genderSplits[el] > 0 && (
+            <p key={el} className="gender">
+              <span>{formatNumberToPercentage(genderSplits[el])}</span>
+              {l10n.getString(el)}
+            </p>
+          )
+      )}
+
+      <Localized id="no-information">
+        <p className="no-information" />
+      </Localized>
+      {(Object.keys(genderSplits) as Array<keyof typeof genderSplits>).map(
+        el =>
+          el.length > 0 &&
+          genderSplits[el] === 0 && (
             <p key={el} className="gender">
               <span>{formatNumberToPercentage(genderSplits[el])}</span>
               {l10n.getString(el)}
