351c7583678d4fd5a65de15ba7071bfd1b038813
moz-rotimib <rotimi@mozillafoundation.org>
2024-04-02 14:41:28 +0200

    feat: add Radio component - add content to variant contribution options in user profile

--- web/src/components/pages/profile/info/variant-contribution-options/index.tsx
+++ web/src/components/pages/profile/info/variant-contribution-options/index.tsx
@@ -1,12 +1,41 @@
 import * as React from 'react'
+import { Localized } from '@fluent/react'
+
+import { Radio } from '../../../../ui/ui'
 
 import './variant-contribution-options.css'
 
 export const VariantContributionOptions = () => {
   return (
     <div className="variant-contribution-options-container">
-      <h3>Variant Contribution Options</h3>
-      <div className="form-fields">ldld</div>
+      <Localized id="variant-contribution-options-header">
+        <h3 />
+      </Localized>
+      <div className="form-fields">
+        <Localized id="variant-contribution-help">
+          <p className="variant-contribution-help" />
+        </Localized>
+
+        <div className="radio-container">
+          <Radio
+            contentClass="radio-content"
+            labelClass="radio-label"
+            name="variant-contribution-option">
+            <Localized id="variant-contribution-option-1">
+              <span />
+            </Localized>
+          </Radio>
+          <Radio
+            contentClass="radio-content"
+            labelClass="radio-label"
+            name="variant-contribution-option"
+            checked>
+            <Localized id="variant-contribution-option-2">
+              <span />
+            </Localized>
+          </Radio>
+        </div>
+      </div>
     </div>
   )
 }
