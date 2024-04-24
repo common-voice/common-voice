e3c40767762cd86ae4c0c8afe955e4e4eb4e6ffc
moz-rotimib <rotimi@mozillafoundation.org>
2024-04-24 16:19:59 +0100

    fix: fix React import for test

--- web/src/components/pages/contribution/tag.tsx
+++ web/src/components/pages/contribution/tag.tsx
@@ -1,17 +1,15 @@
-import React from 'react'
+import * as React from 'react'
 import { Tooltip } from 'react-tippy'
 
 import { DropdownArrowIcon } from '../../ui/icons'
 
 import './tag.css'
 
-const tooltipContent = <div>Tooltip content Rotimi</div>
-
 export const Tag = () => (
   <div className="tag-container">
     <Tooltip
       trigger="click"
-      html={tooltipContent}
+      html={<div>Tooltip content Rotimi</div>}
       style={{ display: 'inherit', gap: '8px' }}
       position="bottom"
       theme="light"
