6b9f57ceeb0dabca78e3e5b2b1a661eb22879a68
moz-rotimib <rotimi@mozillafoundation.org>
2024-10-01 17:58:11 +0200

    test: fix failing tests

--- web/src/components/pages/contribution/sentence-collector/write/sentence-input-and-rules/single-write-rules.tsx
+++ web/src/components/pages/contribution/sentence-collector/write/sentence-input-and-rules/single-write-rules.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import * as React from 'react'
 import { Localized } from '@fluent/react'
 import classNames from 'classnames'
 
@@ -22,7 +22,7 @@ export const SinglewriteRules = ({
   title,
   mode,
 }: Props) => {
-  const [rulesVisible, setRulesVisible] = useState(true)
+  const [rulesVisible, setRulesVisible] = React.useState(true)
 
   const handleClick = () => {
     setRulesVisible(!rulesVisible)
