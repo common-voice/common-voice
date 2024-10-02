5570899dbac5d5fa24bf4594ebf5166de1211b18
moz-rotimib <rotimi@mozillafoundation.org>
2024-10-02 16:49:57 +0200

    test(write page): update write page test

--- web/src/components/pages/contribution/sentence-collector/write/sentence-input-and-rules/small-batch-rules.tsx
+++ web/src/components/pages/contribution/sentence-collector/write/sentence-input-and-rules/small-batch-rules.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import * as React from 'react'
 import { Localized } from '@fluent/react'
 import classNames from 'classnames'
 
@@ -10,7 +10,7 @@ type Props = {
 }
 
 export const SmallBatchRules = ({ title }: Props) => {
-  const [rulesVisible, setRulesVisible] = useState(true)
+  const [rulesVisible, setRulesVisible] = React.useState(true)
 
   const handleClick = () => {
     setRulesVisible(!rulesVisible)
