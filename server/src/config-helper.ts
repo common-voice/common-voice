373b6a24abb0745e4f582d72bd9a85fc733440fe
Dmitrij Feller <dmitrij@mozillafoundation.org>
2024-10-04 09:20:42 +0200

    chore: replace auth0 with fxa in the config

--- server/src/config-helper.ts
+++ server/src/config-helper.ts
@@ -1,64 +1,64 @@
-import * as fs from 'fs';
-import { SESClientConfig } from '@aws-sdk/client-ses';
-import { config } from 'dotenv';
+import * as fs from 'fs'
+import { SESClientConfig } from '@aws-sdk/client-ses'
+import { config } from 'dotenv'
 
 if (process.env.DOTENV_CONFIG_PATH) {
-  const result = config({ path: process.env.DOTENV_CONFIG_PATH });
+  const result = config({ path: process.env.DOTENV_CONFIG_PATH })
   if (result.error) {
-    console.log(result.error);
-    console.log('Failed loading dotenv file, using defaults');
+    console.log(result.error)
+    console.log('Failed loading dotenv file, using defaults')
   }
 }
 
 export type CommonVoiceConfig = {
-  VERSION: string;
-  PROD: boolean;
-  SERVER_PORT: number;
-  DB_ROOT_USER: string;
-  DB_ROOT_PASS: string;
-  MYSQLUSER: string;
-  MYSQLPASS: string;
-  MYSQLDBNAME: string;
-  MYSQLHOST: string;
-  MYSQLREPLICAHOST?: string;
-  MYSQLPORT: number;
-  MYSQLREPLICAPORT?: number;
-  CLIP_BUCKET_NAME: string;
-  DATASET_BUCKET_NAME: string;
-  BULK_SUBMISSION_BUCKET_NAME: string;
-  AWS_REGION: string;
-  ENVIRONMENT: string;
-  RELEASE_VERSION?: string;
-  SECRET: string;
-  AWS_SES_CONFIG: SESClientConfig;
-  STORAGE_LOCAL_DEVELOPMENT_ENDPOINT: string;
-  GCP_CREDENTIALS: object;
-  ADMIN_EMAILS: string;
-  AUTH0: {
-    DOMAIN: string;
-    CLIENT_ID: string;
-    CLIENT_SECRET: string;
-  };
-  BASKET_API_KEY?: string;
-  IMPORT_SENTENCES: boolean;
-  REDIS_URL: string;
-  LAST_DATASET: string;
-  SENTRY_DSN_SERVER: string;
-  MAINTENANCE_MODE: boolean;
-  DEBUG: boolean;
-  FLAG_BUFFER_STREAM_ENABLED: boolean;
-  EMAIL_USERNAME_FROM: string;
-  EMAIL_USERNAME_TO: string;
-};
+  VERSION: string
+  PROD: boolean
+  SERVER_PORT: number
+  DB_ROOT_USER: string
+  DB_ROOT_PASS: string
+  MYSQLUSER: string
+  MYSQLPASS: string
+  MYSQLDBNAME: string
+  MYSQLHOST: string
+  MYSQLREPLICAHOST?: string
+  MYSQLPORT: number
+  MYSQLREPLICAPORT?: number
+  CLIP_BUCKET_NAME: string
+  DATASET_BUCKET_NAME: string
+  BULK_SUBMISSION_BUCKET_NAME: string
+  AWS_REGION: string
+  ENVIRONMENT: string
+  RELEASE_VERSION?: string
+  SECRET: string
+  AWS_SES_CONFIG: SESClientConfig
+  STORAGE_LOCAL_DEVELOPMENT_ENDPOINT: string
+  GCP_CREDENTIALS: object
+  ADMIN_EMAILS: string
+  FXA: {
+    DOMAIN: string
+    CLIENT_ID: string
+    CLIENT_SECRET: string
+  }
+  BASKET_API_KEY?: string
+  IMPORT_SENTENCES: boolean
+  REDIS_URL: string
+  LAST_DATASET: string
+  SENTRY_DSN_SERVER: string
+  MAINTENANCE_MODE: boolean
+  DEBUG: boolean
+  FLAG_BUFFER_STREAM_ENABLED: boolean
+  EMAIL_USERNAME_FROM: string
+  EMAIL_USERNAME_TO: string
+}
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const castDefault = (value: string): any => value;
-const castBoolean = (value: string): boolean => value === 'true';
-const castInt = (value: string): number => parseInt(value);
-const castJson = (value: string): object => JSON.parse(value);
+const castDefault = (value: string): any => value
+const castBoolean = (value: string): boolean => value === 'true'
+const castInt = (value: string): number => parseInt(value)
+const castJson = (value: string): object => JSON.parse(value)
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const configEntry = (key: string, defaultValue: any, cast = castDefault) =>
-  process.env[key] ? cast(process.env[key]) : defaultValue;
+  process.env[key] ? cast(process.env[key]) : defaultValue
 
 const BASE_CONFIG: CommonVoiceConfig = {
   VERSION: configEntry('CV_VERSION', null), // Migration number (e.g. 20171205171637), null = most recent
@@ -93,10 +93,10 @@ const BASE_CONFIG: CommonVoiceConfig = {
     'http://localhost:8080'
   ),
   GCP_CREDENTIALS: configEntry('CV_GCP_CREDENTIALS', {}, castJson),
-  AUTH0: {
-    DOMAIN: configEntry('CV_AUTH0_DOMAIN', ''),
-    CLIENT_ID: configEntry('CV_AUTH0_CLIENT_ID', ''),
-    CLIENT_SECRET: configEntry('CV_AUTH0_CLIENT_SECRET', ''),
+  FXA: {
+    DOMAIN: configEntry('CV_FXA_DOMAIN', ''),
+    CLIENT_ID: configEntry('CV_FXA_CLIENT_ID', ''),
+    CLIENT_SECRET: configEntry('CV_FXA_CLIENT_SECRET', ''),
   },
   IMPORT_SENTENCES: configEntry('CV_IMPORT_SENTENCES', true, castBoolean),
   REDIS_URL: configEntry('CV_REDIS_URL', null),
@@ -112,35 +112,35 @@ const BASE_CONFIG: CommonVoiceConfig = {
   ),
   EMAIL_USERNAME_FROM: configEntry('CV_EMAIL_USERNAME_FROM', null),
   EMAIL_USERNAME_TO: configEntry('CV_EMAIL_USERNAME_TO', null),
-};
+}
 
-let injectedConfig: CommonVoiceConfig;
-let loadedConfig: CommonVoiceConfig;
+let injectedConfig: CommonVoiceConfig
+let loadedConfig: CommonVoiceConfig
 
 export function injectConfig(config: Partial<CommonVoiceConfig>) {
-  injectedConfig = { ...BASE_CONFIG, ...config };
+  injectedConfig = { ...BASE_CONFIG, ...config }
 }
 
 export function getConfig(): CommonVoiceConfig {
   if (injectedConfig) {
-    return injectedConfig;
+    return injectedConfig
   }
 
   if (loadedConfig) {
-    return loadedConfig;
+    return loadedConfig
   }
 
-  let fileConfig = null;
+  let fileConfig = null
 
   try {
-    const config_path = process.env.SERVER_CONFIG_PATH || './config.json';
-    fileConfig = JSON.parse(fs.readFileSync(config_path, 'utf-8'));
+    const config_path = process.env.SERVER_CONFIG_PATH || './config.json'
+    fileConfig = JSON.parse(fs.readFileSync(config_path, 'utf-8'))
   } catch (err) {
     console.error(
       `Could not load config.json, using defaults (error message: ${err.message})`
-    );
+    )
   }
-  loadedConfig = { ...BASE_CONFIG, ...fileConfig };
+  loadedConfig = { ...BASE_CONFIG, ...fileConfig }
 
-  return loadedConfig;
+  return loadedConfig
 }
