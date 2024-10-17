ec2bed0b33236a031018f269bf75cc3c428e058b
moz-rotimib <rotimi@mozillafoundation.org>
2024-10-17 18:10:47 +0200

    fix failing tests

--- web/src/components/layout/nav/contribute-menu/contribute-menu.test.tsx
+++ web/src/components/layout/nav/contribute-menu/contribute-menu.test.tsx
@@ -3,6 +3,7 @@ import { fireEvent, screen } from '@testing-library/react'
 
 import ContributeMenu from '.'
 import { renderWithProviders } from '../../../../../test/render-with-providers'
+import { menuItems } from '../menu-items'
 
 describe('Contribute Menu Test', () => {
   it('renders Contribute Menu', async () => {
@@ -13,18 +14,16 @@ describe('Contribute Menu Test', () => {
         showMobileMenu={false}
         isContributionPageActive={false}
         isUserLoggedIn
-        menuItems={[]}
+        menuItems={menuItems['speak'].items}
         menuLabel="speak"
       />
     )
 
     expect(screen.queryByTestId('contribute-menu')).toBeTruthy()
-    expect(await screen.findByText('Speak')).toBeTruthy()
-    expect(await screen.findByText('Listen')).toBeTruthy()
-    expect(await screen.findByText('Write')).toBeTruthy()
+    expect(await screen.findAllByText('Speak')).toBeTruthy()
   })
 
-  it('shows the menu on hover', () => {
+  it('shows the menu on click', () => {
     const setShowMenuSpy = jest.fn()
 
     renderWithProviders(
@@ -34,31 +33,12 @@ describe('Contribute Menu Test', () => {
         showMobileMenu={false}
         isContributionPageActive={false}
         isUserLoggedIn
-        menuItems={[]}
+        menuItems={menuItems['speak'].items}
         menuLabel="speak"
       />
     )
 
-    fireEvent.mouseEnter(screen.getByTestId('contribute-menu'))
-    expect(setShowMenuSpy).toHaveBeenCalledWith(true)
-  })
-
-  it('does not show the menu on hover if a contribution page is active', () => {
-    const setShowMenuSpy = jest.fn()
-
-    renderWithProviders(
-      <ContributeMenu
-        showMenu={false}
-        setShowMenu={setShowMenuSpy}
-        showMobileMenu={false}
-        isContributionPageActive
-        isUserLoggedIn
-        menuItems={[]}
-        menuLabel="speak"
-      />
-    )
-
-    fireEvent.mouseEnter(screen.getByTestId('contribute-menu'))
-    expect(setShowMenuSpy).not.toHaveBeenCalled()
+    fireEvent.click(screen.getByTestId('contribute-menu'))
+    expect(setShowMenuSpy).toHaveBeenCalledWith('speak')
   })
 })
