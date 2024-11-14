a4d9066276b5d62c731869fefe2ab6a9a91edbd3
Dmitrij Feller <dmitrij@mozillafoundation.org>
2024-11-14 10:02:12 +0100

    feat: add fxa event handler

--- server/src/api/webhooks/handler/post-events-handler.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from 'express'
-
-export const postEventsHandler = async (req: Request, res: Response) => {
-  res.sendStatus(200)
-}
--- server/src/api/webhooks/routes.ts
+++ server/src/api/webhooks/routes.ts
@@ -1,9 +1,11 @@
-import { postEventsHandler } from './handler/post-events-handler'
+import { postFxaEventsHandler } from './handler/post-fxa-events-handler'
 import PromiseRouter from 'express-promise-router'
 import rateLimiter from '../../lib/rate-limiter-middleware'
+import { authenticateFxaEvent } from './validation/authenticate-fxa-event'
 
 export default PromiseRouter({ mergeParams: true }).post(
-  '/events',
-  rateLimiter('api/v1/webhooks/events', { points: 1000, duration: 60 }),
-  postEventsHandler
+  '/fxa/events',
+  rateLimiter('api/v1/webhooks/fxa/events', { points: 1000, duration: 60 }),
+  authenticateFxaEvent,
+  postFxaEventsHandler
 )
