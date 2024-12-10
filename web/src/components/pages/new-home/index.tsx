83b913e19dddf1b637ac82ce9efa405898bd70b7
moz-rotimib <rotimi@mozillafoundation.org>
2024-12-10 14:17:14 +0100

    feat(home page): add partner with us section

--- web/src/components/pages/new-home/index.tsx
+++ web/src/components/pages/new-home/index.tsx
@@ -6,6 +6,7 @@ import { CommunitySection } from './community-section'
 import { FeaturedSection } from './featured-section'
 import { DevelopersSection } from './developers-section'
 import { GetInvolvedSection } from './get-involved'
+import { PartnerSection } from './partner-section'
 
 import Page from '../../ui/page'
 
@@ -22,6 +23,7 @@ const Home = () => {
       <FeaturedSection />
       <DevelopersSection />
       <GetInvolvedSection />
+      <PartnerSection />
     </Page>
   )
 }
