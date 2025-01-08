bb21539088e9fc1208ddc8ce61c815e06daf6c67
moz-rotimib <rotimi@mozillafoundation.org>
2025-01-08 16:21:33 +0100

    chore(home page): update logos for our partners section

--- web/src/components/pages/new-home/partners-section/index.tsx
+++ web/src/components/pages/new-home/partners-section/index.tsx
@@ -11,14 +11,27 @@ export const PartnersSection = () => {
           <h2 className="partners-title" />
         </Localized>
         <div className="partners-logos">
-          <img src={require('./assets/collectivat.svg')} alt="" />
-          <img src={require('./assets/nvidia.svg')} alt="" />
-          <img src={require('./assets/mak.svg')} alt="" />
-          <img src={require('./assets/ff.svg')} alt="" />
-          <img src={require('./assets/giz.svg')} alt="" />
           <img
-            src={require('./assets/Bill_&_Melinda_Gates_Foundation.svg')}
+            src={require('./assets/collectivat-small.jpg')}
             alt=""
+            className="logo collectivat"
+          />
+          <img src={require('./assets/nvidia-logo.svg')} alt="" />
+          <img
+            src={require('./assets/mak-logo-small.jpg')}
+            alt=""
+            className="logo mak"
+          />
+          <img
+            src={require('./assets/ff-logo.svg')}
+            alt=""
+            className="logo ff"
+          />
+          <img src={require('./assets/giz-logo.jpg')} alt="" className="logo" />
+          <img
+            src={require('./assets/Bill_&_Melinda_Gates_Foundation_logo.jpg')}
+            alt=""
+            className="logo"
           />
         </div>
       </div>
