5eb635d9c83f121b5e8bdc4c1f40762ed5dc5373
Dmitrij Feller <dmitrij@mozillafoundation.org>
2025-01-08 14:42:13 +0100

    feat: round hours that are less than 10h to one decimal

--- server/src/lib/utils/secondsToHours.ts
+++ server/src/lib/utils/secondsToHours.ts
@@ -1,11 +1,17 @@
-const HOUR_IN_SECONDS = 3600;
+const HOUR_IN_SECONDS = 3600
 
 /**
- * Converts seconds to hours, defaults to 0 if no seconds provided. Rounds up to nears whole number.
- * 2.1 hours will be returned as 3
+ * Converts seconds to hours, defaults to 0 if no seconds provided. Rounds up to nears whole number for
+ * durations longer than 10 hours. Durations shorter than 10h will be displayed with a decimal.
+ * @example
+ * 2.1 hours will be returned as 2.1 hours
+ * 12.1 hours will be returned as 13 hours
  * @param {number} seconds
  * @return {number} hours
  */
 export const secondsToHours = (seconds: number): number => {
-  return Math.ceil(seconds / HOUR_IN_SECONDS) || 0;
-};
+  if (seconds <= 0) return 0
+
+  const hours = seconds / HOUR_IN_SECONDS
+  return hours >= 10 ? Math.ceil(hours) : parseFloat(hours.toFixed(1))
+}
