703b06e159a1c2f6149b28bf8578538a96a24336
moz-rotimib <rotimi@mozillafoundation.org>
2025-01-28 15:56:53 +0100

    chore: update custom tracking for listen page

--- web/src/services/tracker-ga4.ts
+++ web/src/services/tracker-ga4.ts
@@ -3,6 +3,9 @@ type GA4Events =
   | 'rerecord-clip'
   | 'submit-clips'
   | 'discard-ongoing'
+  | 'listen-clip'
+  | 'vote-yes'
+  | 'vote-no'
 
 export const trackGtag = (
   eventName: GA4Events,
