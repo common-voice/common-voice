15ab8d822e43ddb0b2d9390385d6f155019e0892
moz-rotimib <rotimi@mozillafoundation.org>
2025-02-19 11:04:37 -0500

    865 options to select spontaneous or scripted on cv request a language page (#4800)
    * feat(request language form): update request language form with toggle for scripted and spontaneous speech
    
    * fix(language request): css fixes for language request page
    
    * fix(request language): fix test for request language page
    
    * feat(request language page): fixes for request language page
    - fix test for request language page
    - add ga4 tracking
    - add platforms to BE request
    
    * fix(request language page): add max width to toggle label

--- web/src/components/pages/languages/request/request.test.tsx
+++ web/src/components/pages/languages/request/request.test.tsx
@@ -1,114 +1,133 @@
-import * as React from 'react';
-import * as ReactRouterDom from 'react-router-dom';
-import { axe, toHaveNoViolations } from 'jest-axe';
-import { act, RenderResult } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import * as React from 'react'
+import * as ReactRouterDom from 'react-router-dom'
+import { axe, toHaveNoViolations } from 'jest-axe'
+import { act, RenderResult } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
-import { renderWithProviders } from '../../../../../test/render-with-providers';
+import { renderWithProviders } from '../../../../../test/render-with-providers'
 
-expect.extend(toHaveNoViolations);
+expect.extend(toHaveNoViolations)
 
-import LangugagesRequestFormPage from './request';
+import LangugagesRequestFormPage from './request'
 
-jest.mock('../../../../logger');
+jest.mock('../../../../logger')
 
 // mock window.scrollTo
-global.scrollTo = jest.fn();
+global.scrollTo = jest.fn()
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const mockUseHistoryPush = jest.fn(() => null as any);
+const mockUseHistoryPush = jest.fn(() => null as any)
 jest.mock('react-router-dom', () => ({
   ...(jest.requireActual('react-router-dom') as typeof ReactRouterDom),
   useHistory: () => ({
     push: mockUseHistoryPush,
   }),
-}));
+}))
 
-const mockSendLanguageRequest = jest.fn(() => Promise.resolve());
+const mockSendLanguageRequest = jest.fn(() => Promise.resolve())
 jest.mock('../../../../hooks/store-hooks', () => ({
   useAPI: () => ({
     sendLanguageRequest: mockSendLanguageRequest,
   }),
-}));
+}))
 
 async function fillInForm({
   getByRole,
   getByLabelText,
+  getByTestId,
 }: {
-  getByRole: any; // eslint-disable-line @typescript-eslint/no-explicit-any
-  getByLabelText: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+  getByRole: any // eslint-disable-line @typescript-eslint/no-explicit-any
+  getByLabelText: any // eslint-disable-line @typescript-eslint/no-explicit-any
+  getByTestId?: any // eslint-disable-line @typescript-eslint/no-explicit-any
 }) {
   // fill in main form
-  userEvent.type(getByLabelText(/Your email address/), 'billgates@example.com');
+  userEvent.type(getByLabelText(/Your email address/), 'billgates@example.com')
+
+  if (getByTestId) {
+    userEvent.click(getByTestId('request-for-scripted-speech-toggle'))
+  }
+
   userEvent.type(
     getByLabelText(/Information about the language/),
     'The language is JavaScript lol!'
-  );
+  )
   userEvent.click(
     getByLabelText(
       /I'm okay with you handling this info as you explain in Mozilla's Privacy Policy/
     )
-  );
+  )
 
   // submit
   await act(async () => {
-    await userEvent.click(getByRole('button', { name: 'Submit' }));
-  });
+    await userEvent.click(getByRole('button', { name: 'Submit' }))
+  })
 }
 
 describe('LanguagesRequestFormPage', () => {
   afterEach(() => {
-    jest.resetAllMocks();
-  });
+    jest.resetAllMocks()
+  })
 
   it('should render with no accessibility violations', async () => {
-    let renderResult: RenderResult;
+    let renderResult: RenderResult
     act(() => {
-      renderResult = renderWithProviders(<LangugagesRequestFormPage />);
-    });
-    const results = await axe(renderResult.container);
-    expect(results).toHaveNoViolations();
-  });
+      renderResult = renderWithProviders(<LangugagesRequestFormPage />)
+    })
+    const results = await axe(renderResult.container)
+    expect(results).toHaveNoViolations()
+  })
 
   it('submits valid data', async () => {
-    let renderResult: RenderResult;
+    let renderResult: RenderResult
     act(() => {
-      renderResult = renderWithProviders(<LangugagesRequestFormPage />);
-    });
-    const { getByRole, getByLabelText } = renderResult;
+      renderResult = renderWithProviders(<LangugagesRequestFormPage />)
+    })
+    const { getByRole, getByLabelText, getByTestId } = renderResult
 
-    await fillInForm({ getByRole, getByLabelText });
+    await fillInForm({ getByRole, getByLabelText, getByTestId })
 
     // correctly sends email request to server
     await expect(mockSendLanguageRequest).toBeCalledWith({
       email: 'billgates@example.com',
       languageInfo: 'The language is JavaScript lol!',
       languageLocale: 'en-US',
-    });
+      platforms: ['scripted-speech'],
+    })
 
     // redirects to success page
     await expect(mockUseHistoryPush).toBeCalledWith(
       '/en/language/request/success'
-    );
-  });
+    )
+  })
+
+  it('disables the submit button if no toggle option is selected', async () => {
+    let renderResult: RenderResult
+    act(() => {
+      renderResult = renderWithProviders(<LangugagesRequestFormPage />)
+    })
+
+    const { getByTestId } = renderResult
+    // button should be disabled before the toggles are selected
+    expect(getByTestId('request-language-btn').hasAttribute('disabled'))
+  })
 
   it('shows error if server endpoint errors', async () => {
     mockSendLanguageRequest.mockImplementation(
       jest.fn(() => Promise.reject(new Error('Something Went Wrong')))
-    );
+    )
 
-    let renderResult: RenderResult;
+    let renderResult: RenderResult
     act(() => {
-      renderResult = renderWithProviders(<LangugagesRequestFormPage />);
-    });
-    const { getByRole, getByLabelText, queryByText } = renderResult;
+      renderResult = renderWithProviders(<LangugagesRequestFormPage />)
+    })
+    const { getByRole, getByLabelText, queryByText, getByTestId } = renderResult
 
-    await fillInForm({ getByRole, getByLabelText });
+    await fillInForm({ getByRole, getByLabelText, getByTestId })
 
     // no redirect
-    await expect(mockUseHistoryPush).not.toBeCalled();
+    await expect(mockUseHistoryPush).not.toBeCalled()
 
     // showing error message instead
-    expect(queryByText(/Sorry, something went wrong/)).toBeTruthy();
-  });
-});
+    expect(queryByText(/Sorry, something went wrong/)).toBeTruthy()
+  })
+})
---
+++ web/src/components/pages/languages/request/toggle.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react'
+import { useLocalization } from '@fluent/react'
+
+import './toggle.css'
+
+type ToggleProps = {
+  label: string
+  checked: boolean
+  onToggle: (checked: boolean) => void
+}
+
+const Toggle: React.FC<ToggleProps> = ({ label, checked, onToggle }) => {
+  const { l10n } = useLocalization()
+
+  const handleToggle = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    onToggle(evt.target.checked)
+  }
+
+  return (
+    <div className="toggle-row">
+      <span>{l10n.getString(label)}</span>
+      <label className="switch" htmlFor={`toggle-${label}`}>
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={handleToggle}
+          id={`toggle-${label}`}
+          aria-label={l10n.getString(label)}
+          data-testid={label}
+        />
+        <span className="slider"></span>
+      </label>
+    </div>
+  )
+}
+
+export default Toggle
