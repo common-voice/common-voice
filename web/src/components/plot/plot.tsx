c0cd20043e893a3af8983839403f00f6f19980f5
Abdelrahman Haridy <abdelrahman.haridy01@gmail.com>
2025-02-22 18:59:07 +0300

    Update hero banner

--- web/src/components/plot/plot.tsx
+++ web/src/components/plot/plot.tsx
@@ -138,13 +138,13 @@ const BAR_HEIGHT = TOTAL_LINE_MARGIN * ((TICK_COUNT - 1) / TICK_COUNT);
 // if window width is mobile, BAR_COUNT should be 5 only
 const BAR_COUNT_MOBILE = 5;
 
-function getBarX(i: number) {
-  const barCount = window.innerWidth < 768 ? BAR_COUNT_MOBILE : BAR_COUNT;
-  return (
-    LINE_OFFSET +
-    i * ((window.innerWidth - PLOT_PADDING - TEXT_OFFSET) / barCount)
-  );
-}
+// function getBarX(i: number) {
+//   const barCount = window.innerWidth < 768 ? BAR_COUNT_MOBILE : BAR_COUNT;
+//   return (
+//     LINE_OFFSET +
+//     i * ((window.innerWidth - PLOT_PADDING - TEXT_OFFSET) / barCount)
+//   );
+// }
 
 function formatNumber(n: number) {
   return n > 1000 ? Math.round(n / 1000) + 'k' : n.toString();
@@ -177,9 +177,15 @@ export const BarPlot = ({
     {({ max, width }) => {
       const barWidth = width < 400 ? BAR_WIDTH_XS : BAR_WIDTH_LG;
 
+      const getBarX = (i: number) =>
+        LINE_OFFSET +
+        PLOT_PADDING -
+        barWidth / 2 +
+        (i * (width - PLOT_PADDING - TEXT_OFFSET)) / BAR_COUNT;
+
       return (
         <>
-          {Array.from({ length: BAR_COUNT_MOBILE }).map((_, i) => (
+          {Array.from({ length: BAR_COUNT }).map((_, i) => (
             <rect
               key={i}
               className="bg"
