7cd9aaf332f2a85f443f83599dce91870ac70d06
moz-rotimib <rotimi@mozillafoundation.org>
2025-03-13 17:07:27 +0100

    fix(languages page): fix get involved button and modal on languages page

--- web/src/components/pages/languages/language-card/language-card.tsx
+++ web/src/components/pages/languages/language-card/language-card.tsx
@@ -1,22 +1,21 @@
-import * as React from 'react';
-import { Localized } from '@fluent/react';
-import classNames from 'classnames';
+import * as React from 'react'
+import { Localized } from '@fluent/react'
 
-import { LanguageStatistics } from 'common';
-import { createCrossLocalization } from '../../../../services/localization';
-import { useAvailableLocales, useLocale } from '../../../locale-helpers';
-import { ModalOptions } from '../languages';
+import { LanguageStatistics } from 'common'
+import { createCrossLocalization } from '../../../../services/localization'
+import { useAvailableLocales, useLocale } from '../../../locale-helpers'
+import { ModalOptions } from '../languages'
 
-import LanguageCardCTA from './cta';
-import LanguageCardData from './data';
+import LanguageCardCTA from './cta'
+import LanguageCardData from './data'
 
-import styles from './language-card.module.css';
+import styles from './language-card.module.css'
 
 interface LanguageCardProps {
-  type: 'launched' | 'in-progress';
-  localeMessages: string[][];
-  language: LanguageStatistics;
-  setModalOptions?: ({ locale, l10n }: ModalOptions) => any; // eslint-disable-line @typescript-eslint/no-explicit-any
+  type: 'launched' | 'in-progress'
+  localeMessages: string[][]
+  language: LanguageStatistics
+  setModalOptions?: ({ locale, l10n }: ModalOptions) => any // eslint-disable-line @typescript-eslint/no-explicit-any
 }
 
 const LanguageCard = ({
@@ -25,14 +24,14 @@ const LanguageCard = ({
   language,
   setModalOptions,
 }: LanguageCardProps) => {
-  const [globalLocale] = useLocale();
-  const availableLocales = useAvailableLocales();
+  const [globalLocale] = useLocale()
+  const availableLocales = useAvailableLocales()
 
   const l10n = createCrossLocalization(
     localeMessages,
     [language.locale, globalLocale],
     availableLocales
-  );
+  )
 
   return (
     <div className={styles.LanguageCard}>
@@ -47,16 +46,15 @@ const LanguageCard = ({
       <LanguageCardCTA
         type={type}
         locale={language.locale}
-        l10n={l10n}
         onClick={() => {
           if (type === 'in-progress') {
             // show modal
-            setModalOptions({ locale: language.locale, l10n });
+            setModalOptions({ locale: language.locale, l10n })
           }
         }}
       />
     </div>
-  );
-};
+  )
+}
 
-export default LanguageCard;
+export default LanguageCard
