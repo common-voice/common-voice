fa67bfb89d52c5b123388a81a89011b36fc40a00
moz-rotimib <rotimi@mozillafoundation.org>
2025-03-31 12:49:54 +0200

    feat(datasets download page): update download page with Spontaneous speech announcement

--- web/src/components/pages/datasets/dataset-intro.tsx
+++ web/src/components/pages/datasets/dataset-intro.tsx
@@ -1,43 +1,58 @@
-import * as React from 'react';
-import { Localized } from '@fluent/react';
-import { useState } from 'react';
-import { TextButton } from '../../ui/ui';
-import PageHeading from '../../ui/page-heading';
+import * as React from 'react'
+import { Localized } from '@fluent/react'
+import { useState } from 'react'
+import classNames from 'classnames'
+import { TextButton } from '../../ui/ui'
+import PageHeading from '../../ui/page-heading'
 
-import './dataset-intro.css';
-import classNames from 'classnames';
+import './dataset-intro.css'
 
 const DatasetIntro = () => {
-  const [showIntroTextMdDown, setShow] = useState(false);
+  const [showIntroTextMdDown, setShow] = useState(false)
 
   return (
     <div className="dataset-intro">
-      <div>
-        <PageHeading isLight>
-          <Localized id="datasets-heading" />
-          <p className="intro-summary">
-            <Localized id="datasets-headline" />
-          </p>
-        </PageHeading>
-      </div>
+      <div className="heading-and-intro-container">
+        <div>
+          <PageHeading isLight>
+            <Localized id="datasets-heading" />
+            <p className="intro-summary">
+              <Localized id="datasets-headline" />
+            </p>
+          </PageHeading>
+        </div>
 
-      <div>
-        {!showIntroTextMdDown && (
-          <Localized id="show-wall-of-text">
-            <TextButton
-              className="hidden-lg-up"
-              onClick={() => setShow(true)}
+        <div>
+          {!showIntroTextMdDown && (
+            <Localized id="show-wall-of-text">
+              <TextButton
+                className="hidden-lg-up"
+                onClick={() => setShow(true)}
+              />
+            </Localized>
+          )}
+          <Localized id="datasets-positioning">
+            <p
+              className={classNames({ 'hidden-md-down': !showIntroTextMdDown })}
             />
           </Localized>
-        )}
-        <Localized id="datasets-positioning">
-          <p
-            className={classNames({ 'hidden-md-down': !showIntroTextMdDown })}
-          />
-        </Localized>
+        </div>
+      </div>
+      <div className="mars-container">
+        <div className="speech-bubble-container">
+          <Localized id="datasets-speech-bubble-content">
+            <p />
+          </Localized>
+        </div>
+        <img
+          src={require('./images/tail.svg')}
+          alt=""
+          className="speech-bubble-tail"
+        />
+        <img src={require('./images/waving-mars.svg')} alt="" />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default DatasetIntro;
+export default DatasetIntro
