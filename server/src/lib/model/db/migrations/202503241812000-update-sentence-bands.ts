9137a6de6840b57e35421de0252f74319affb09a
Francis Tyers <ftyers@users.noreply.github.com>
2025-04-01 15:58:45 +0100

    Update 202503241812000-update-sentence-bands.ts
    Add the full list of languages for the May target.

--- server/src/lib/model/db/migrations/202503241812000-update-sentence-bands.ts
+++ server/src/lib/model/db/migrations/202503241812000-update-sentence-bands.ts
@@ -1,7 +1,11 @@
 export const up = async function (db: any): Promise<any> {
   await db.runSql(`
     UPDATE locales SET target_sentence_count = 750
-    WHERE name IN ( 'mki', 'gig', 'ggg', 'gjk', 'mve', 'odk', 'kvx', 'kxp', 'phr', 'ydg', 'sbn', 'lrk', 'btv', 'ssi') `);
+    WHERE name IN ( 'mki', 'gig', 'ggg', 'gjk', 'mve', 'odk', 'kvx', 'kxp', 'phr', 'ydg', 'sbn', 
+    'lrk', 'btv', 'ssi', 'tar', 'tli', 'yav', 'gid', 'udl', 'bag', 'mgg', 'mcn', 'mcx', 'bkh', 
+    'nla', 'giz', 'jgo', 'fmp', 'mua', 'mug', 'mhk', 'mse', 'mdd', 'ajg', 'kdh', 'gej', 'nmz', 
+    'prq', 'cpy', 'qug', 'qus', 'qur', 'mcf', 'hux', 'ttj', 'koo', 'rwm', 'bxk', 'cgg', 'lua', 
+    'hem', 'cjk', 'rof') `);
 };
 
 export const down = async function (): Promise<any> {
