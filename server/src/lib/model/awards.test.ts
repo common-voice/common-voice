fc52b792d4e17906d43d11675d9ec4119e757eff
Kathy Reid <katherine@mozillafoundation.com>
2025-05-01 01:56:09 +1000

    Incremental commit, started to refactor functions in awards.ts for easier testing NOTE: this will still fail checks

--- server/src/lib/model/awards.test.ts
+++ server/src/lib/model/awards.test.ts
@@ -1,17 +1,10 @@
-// Arrange
-
 import Awards from './awards'
-
-// Act
-
-// Assert
 
 describe('Test Custom Awards module', () => {
   it('should return correct date', () => {
     const db = { query: jest.fn(async () => {}) }
+
     Awards.checkProgress('test_id', { id: 1 }, db)
     expect(false).toBe(true)
   })
 })
-
-// arrange, act, assert
--- server/src/lib/model/awards.ts
+++ server/src/lib/model/awards.ts
@@ -2,42 +2,32 @@ import { pick } from 'common'
 import { getLocaleId } from './db'
 import { getMySQLInstance } from './db/mysql'
 import CustomGoal from './custom-goal'
-import { has } from 'fp-ts/lib/ReadonlyRecord'
-import { isPropertyAccessChain } from 'typescript'
 
-
-
-
-
+const db = getMySQLInstance()
 
 export default {
-
-  isReached(
-
-
-  )
-
   async checkProgress(
     client_id: string,
     locale: { id: number } | { name: string }
   ) {
     const localeId = 'id' in locale ? locale.id : await getLocaleId(locale.name)
-    console.log('localeId: ', localeId)
 
-    const [goal] = await CustomGoal.find(client_id, localeId)
-    console.log('goal: ', goal)
+    // check to see if a) there is a custom goal and b) if it has been reached
+    const customGoalReached = await this.checkCustomGoalReached(
+      client_id,
+      localeId
+    )
 
-    if (!goal) return
+    // if a CustomGoal is reached, we want to create an Award
+    if (customGoalReached) {
+      // define
+      const intervalStart = new Date(goal.current_interval_start)
+        .toISOString()
+        .slice(0, 19)
+        .replace('T', ' ')
 
-    const isReached = Object.values(goal.current).every(v => v >= goal.amount)
-
-    console.log('isReached: ', isReached)
-    if (!isReached) return
-
-    const intervalStart = new Date(goal.current_interval_start)
-      .toISOString()
-      .slice(0, 19)
-      .replace('T', ' ')
+      console.log('intervalStart: ', intervalStart)
+    }
 
     // how could we get a negative internval start date
     // interval could have the wrong positively / negativity
@@ -53,14 +43,12 @@ export default {
     // unit tests you don't want side effects, repeatable without side effects
     // mock the db - don't write anything to db, log to console for the query string
     // as much logic as possible in pure functions
-    // interface vs types 
+    // interface vs types
     // type is usually a Type - like an object - a collection of primitive datatypes - e.g. Car - maker, color, engine
-    // use interface - for methods, that are extensible 
-    // interfaces = behaviours / methods 
+    // use interface - for methods, that are extensible
+    // interfaces = behaviours / methods
     // types - properties / attributes
-    // need to check speak / listen 
-
-    console.log('intervalStart: ', intervalStart)
+    // need to check speak / listen
 
     const [[hasAwardAlready]] = await db.query(
       `
@@ -83,6 +71,43 @@ export default {
       `,
       [client_id, goal.id, intervalStart]
     )
+  },
+
+  /**
+   *
+   * @param client_id TODO DOCS
+   * @param localeId
+   */
+  async checkCustomGoalExists(client_id: string, localeId: number) {
+    const [goal] = await CustomGoal.find(client_id, localeId)
+    console.log('goal: ', goal)
+    if (goal) {
+      return goal
+    } else {
+      return false
+    }
+  },
+
+  /**
+   * Checks to see if a CustomGoal has been reached
+   *
+   * @param client_id - The unique identifier of the data contributor, who has previously created one or more Custom Goals
+   * @param localeId - The unique identifier of the Locale of the Custom Goal (as Custom Goals can be set on multiple Locales)
+   * @returns If there is no Custom Goal for the given client_id and localeID, returns false.
+   *          if there IS a Custom Goal for the given client_id and localeID AND the goal is reached, returns true.
+   * @example
+   * // TODO
+   */
+  async checkCustomGoalReached(goal: typeof CustomGoal) {
+    // there is a CustomGoal; check to see if the amount has been reached for the CustomGoal
+    const isReached = Object.values(goal.current).every(v => v >= goal.amount)
+    console.log('isReached: ', isReached)
+
+    if (!isReached) {
+      return false
+    } else {
+      return true
+    }
   },
 
   async find(client_id: string) {
