95468b47348a9b666f4b6d609de93a514de0da4a
Bülent Özden <bulentozden2007@gmail.com>
2025-06-23 12:43:37 +0300

    chore: Resolve reviews - remove unnecessary

--- server/src/application/text-cleaner.ts
+++ server/src/application/text-cleaner.ts
@@ -1,10 +1,3 @@
 export const cleanText = (s: string): string => {
-  return s.trim().replace(/\s/gi, ' ').replace(/\s+/g, ' ')
-}
-
-export const cleanTextArray = (ss: string[]): string[] => {
-  for (let i = 0; i < ss.length; i++) {
-    ss[i] = ss[i].trim().replace(/\s/gi, ' ').replace(/\s+/g, ' ')
-  }
-  return ss
+  return s.trim().replace(/\s/gi, ' ')
 }
