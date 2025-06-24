c1449e8be46beb772091ed7179bf1dd2af377434
moz-rotimib <rotimi@mozillafoundation.org>
2025-06-24 17:37:35 +0200

    fix: Update React import syntax and enhance sidebar navigation styles

--- web/src/components/pages/guidelines/constants.ts
+++ web/src/components/pages/guidelines/constants.ts
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from 'react'
 
 import { AddingSentences } from './sidebar-content/sentence-collection/adding-sentences'
 import { CitingSentences } from './sidebar-content/sentence-collection/citing-sentences'
