e41bb755575cfeec26c5c719a9ab8e5adf70e04d
Dmitrij Feller <dmitrij@mozillafoundation.org>
2025-07-16 11:54:08 +0200

    feat: check how many api credentials a user has before creating new ones

--- server/src/api/profile/handler/create-api-credentials-handler.ts
+++ server/src/api/profile/handler/create-api-credentials-handler.ts
@@ -1,18 +1,35 @@
 import { Request, Response } from 'express'
 import { StatusCodes } from 'http-status-codes'
 import { createClientCredentialsCommandHandler } from '../../../application/profile/command-handler/createClientCredentialsCommandHandler'
+import { getClientCredentialsQueryHandler } from '../../../application/profile/query-handler/getClientCredentialsQueryHandler'
+
+const MAX_CREDENTIALS_PER_USER = 10
 
 export const createApiCredentialsHandler = async (
   req: Request,
   res: Response
 ) => {
-  const clientId = req.session.user.client_id
+  const userId = req.session.user.client_id
 
-  if (!clientId)
-    return res.status(StatusCodes.BAD_REQUEST).json({ message: 'no client id' })
+  if (!userId) {
+    console.log('no user id')
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: 'no user client id' })
+  }
+
+  const userCredentials = await getClientCredentialsQueryHandler({
+    userId: userId,
+  })
+
+  if (userCredentials.length >= MAX_CREDENTIALS_PER_USER) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: 'too many credentials' })
+  }
 
   const result = await createClientCredentialsCommandHandler({
-    userId: clientId,
+    userId: userId,
     description: req.body.description,
   })
 
