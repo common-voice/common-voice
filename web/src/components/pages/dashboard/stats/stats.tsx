834c8db21d189d85d741db75b0845137f1f65bff
Bülent Özden <bulent@mozillafoundation.org>
2025-08-24 22:55:10 +0300

    Fix rule-of-hooks problem (#5054)

--- web/src/components/pages/dashboard/stats/stats.tsx
+++ web/src/components/pages/dashboard/stats/stats.tsx
@@ -7,57 +7,59 @@ import StatsCard from './stats-card'
 
 import './stats.css'
 
+const ContributionActivityYou = ({ locale }: { locale: string }) => (
+  <ContributionActivity from="you" locale={locale} key={'you_' + locale} />
+)
+const ContributionActivityEveryone = ({ locale }: { locale: string }) => (
+  <ContributionActivity
+    from="everyone"
+    locale={locale}
+    key={'everyone_' + locale}
+  />
+)
+
 const StatsPage = ({ allGoals, dashboardLocale }: Props) => {
-  if (!allGoals) {
-    return null
-  }
-
-  const ContributionActivityYou = ({ locale }: { locale: string }) => (
-    <ContributionActivity from="you" locale={locale} key={'you_' + locale} />
-  )
-  const ContributionActivityEveryone = ({ locale }: { locale: string }) => (
-    <ContributionActivity
-      from="everyone"
-      locale={locale}
-      key={'everyone_' + locale}
-    />
-  )
-
   return (
     <div className="stats-page">
-      <div className="cards">
-        {['speak', 'listen'].map(type => {
-          const [current, goals] =
-            allGoals.globalGoals[type == 'speak' ? 'clips' : 'votes']
-          return (
-            <ProgressCard
-              key={type + dashboardLocale}
-              type={type as ProgressCardProps['type']}
-              locale={dashboardLocale}
-              personalCurrent={current}
-              personalGoal={
-                allGoals
-                  ? (goals.find(g => !g.date) || { goal: Infinity }).goal
-                  : null
-              }
-            />
-          )
-        })}
-      </div>
+      {!allGoals ? (
+        <>...</>
+      ) : (
+        <>
+          <div className="cards">
+            {['speak', 'listen'].map(type => {
+              const [current, goals] =
+                allGoals.globalGoals[type == 'speak' ? 'clips' : 'votes']
+              return (
+                <ProgressCard
+                  key={type + dashboardLocale}
+                  type={type as ProgressCardProps['type']}
+                  locale={dashboardLocale}
+                  personalCurrent={current}
+                  personalGoal={
+                    allGoals
+                      ? (goals.find(g => !g.date) || { goal: Infinity }).goal
+                      : null
+                  }
+                />
+              )
+            })}
+          </div>
 
-      <div className="cards">
-        <StatsCard
-          id="stats-contribution"
-          scrollable
-          title="contribution-activity"
-          currentLocale={dashboardLocale}
-          tabs={{
-            you: ContributionActivityYou,
-            everyone: ContributionActivityEveryone,
-          }}
-        />
-        <LeaderboardCard currentLocale={dashboardLocale} />
-      </div>
+          <div className="cards">
+            <StatsCard
+              id="stats-contribution"
+              scrollable
+              title="contribution-activity"
+              currentLocale={dashboardLocale}
+              tabs={{
+                you: ContributionActivityYou,
+                everyone: ContributionActivityEveryone,
+              }}
+            />
+            <LeaderboardCard currentLocale={dashboardLocale} />
+          </div>
+        </>
+      )}
     </div>
   )
 }
