d1bbf6679b26b21a53b42a875f062e1ce0492d9d
moz-bozden <bulent@mozillafoundation.org>
2025-10-07 18:32:36 +0300

    Add new LanguageData definitions

--- common/language.ts
+++ common/language.ts
@@ -1,38 +1,38 @@
 // a single accent object
 export type Accent = {
-  id: number;
-  name: string;
-  token?: string;
-  clientId?: string;
-};
+  id: number
+  name: string
+  token?: string
+  clientId?: string
+}
 
 type SentenceCount = {
-  currentCount: number;
-  targetSentenceCount: number;
-};
+  currentCount: number
+  targetSentenceCount: number
+}
 
-// a single accent object
+// a single language object
 export type Language = {
-  id: number;
-  name: string;
-  sentenceCount: SentenceCount;
-  target_sentence_count?: number;
-  is_contributable?: boolean;
-  is_translated?: boolean;
-  native_name: string;
-  text_direction: string;
-};
+  id: number
+  name: string
+  sentenceCount: SentenceCount
+  target_sentence_count?: number
+  is_contributable?: boolean
+  is_translated?: boolean
+  native_name: string
+  text_direction: string
+}
 
 // single variant object
 export type Variant = {
-  id: number;
-  locale: string;
-  name: string;
-  tag: string;
-};
+  id: number
+  locale: string
+  name: string
+  tag: string
+}
 
 export type UserVariant = Variant & {
-  is_preferred_option: boolean;
+  is_preferred_option: boolean
 }
 
 /*
@@ -40,7 +40,33 @@ export type UserVariant = Variant & {
   accent/locale/variant data for a user
 */
 export type UserLanguage = {
-  locale: string;
-  variant?: UserVariant;
-  accents?: Accent[];
-};
+  locale: string
+  variant?: UserVariant
+  accents?: Accent[]
+}
+
+// Combined Language Data
+export type VariantData = {
+  id: number
+  code: string
+  name: string
+  type?: string
+  locale_id: number
+}
+export type AccentData = {
+  id: number
+  code: string
+  name: string
+  locale_id: number
+}
+export type LanguageData = {
+  id: number
+  code: string
+  target_sentence_count?: number
+  native_name: string
+  is_contributable?: number
+  is_translated?: number
+  text_direction: string
+  variants: VariantData[]
+  predefined_accents: AccentData[]
+}
