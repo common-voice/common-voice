cbf0c8a077858813309f7fd24db7c2e39beb6d55
moz-bozden <bulent@mozillafoundation.org>
2025-11-14 02:48:12 +0300

    Protect profile routes

--- server/src/api/profile/routes.ts
+++ server/src/api/profile/routes.ts
@@ -5,16 +5,30 @@ import { CreateApiCredentialsRequest } from './validation/create-api-credentials
 import { createApiCredentialsHandler } from './handler/create-api-credentials-handler'
 import { getApiCredentialsHandler } from './handler/get-api-credentials-handler'
 import { deleteApiCredentialsHandler } from './handler/delete-api-credentials-handler'
+import { RequireUserMiddleware } from '../../lib/middleware/requireUserMiddleware'
+import { RequireFeatureMiddleware } from '../../lib/middleware/requireFeatureMiddleware'
 
 export const profilesRouter = PromiseRouter({ mergeParams: true })
   .post(
     '/api-credentials',
     rateLimiter('api/v1/profiles/api-credentials', {
-      points: 10,
+      points: 1,
       duration: 60,
     }),
     validateStrict({ body: CreateApiCredentialsRequest }),
+    new RequireUserMiddleware().handle,
+    RequireFeatureMiddleware.handle('papi-credentials'),
     createApiCredentialsHandler
   )
-  .get('/api-credentials', getApiCredentialsHandler)
-  .delete('/api-credentials/:client_id', deleteApiCredentialsHandler)
+  .get(
+    '/api-credentials',
+    new RequireUserMiddleware().handle,
+    RequireFeatureMiddleware.handle('papi-credentials'),
+    getApiCredentialsHandler
+  )
+  .delete(
+    '/api-credentials/:client_id',
+    new RequireUserMiddleware().handle,
+    RequireFeatureMiddleware.handle('papi-credentials'),
+    deleteApiCredentialsHandler
+  )
