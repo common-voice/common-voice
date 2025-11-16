7b4fc29424d23b267890bb04cf0e33d630f3c15b
Bülent Özden <bulent@mozillafoundation.org>
2025-11-16 05:22:49 +0300

    1477-Handle not found errors (#5174)
    * Add and update FE URLs
    
    * Fix footer link
    
    * chore: Fix several links
    
    * Gate access
    
    * Handle acces in BE
    
    * Rewrite to match SS
    
    * chore: fix missing links

--- web/src/components/pages/datasets/datasets.tsx
+++ web/src/components/pages/datasets/datasets.tsx
@@ -1,14 +1,36 @@
-import * as React from 'react';
+import * as React from 'react'
 
-import DatasetInfo from './dataset-info';
-import Resources from './resources';
-import Page from '../../ui/page';
+import DatasetInfo from './dataset-info'
+import Resources from './resources'
+import Page from '../../ui/page'
+import { Spinner } from '../../ui/ui'
+import {
+  useFeature,
+  useFeatureContextLoaded,
+} from '../../../contexts/feature-context'
+import URLS from '../../../urls'
 
-const Datasets = () => (
-  <Page className="datasets-content">
-    <DatasetInfo />
-    <Resources />
-  </Page>
-);
+const Datasets = () => {
+  const hasOldDatasetsFeature = useFeature('datasets-old')
+  const featureContextLoaded = useFeatureContextLoaded()
 
-export default Datasets;
+  // Show spinner while feature context is loading
+  if (!featureContextLoaded) {
+    return <Spinner />
+  }
+
+  // Redirect to MDC datasets if feature flag is not present
+  if (!hasOldDatasetsFeature) {
+    window.location.href = URLS.MDC_DATASETS
+    return <Spinner />
+  }
+
+  return (
+    <Page className="datasets-content">
+      <DatasetInfo />
+      <Resources />
+    </Page>
+  )
+}
+
+export default Datasets
