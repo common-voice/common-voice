1d20491abc8897014c09e991305b6f589ea8a676
moz-bozden <bulent@mozillafoundation.org>
2025-11-29 12:51:11 +0300

    Update backend to gate bulk sentences

--- server/src/api/bulk-submissions/routes.ts
+++ server/src/api/bulk-submissions/routes.ts
@@ -1,9 +1,11 @@
 import PromiseRouter from 'express-promise-router'
 import rateLimiter from '../../lib/middleware/rate-limiter-middleware'
+import { RequireFeatureMiddleware } from '../../lib/middleware/requireFeatureMiddleware'
 import { addBulkSubmissionHandler } from './handler/add-bulk-submission-handler'
 
 export const bulkSubmissionsRouter = PromiseRouter({ mergeParams: true }).post(
   '/',
+  RequireFeatureMiddleware.handle('bulk-upload'),
   rateLimiter('api/v1/:locale/bulk-submissions/', { points: 1, duration: 600 }),
   addBulkSubmissionHandler
 )
