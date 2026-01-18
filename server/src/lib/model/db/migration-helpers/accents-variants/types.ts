d1c9a9ba897cdfecca97d80bd63f2d7c30469214
moz-bozden <bulent@mozillafoundation.org>
2026-01-18 14:10:39 +0300

    Add new mapping types

--- server/src/lib/model/db/migration-helpers/accents-variants/types.ts
+++ server/src/lib/model/db/migration-helpers/accents-variants/types.ts
@@ -1,2 +1,14 @@
-// array of tuples [accent_token, variant_token]
+// accent -> variant mapping
+// array of tuples [accent_token, variant_token, force_delete_old?]
+// Unreferenced accents are already get deleted
 export type AV_MAPPING_TYPE = [string, string, boolean?][]
+
+// accent -> accent mapping
+// array of tuples [old_accent_token, new_accent_token, force_delete_old?]
+// Unreferenced accents are already get deleted
+export type AA_MAPPING_TYPE = [string, string, boolean?][]
+
+// accent -> accent + variant mapping
+// array of tuples [old_accent_token, new_accent_token, new_variant_token, force_delete_old?]
+// Unreferenced accents are already get deleted
+export type AAV_MAPPING_TYPE = [string, string, string, boolean?][]
