34062c4d018556a213bba0841b3a7c66e2f2a566
moz-bozden <bulent@mozillafoundation.org>
2026-01-18 11:48:54 +0300

    Update all to Band A

--- server/src/lib/model/db/migrations/20260116160000-update-sentence-bands.ts
+++ server/src/lib/model/db/migrations/20260116160000-update-sentence-bands.ts
@@ -1,20 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export const up = async function (db: any): Promise<any> {
   // BAND A (750 sentences - <1M speakers)
   await db.runSql(
     `
       UPDATE locales SET target_sentence_count = 750
       WHERE name IN (
-        'aii', 'efk'
+        'aii', 'efk', 'laj', 'tum'
         )
-    `
-  )
-  // BAND B (2000 sentences - 1-10M speakers)
-  await db.runSql(
-    `
-      UPDATE locales SET target_sentence_count = 2000
-      WHERE name IN ( 
-        'laj', 'tum'
-      )
     `
   )
 }
