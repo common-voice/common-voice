50ad6c6f6260e2e6195bed45c2201ecceea275f8
moz-bozden <bulent@mozillafoundation.org>
2026-01-19 13:34:47 +0300

    chore: fix comments

--- server/src/lib/model/db/migration-helpers/accents-variants/types.ts
+++ server/src/lib/model/db/migration-helpers/accents-variants/types.ts
@@ -1,14 +1,14 @@
 // accent -> variant mapping
-// array of tuples [accent_token, variant_token, force_delete_old?]
-// Unreferenced accents are already get deleted
+// array of tuples [accent_token, variant_token, delete_old?]
+// If delete_old is true (default), unreferenced accents will be deleted
 export type AV_MAPPING_TYPE = [string, string, boolean?][]
 
 // accent -> accent mapping
-// array of tuples [old_accent_token, new_accent_token, force_delete_old?]
-// Unreferenced accents are already get deleted
+// array of tuples [old_accent_token, new_accent_token, delete_old?]
+// If delete_old is true (default), unreferenced accents will be deleted
 export type AA_MAPPING_TYPE = [string, string, boolean?][]
 
 // accent -> accent + variant mapping
-// array of tuples [old_accent_token, new_accent_token, new_variant_token, force_delete_old?]
-// Unreferenced accents are already get deleted
+// array of tuples [old_accent_token, new_accent_token, new_variant_token, delete_old?]
+// If delete_old is true (default), unreferenced accents will be deleted
 export type AAV_MAPPING_TYPE = [string, string, string, boolean?][]
